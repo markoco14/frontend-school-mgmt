c4e707f30f65f06b38b65e73d9d0599a2536f9fe
Mark O'Connor <mark.oconnor14@gmail.com>
2023-06-20 10:15:34 +0800

    feature: make basic owner registration form on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { InferGetServerSidePropsType, GetServerSideProps } from 'next';
 import Link from 'next/link';
+import { useRef, useState } from 'react';
 
 type Student = {
   first_name: string;
@@ -16,18 +17,57 @@ export const getServerSideProps: GetServerSideProps<{
 };
 
 export default function Home({students}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const [isSignUp, setIsSignUp] = useState<boolean>(true);
+  const [ownerName, setOwnerName] = useState<string>('');
+
+  const ownerNameRef = useRef<HTMLInputElement>(null);
+
+  async function handleSubmit() {
+    if (ownerName === '' || ownerName === undefined || ownerName === null) {
+              alert('You need to choose your name')
+              return;
+            }
+            console.log("Name saved in the DB")
+            setOwnerName('')
+            ownerNameRef.current ? ownerNameRef.current.value = '' : null;
+            alert('Name saved successfully')
+  }
+
   return (
     <main
       className='min-h-screen p-24 max-w-[600px] mx-auto'
     >
       <nav className='flex gap-2'>
         <Link href="/">Home</Link>
-        <Link href="/school-mgmt">School</Link>
-        <Link href="/student-mgmt">Student</Link>
       </nav>
       <h1>Easy Cram School Management In The Cloud.</h1>
       <section>
         <h2>Sign up your school to get started.</h2>
+        <div>
+          <button onClick={() => setIsSignUp(true)}>Sign Up</button>
+          <button onClick={() => setIsSignUp(false)}>Log In</button>
+        </div>
+        {isSignUp ? (
+          <form onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit();
+            }}>
+            <div>
+              <label>Name</label>
+              <input ref={ownerNameRef} type="text" onChange={(e) => setOwnerName(e.target.value)}/>
+            </div>
+            <button>Save</button>
+          </form>
+        ) : (
+          <ul>
+            <li>Owner A</li>
+            <li>Owner B</li>
+            <li>Owner C</li>
+            <li>Owner D</li>
+            <li>Owner E</li>
+            <li>Owner F</li>
+          </ul>
+        )}
       </section>
     </main>
   )
