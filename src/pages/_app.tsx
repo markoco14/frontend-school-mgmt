af7d00427018ea785b97d5101a7d266666fda094
Mark O'Connor <mark.oconnor14@gmail.com>
2023-06-29 18:17:34 +0800

    feature: install react hot toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { UserContext } from '@/src/context'
 import { useState } from 'react'
+import { Toaster } from 'react-hot-toast'
 
 export default function App({ Component, pageProps }: AppProps) {
   const [user, setUser] = useState(null)
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <UserContext.Provider value={{user, setUser}}>
       <Component {...pageProps} />
+      <Toaster />
     </UserContext.Provider>
   )
 }
