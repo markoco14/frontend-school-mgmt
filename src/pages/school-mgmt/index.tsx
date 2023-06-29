75a3cba69068dc0ab659f0450420665d276efb0b
Mark O'Connor <mark.oconnor14@gmail.com>
2023-06-29 17:23:07 +0800

    feature: apply basic layout and styles to school-mgmt page

--- src/pages/school-mgmt/index.tsx
+++ src/pages/school-mgmt/index.tsx
@@ -41,7 +41,7 @@ export default function Home() {
     <main
       className='min-h-screen max-w-[600px] mx-auto'
     >
-      <nav className='flex justify-between'>
+      <nav className='h-[48px] flex justify-between items-center px-4'>
         <div className='flex gap-2'>
           <Link href="/">Home</Link>
           <Link href="/school-mgmt/">Schools</Link>
@@ -52,41 +52,40 @@ export default function Home() {
           router.push('/')
         }}>Log Out</button>
       </nav>
-      <h1 className='mb-8'>Manage all your schools from here!</h1>
-      {loading ? (
-        <p className='flex justify-center bg-white p-4 rounded-lg'>loading...</p>
-      ) : (
-        <section className='bg-white p-4 rounded-lg'>
-          {mySchools.length > 0 ? (
-            <>
-              <div className="flex justify-between items-center mb-4">
-                <h2 className='text-3xl'>Your schools</h2>
-                <Link href="/school-mgmt/add" className='bg-blue-500 px-2 py-2 rounded'>+ school</Link>
-              </div>
-              <ul className='flex flex-col gap-2'>
-                {mySchools?.map((school: School, index) => (
-                  <li key={index} className='p-2 rounded-md hover:bg-gray-200 flex justify-between'>
-                    <span>{school.name}</span>
-                    <div className='flex gap-2'>
-                      {/* TODO: EDIT TRIGGERS MODEL TO UPDATE INFO OR DELETE */}
-                      <button>Edit</button>
-                    </div>
-                  </li>
-                  ))}
-              </ul>
-            </>
-          ) : (
+      <div>
+        <h1 className='rounded-lg mb-4 p-4 bg-white'>Manage all your schools from here!</h1>
+        {loading ? (
+          <p className='flex justify-center bg-white p-4 rounded-lg'>loading...</p>
+        ) : (
+          <section className='bg-white p-4 rounded-lg'>
+            {mySchools.length > 0 ? (
               <>
-                <p>You have no schools</p>
-                <Link href="/school-mgmt/add">Add School</Link>
+                <div className="flex justify-between items-center mb-4">
+                  <h2 className='text-3xl'>Your schools</h2>
+                  <Link href="/school-mgmt/add" className='bg-blue-500 px-2 py-2 rounded'>+ school</Link>
+                </div>
+                <ul className='flex flex-col gap-2'>
+                  {mySchools?.map((school: School, index) => (
+                    <li key={index} className='p-2 rounded-md hover:bg-blue-200 hover:cursor-not-allowed flex justify-between'>
+                      <span>{school.name}</span>
+                      <div className='flex gap-2'>
+                        {/* TODO: EDIT TRIGGERS MODEL TO UPDATE INFO OR DELETE */}
+                      </div>
+                    </li>
+                    ))}
+                </ul>
               </>
+            ) : (
+                <>
+                  <p>You have no schools</p>
+                  <Link href="/school-mgmt/add">Add School</Link>
+                </>
 
-          )}
-          
-        </section>
-      )}
-
-
+            )}
+            
+          </section>
+        )}
+      </div>
     </main>
   )
 }
