065e6716db25bd4ba52ad94423e1fa42f8a0e505
Mark O'Connor <mark.oconnor14@gmail.com>
2023-06-30 13:05:36 +0800

    feature: introduce app layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,28 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { UserContext } from '@/src/context'
-import { useState } from 'react'
+import { ReactElement, ReactNode, useState } from 'react'
 import { Toaster } from 'react-hot-toast'
+import { NextPage } from 'next'
 
-export default function App({ Component, pageProps }: AppProps) {
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const [user, setUser] = useState(null)
 
-  return (
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return getLayout(
     <UserContext.Provider value={{user, setUser}}>
       <Component {...pageProps} />
       <Toaster />
     </UserContext.Provider>
   )
 }
+
