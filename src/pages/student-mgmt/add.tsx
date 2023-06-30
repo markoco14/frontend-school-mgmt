3efaaa96da28d70e0c802326068ade61534a021d
Mark O'Connor <mark.oconnor14@gmail.com>
2023-06-30 13:24:46 +0800

    feature: style add student and delete student pages

--- src/pages/student-mgmt/add.tsx
+++ src/pages/student-mgmt/add.tsx
@@ -1,46 +1,70 @@
-import Layout from "@/src/modules/core/infrastructure/ui/components/Layout"
-import { Student } from "@/src/modules/student-mgmt/domain/entities/Student"
-import { studentAdapter } from "@/src/modules/student-mgmt/infrastructure/adapters/studentAdapter"
-import Link from "next/link"
-import { FormEvent, useState } from "react"
+import Layout from "@/src/modules/core/infrastructure/ui/components/Layout";
+import { Student } from "@/src/modules/student-mgmt/domain/entities/Student";
+import { studentAdapter } from "@/src/modules/student-mgmt/infrastructure/adapters/studentAdapter";
+import Link from "next/link";
+import { FormEvent, useState } from "react";
 
 export default function AddStudent() {
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+  const [age, setAge] = useState<number>(0);
 
-	const [firstName, setFirstName] = useState<string>('')
-	const [lastName, setLastName] = useState<string>('')
-	const [age, setAge] = useState<number>(0)
+  async function handleAddStudent(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (!firstName || !lastName || !age) {
+      return;
+    }
 
-	async function handleAddStudent(e: FormEvent<HTMLFormElement>) {
-		e.preventDefault();	
-		if (!firstName || !lastName || !age) {
-			return
-		}	
+    const student: Student = await studentAdapter.addStudent({
+      firstName: firstName,
+      lastName: lastName,
+      age: age,
+    });
 
-		const student: Student = await studentAdapter.addStudent({firstName: firstName, lastName: lastName, age: age})
-		
-		return student;
-	}
+    return student;
+  }
 
-	return (
-		<Layout>
-			<h1>Add student</h1>
-			<Link href="/student-mgmt/add">Add</Link>
-			<Link href="/student-mgmt/delete">Delete</Link>
-			<form onSubmit={(e) => handleAddStudent(e)}>
-				<div className="flex flex-col">
-					<label>First Name</label>
-					<input className='px-1 py-2 rounded shadow'required type="text" onChange={(e) => setFirstName(e.target.value)}/>
-				</div>
-				<div className="flex flex-col">
-					<label>Last Name</label>
-					<input className='px-1 py-2 rounded shadow'required type="text" onChange={(e) => setLastName(e.target.value)}/>
-				</div>
-				<div className="flex flex-col">
-					<label>Age</label>
-					<input className='px-1 py-2 rounded shadow'required type="number" onChange={(e) => setAge(Number(e.target.value))}/>
-				</div>
-				<button>Add</button>
-			</form>
-		</Layout>
-	)
+  return (
+    <Layout>
+      <h1 className="mb-4 p-4">Sign up your students and manage their info.</h1>
+      <section className="bg-white p-4 rounded-lg">
+        <div className="flex justify-between items-baseline mb-4">
+          <h2 className="text-3xl">Enter your new student information</h2>
+          <Link href="/student-mgmt/">Back</Link>
+        </div>
+        <form onSubmit={(e) => handleAddStudent(e)}>
+          <div className="flex flex-col">
+            <label>First Name</label>
+            <input
+              className="px-1 py-2 rounded shadow"
+              required
+              type="text"
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col">
+            <label>Last Name</label>
+            <input
+              className="px-1 py-2 rounded shadow"
+              required
+              type="text"
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col">
+            <label>Age</label>
+            <input
+              className="px-1 py-2 rounded shadow"
+              required
+              type="number"
+              onChange={(e) => setAge(Number(e.target.value))}
+            />
+          </div>
+          <button className="bg-blue-300 text-blue-900 hover:bg-blue-500 hover:text-white px-4 py-1 rounded">
+            Add
+          </button>
+        </form>
+      </section>
+    </Layout>
+  );
 }
--- src/pages/student-mgmt/delete.tsx
+++ src/pages/student-mgmt/delete.tsx
@@ -1,11 +1,11 @@
-import Layout from '@/src/modules/core/infrastructure/ui/components/Layout';
-import { studentAdapter } from '@/src/modules/student-mgmt/infrastructure/adapters/studentAdapter';
-import type { InferGetServerSidePropsType, GetServerSideProps } from 'next';
-import Link from 'next/link';
-import { useState } from 'react';
+import Layout from "@/src/modules/core/infrastructure/ui/components/Layout";
+import { studentAdapter } from "@/src/modules/student-mgmt/infrastructure/adapters/studentAdapter";
+import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
+import Link from "next/link";
+import { useState } from "react";
 
 type Student = {
-	id: number;
+  id: number;
   first_name: string;
   last_name: string;
   age: number;
@@ -19,30 +19,42 @@ export const getServerSideProps: GetServerSideProps<{
   return { props: { students } };
 };
 
-export default function Home({students}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Home({
+  students,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const [filteredStudents, setFilteredStudents] = useState<Student[]>(students);
 
-	async function handleDeleteStudent(id: number) {
-    const response = await studentAdapter.deleteStudentById({id: id});
-    const newFilteredStudents = filteredStudents.filter(student => student.id !== id);
+  async function handleDeleteStudent(id: number) {
+    const response = await studentAdapter.deleteStudentById({ id: id });
+    const newFilteredStudents = filteredStudents.filter(
+      (student) => student.id !== id
+    );
     setFilteredStudents(newFilteredStudents);
 
-    return response.json()
-	}
+    return response.json();
+  }
 
   return (
     <Layout>
-      <h1>Testing the Django Api and Frontend</h1>
-      <Link href="/student-mgmt/add">Add</Link>
-      <Link href="/student-mgmt/delete">Delete</Link>
-      <ul>
-        {filteredStudents?.map((student: Student, index) => (
-          <li key={index} className='flex justify-between gap-4'>
-						<span>{student.first_name} {student.last_name} {student.age}</span>
-						<button onClick={() => handleDeleteStudent(student.id)}>Delete</button>
-					</li>
-        ))}
-      </ul>
+      <h1 className="mb-4 p-4">Sign up your students and manage their info.</h1>
+      <section className="bg-white p-4 rounded-lg">
+        <div className="flex justify-between items-baseline mb-4">
+          <h2 className="text-3xl">Edit student profiles here.</h2>
+          <Link href="/student-mgmt/">Back</Link>
+        </div>
+        <ul>
+          {filteredStudents?.map((student: Student, index) => (
+            <li key={index} className="flex justify-between gap-4">
+              <span>
+                {student.first_name} {student.last_name} {student.age}
+              </span>
+              <button onClick={() => handleDeleteStudent(student.id)}>
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
-  )
+  );
 }
