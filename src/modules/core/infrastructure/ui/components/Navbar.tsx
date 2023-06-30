5eec525f8b52374c7e88abb7029a1ca1b8c64af8
Mark O'Connor <mark.oconnor14@gmail.com>
2023-06-30 13:27:28 +0800

    feature: add if user link rendering to nav bar component

--- src/modules/core/infrastructure/ui/components/Navbar.tsx
+++ src/modules/core/infrastructure/ui/components/Navbar.tsx
@@ -4,15 +4,16 @@ import { useRouter } from "next/router";
 import { useContext } from "react";
 
 export default function Navbar() {
-	const context = useContext(UserContext);
-	const router = useRouter();
-	return (
-		<nav className="h-[48px] flex justify-between items-center px-4">
-        <div className="flex gap-2">
-          <Link href="/">Home</Link>
-          <Link href="/school-mgmt/">Schools</Link>
-          <Link href="/student-mgmt/">Students</Link>
-        </div>
+  const context = useContext(UserContext);
+  const router = useRouter();
+  return (
+    <nav className="h-[48px] flex justify-between items-center px-4">
+      <div className="flex gap-2">
+        <Link href="/">Home</Link>
+        {context?.user ? <Link href="/school-mgmt/">Schools</Link> : null}
+        {context?.user ? <Link href="/student-mgmt/">Students</Link> : null}
+      </div>
+      {context?.user ? (
         <button
           onClick={() => {
             context.setUser();
@@ -21,6 +22,7 @@ export default function Navbar() {
         >
           Log Out
         </button>
-      </nav>
-	)
+      ) : null}
+    </nav>
+  );
 }
