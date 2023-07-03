2a42c1f40385f6b23e7c1e4c6ece866b011def93
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-03 09:10:29 +0800

    feature: add getStudentsByOwnerId to studentAdapter

--- src/modules/student-mgmt/infrastructure/adapters/studentAdapter.ts
+++ src/modules/student-mgmt/infrastructure/adapters/studentAdapter.ts
@@ -15,6 +15,13 @@ class StudentAdapter {
 		return student;
 	}
 
+	public async getStudentsByOwnerId({id}: {id: number}): Promise<Student[]> {
+		const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/get-students-by-owner/${id}/`);
+		const students: Student[] = await res.json();
+		
+		return students;
+	}
+
 	public async getStudentsBySchoolId({id}: {id: number}): Promise<Student[]> {
 		const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/get-students-by-school/${id}/`);
 		const students: Student[] = await res.json();
