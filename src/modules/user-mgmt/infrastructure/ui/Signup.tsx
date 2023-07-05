c8fb8332be432dcc2c80142a387bbd6ac35babf6
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-05 18:20:27 +0800

    refactor: move sign up logic to component

---
+++ src/modules/user-mgmt/infrastructure/ui/Signup.tsx
@@ -0,0 +1,81 @@
+import { UserContext } from "@/src/context";
+import { useRouter } from "next/router";
+import { useContext } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import toast from "react-hot-toast";
+import { User } from "@/src/modules/user-mgmt/domain/entities/User";
+import { userAdapter } from "../adapters/userAdapter";
+
+
+type Inputs = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+
+export default function Signup() {
+
+	const { reset, register, handleSubmit, formState: { errors }} = useForm<Inputs>();
+
+	const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    try {
+      const user: User = await userAdapter.addUser({
+        firstName: data.firstName,
+        lastName: data.lastName,
+        email: data.email,
+        password: data.password
+      });
+      toast.success('User added.');
+      reset();
+      console.log(user)
+    } catch (error) {
+      console.error(error)
+    }
+  };
+
+  return (
+    <>
+			<h2 className="mb-4">Sign up to get started.</h2>
+			<form
+				onSubmit={handleSubmit(onSubmit)}
+			>
+				<div className="flex flex-col mb-4">
+						<label className="mb-2">First Name</label>
+					<input
+						type="text"
+						{...register("firstName", { required: true, minLength: 2, maxLength: 50 })}
+						className="shadow-md border p-2 rounded"
+					/>
+				</div>
+				<div className="flex flex-col mb-4">
+					<label className="mb-2">Last Name</label>
+					<input
+						type="text"
+						{...register("lastName", { required: true, minLength: 2, maxLength: 50 })}
+						className="shadow-md border p-2 rounded"
+					/>
+				</div>
+				<div className="flex flex-col mb-4">
+					<label className="mb-2">Email</label>
+					<input
+						type="email"
+						{...register("email", { required: true, minLength: 3, maxLength: 50 })}
+						className="shadow-md border p-2 rounded"
+					/>
+				</div>
+				<div className="flex flex-col mb-4">
+					<label className="mb-2">Password</label>
+					<input
+						type="password"
+						{...register("password", { required: true, minLength: 2, maxLength: 50 })}
+						className="shadow-md border p-2 rounded"
+					/>
+				</div>
+				<button className="bg-blue-300 text-blue-900 hover:bg-blue-500 hover:text-white px-4 py-1 rounded">
+					Save
+				</button>
+			</form>
+		</>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ import Layout from "../modules/core/infrastructure/ui/components/Layout";
 import { useForm, SubmitHandler } from "react-hook-form";
 import toast from "react-hot-toast";
 import Login from "../modules/user-mgmt/infrastructure/ui/Login";
+import Signup from "../modules/user-mgmt/infrastructure/ui/Signup";
 
 type Inputs = {
   firstName: string;
@@ -29,30 +30,8 @@ export default function Home({
   users,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
 
-  console.log('logging users', users)
-
   const context = useContext(UserContext);
-  const router = useRouter();
   const [isSignUp, setIsSignUp] = useState<boolean>(true);
-
-  const { reset, register, handleSubmit, formState: { errors }} = useForm<Inputs>();
-
-  const onSubmit: SubmitHandler<Inputs> = async (data) => {
-    try {
-      const user: User = await userAdapter.addUser({
-        firstName: data.firstName,
-        lastName: data.lastName,
-        email: data.email,
-        password: data.password
-      });
-      toast.success('User added.');
-      reset();
-      console.log(user)
-      users.push(user)
-    } catch (error) {
-      console.error(error)
-    }
-  };
 
   return (
     <Layout>
@@ -103,48 +82,7 @@ export default function Home({
             </div>
 
             {isSignUp ? (
-              <>
-                <h2 className="mb-4">Sign up to get started.</h2>
-                <form
-                  onSubmit={handleSubmit(onSubmit)}
-                >
-                  <div className="flex flex-col mb-4">
-                      <label className="mb-2">First Name</label>
-                    <input
-                      type="text"
-                      {...register("firstName", { required: true, minLength: 2, maxLength: 50 })}
-                      className="shadow-md border p-2 rounded"
-                    />
-                  </div>
-                  <div className="flex flex-col mb-4">
-                    <label className="mb-2">Last Name</label>
-                    <input
-                      type="text"
-                      {...register("lastName", { required: true, minLength: 2, maxLength: 50 })}
-                      className="shadow-md border p-2 rounded"
-                    />
-                  </div>
-                  <div className="flex flex-col mb-4">
-                    <label className="mb-2">Email</label>
-                    <input
-                      type="email"
-                      {...register("email", { required: true, minLength: 3, maxLength: 50 })}
-                      className="shadow-md border p-2 rounded"
-                    />
-                  </div>
-                  <div className="flex flex-col mb-4">
-                    <label className="mb-2">Password</label>
-                    <input
-                      type="password"
-                      {...register("password", { required: true, minLength: 2, maxLength: 50 })}
-                      className="shadow-md border p-2 rounded"
-                    />
-                  </div>
-                  <button className="bg-blue-300 text-blue-900 hover:bg-blue-500 hover:text-white px-4 py-1 rounded">
-                    Save
-                  </button>
-                </form>
-              </>
+              <Signup />
             ) : (
               <Login />
             )}
