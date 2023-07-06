08809ca234902d903f0a4c2b9c58f56daf4e913c
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-06 23:33:52 +0800

    feature: add getReportsAll request to report-mgmt index page in getServerSideProps

--- src/pages/report-mgmt/index.tsx
+++ src/pages/report-mgmt/index.tsx
@@ -1,13 +1,27 @@
-import Link from 'next/link';
 import Layout from '@/src/modules/core/infrastructure/ui/components/Layout';
+import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
+import { reportAdapter } from '@/src/modules/report-mgmt/infrastructure/adapters/reportAdapter';
+import { Report } from '@/src/modules/report-mgmt/domain/entities/Report';
 
-export default function ReportsHome() {
+export const getServerSideProps: GetServerSideProps<{
+  reports: Report[];
+}> = async () => {
+  const reports = await reportAdapter.getReportsAll();
+
+  return { props: { reports } };
+};
+
+export default function ReportsHome({
+  reports,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <Layout>
       <div>
         <h1 className="mb-4 p-4">Write daily reports here.</h1>
         <section className="bg-white p-4 rounded-lg">
-          <p>content goes here</p>
+          {reports?.map((report: Report, index: number) => (
+            <p key={index}>{report.content} {report.is_complete ? "Complete" : "WIP"}</p>
+          ))}
         </section>
       </div>
     </Layout>
