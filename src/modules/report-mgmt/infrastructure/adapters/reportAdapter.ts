c7289d324d60975f5fbc98bacb22a8701aa4e508
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-06 23:31:08 +0800

    feature: add getReportsAll method to reportAdapter

--- src/modules/report-mgmt/infrastructure/adapters/reportAdapter.ts
+++ src/modules/report-mgmt/infrastructure/adapters/reportAdapter.ts
@@ -1,9 +1,12 @@
 import { Report } from "../../domain/entities/Report";
 
-Report
-
 class ReportAdapter {
+	public async getReportsAll(): Promise<Report[]> {
+		const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/get-reports-all/`);
+		const reports: Report[] = await res.json();
 
+		return reports
+	}
 }
 
 export const reportAdapter = new ReportAdapter();
