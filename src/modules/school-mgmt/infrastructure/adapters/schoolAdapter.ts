386a45d9c73c3acaa814762615cae0595b89ddba
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-06 11:27:37 +0800

    feature: create addSchool method in schoolAdapter

--- src/modules/school-mgmt/infrastructure/adapters/schoolAdapter.ts
+++ src/modules/school-mgmt/infrastructure/adapters/schoolAdapter.ts
@@ -10,6 +10,26 @@ class SchoolAdapter {
 
 		return schoolList;
 	}
+
+	public async addSchool({ schoolName, id }: { schoolName: string, id: number }): Promise<School> {
+		const response = await fetch(
+				`${process.env.NEXT_PUBLIC_API_URL}/add-school/`,
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						name: schoolName,
+						owner: id,
+					}),
+				}
+			);
+		const data = await response.json();
+		const school: School = data.data
+
+		return school
+	}
  
 }
 
