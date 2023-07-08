01c5c90a53d9ad034f0b4465ccab1870809d12ef
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-08 12:38:29 +0800

    feature: add update report by id to report adapter

--- src/modules/report-mgmt/infrastructure/adapters/reportAdapter.ts
+++ src/modules/report-mgmt/infrastructure/adapters/reportAdapter.ts
@@ -21,11 +21,24 @@ class ReportAdapter {
 		return report;
 	}
 
-	public async getTodayReportByStudentId({student_id}: {student_id: number}) : Promise<Report[]> {
+	public async getTodayReportByStudentId({student_id}: {student_id: number}): Promise<Report[]> {
 		const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/get-today-report-by-student-id/${student_id}`);
 		const report: Report[] = await res.json();
 
 		return report
+	}
+
+	public async updateReportById({report}: {report: Report}): Promise<Report> {		
+		const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/update-report/${report.id}/`, { 
+			method: 'POST', 
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ id: report.id, content: report.content, is_complete: report.is_complete, student_id: report.student_id }) 
+		})
+		const updatedReport: Report = await response.json();
+
+		return updatedReport;
 	}
 }
 
