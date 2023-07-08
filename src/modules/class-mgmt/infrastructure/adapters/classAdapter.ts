debb3da6022050903c9c2e273edf6d4180c2c662
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-08 22:53:33 +0800

    feature: add getClasses method to adapter and fetch request to classes index page

--- src/modules/class-mgmt/infrastructure/adapters/classAdapter.ts
+++ src/modules/class-mgmt/infrastructure/adapters/classAdapter.ts
@@ -1,7 +1,12 @@
 import { Class } from "../../domain/entities/Class";
 
 class ClassAdapter {
-	
+	public async getClasses(): Promise<Class[]> {
+		const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/get-classes/`);
+		const classes: Class[] = await res.json();
+
+		return classes
+	}
 }
 
 export const classAdapter = new ClassAdapter();
--- src/pages/class-mgmt/index.tsx
+++ src/pages/class-mgmt/index.tsx
@@ -1,6 +1,21 @@
+import { Class } from "@/src/modules/class-mgmt/domain/entities/Class";
+import { classAdapter } from "@/src/modules/class-mgmt/infrastructure/adapters/classAdapter";
 import Layout from "@/src/modules/core/infrastructure/ui/components/Layout";
+import { useEffect, useState } from "react";
 
 export default function ClassHome() {
+  const [classes, setClasses] = useState<Class[]>([]);
+
+  useEffect(() => {
+    async function getData() {
+      await classAdapter.getClasses().then((res) => {
+        console.log(res)
+        setClasses(res);
+      });
+    }
+
+    getData();
+  }, [])
 
   return (
     <Layout>
@@ -8,6 +23,11 @@ export default function ClassHome() {
         <h1 className="mb-4 p-4">Create classes here</h1>
         <section className="bg-white p-4 rounded-lg">
           <h2>Class time!</h2>
+          <ul>
+            {classes?.map((classes: Class, index: number) => (
+              <li key={index}>{classes.name}</li>
+            ))}
+          </ul>
         </section>
       </div>
     </Layout>
