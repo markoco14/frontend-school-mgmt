3bdebebd1a926c7d4e2197b7a17db56dec82b3ef
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-10 09:01:24 +0800

    feature: use StudentList component in Class List By Id page

--- src/pages/class-mgmt/[class_id]/index.tsx
+++ src/pages/class-mgmt/[class_id]/index.tsx
@@ -1,6 +1,7 @@
 import Layout from "@/src/modules/core/infrastructure/ui/components/Layout";
 import { Student } from "@/src/modules/student-mgmt/domain/entities/Student";
 import { studentAdapter } from "@/src/modules/student-mgmt/infrastructure/adapters/studentAdapter";
+import StudentList from "@/src/modules/student-mgmt/infrastructure/ui/StudentList";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import Link from "next/link";
 
@@ -25,14 +26,14 @@ export default function ClassList({
         <h1 className="mb-4 p-4">Create classes here</h1>
         <section className="bg-white p-4 rounded-lg">
           <div className="flex justify-between items-baseline mb-4">
-            <h2 className='text-3xl'>Class time!</h2>
+            <h2 className='text-3xl'>Class name goes here</h2>
             <Link href="/class-mgmt">Back</Link>
           </div>
-          <ul>
-            {students?.map((student: Student, index: number) => (
-              <li key={index}>{student.first_name} {student.last_name}</li>
-            ))}
-          </ul>
+          <div className="flex items-baseline gap-4 mb-4">
+            <p className="text-xl">Student List</p>
+            <button onClick={() => console.log('adding student')}>+ Student</button>
+          </div>
+          <StudentList students={students}/>
         </section>
       </div>
     </Layout>
