10f4742126cc083ffabbc839e60d3357fb4e6c61
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-12 10:03:50 +0800

    feature: add getReportDetailsByReportId method to reportDetailAdapter

--- src/modules/report-mgmt/infrastructure/adapters/reportDetailAdapter.ts
+++ src/modules/report-mgmt/infrastructure/adapters/reportDetailAdapter.ts
@@ -1,7 +1,12 @@
 import { ReportDetail } from "../../domain/entities/ReportDetail";
 
 class ReportDetailAdapter {
-	
+	public async getReportDetailsByReportId({id}: {id: number}): Promise<ReportDetail[]> {
+		const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/get-report-details-by-report-id/${id}/`);
+		const reportDetails: ReportDetail[] = await res.json();
+
+		return reportDetails
+	}
 }
 
 export const reportDetailAdapter = new ReportDetailAdapter();
