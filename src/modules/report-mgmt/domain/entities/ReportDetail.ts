bd6e2a0d4bfe9207bcbf66b9a7b3815b3f291850
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-12 10:01:40 +0800

    feature: add 'student_id: Student' to ReportDetail entity

--- src/modules/report-mgmt/domain/entities/ReportDetail.ts
+++ src/modules/report-mgmt/domain/entities/ReportDetail.ts
@@ -1,5 +1,10 @@
+import { Student } from "@/src/modules/student-mgmt/domain/entities/Student";
+
 export class ReportDetail {
 	constructor(
+		public id: number,
+		public report_id: number,
 		public content: string,
+		public student_id: Student,
 	) {}
 }
