03bfcdc18ac81fa3f27f8eb24a82e0394b2ee2e7
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-12 11:03:57 +0800

    feature: add updateReportDetailByReportId method to reportDetailAdapter

--- src/modules/report-mgmt/infrastructure/adapters/reportDetailAdapter.ts
+++ src/modules/report-mgmt/infrastructure/adapters/reportDetailAdapter.ts
@@ -7,6 +7,20 @@ class ReportDetailAdapter {
 
 		return reportDetails
 	}
+
+	public async updateReportDetailById({id, content, is_complete}: {id: number, content: string, is_complete: number}): Promise<ReportDetail> {
+		console.log('hitting endpoint')		
+		const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/update-report-detail-by-report-id/${id}/`, { 
+			method: 'POST', 
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ id: id, content: content, is_complete: is_complete }) 
+		})
+		const updatedReport: ReportDetail = await response.json();
+
+		return updatedReport;
+	}
 }
 
 export const reportDetailAdapter = new ReportDetailAdapter();
