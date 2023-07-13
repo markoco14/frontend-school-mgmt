4f2faa6bd3d94181cd98e1b497cf990868d327a1
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-13 09:08:22 +0800

    WIP: set up create report details

--- src/modules/report-mgmt/infrastructure/adapters/reportDetailAdapter.ts
+++ src/modules/report-mgmt/infrastructure/adapters/reportDetailAdapter.ts
@@ -1,11 +1,20 @@
 import { ReportDetail } from "../../domain/entities/ReportDetail";
 
 class ReportDetailAdapter {
+
 	public async getReportDetailsByReportId({id}: {id: number}): Promise<ReportDetail[]> {
 		const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/get-report-details-by-report-id/${id}/`);
 		const reportDetails: ReportDetail[] = await res.json();
-
+		
 		return reportDetails
+	}
+
+	public async createReportDetails({student_id, report_id}: {student_id: number, report_id: number}): Promise<ReportDetail> {
+		console.log(`creating report detail for report ${report_id} student ${student_id}`)
+		// const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/get-report-details-by-report-id/${id}/`);
+		// const reportDetails: ReportDetail = await res.json();
+		
+		// return reportDetails
 	}
 
 	public async updateReportDetailById({id, content}: {id: number, content: string}): Promise<ReportDetail> {
--- src/pages/report-mgmt/index.tsx
+++ src/pages/report-mgmt/index.tsx
@@ -1,18 +1,21 @@
-import Layout from '@/src/modules/core/infrastructure/ui/components/Layout';
+import Layout from "@/src/modules/core/infrastructure/ui/components/Layout";
 import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
-import { reportAdapter } from '@/src/modules/report-mgmt/infrastructure/adapters/reportAdapter';
-import { Report } from '@/src/modules/report-mgmt/domain/entities/Report';
-import { useEffect, useState } from 'react';
-import Link from 'next/link';
-import { classAdapter } from '@/src/modules/class-mgmt/infrastructure/adapters/classAdapter';
-import { Class } from '@/src/modules/class-mgmt/domain/entities/Class';
-import { format } from 'date-fns';
-import { useRouter } from 'next/router';
+import { reportAdapter } from "@/src/modules/report-mgmt/infrastructure/adapters/reportAdapter";
+import { Report } from "@/src/modules/report-mgmt/domain/entities/Report";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { classAdapter } from "@/src/modules/class-mgmt/infrastructure/adapters/classAdapter";
+import { Class } from "@/src/modules/class-mgmt/domain/entities/Class";
+import { format } from "date-fns";
+import { useRouter } from "next/router";
+import toast from "react-hot-toast";
+import { studentAdapter } from "@/src/modules/student-mgmt/infrastructure/adapters/studentAdapter";
+import { reportDetailAdapter } from "@/src/modules/report-mgmt/infrastructure/adapters/reportDetailAdapter";
 
 export const getServerSideProps: GetServerSideProps<{
   reports: Report[];
 }> = async () => {
-  // TODO: CHANGE THIS TO 
+  // TODO: CHANGE THIS TO
   // GET REPORTS AND THEIR ACCOMPANYING CLASS NAMES
   // THIS WAY GET THE REPORT STATUS
   // AND CAN RENDER DIFFERENT BUTTONS
@@ -26,44 +29,62 @@ export default function ReportsHome({
   reports,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const router = useRouter();
-  const [day, setDay] = useState<string>('');
-  const [date, setDate] = useState<string>('');
-  const [dates, setDates] = useState<string[]>(['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']);
+  const [day, setDay] = useState<string>("");
+  const [date, setDate] = useState<string>("");
+  const [dates, setDates] = useState<string[]>([
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ]);
   const [classes, setClasses] = useState<Class[]>();
   useEffect(() => {
     async function getData() {
-      await classAdapter.getClasses().then((res) => setClasses(res))
+      await classAdapter.getClasses().then((res) => {
+        setClasses(res);
+        console.log(res);
+      });
     }
 
     if (!day) {
       const date = new Date();
       setDay(dates[date.getDay()]);
-      setDate(format(date, 'yyyy-MM-dd'))
+      setDate(format(date, "yyyy-MM-dd"));
     }
-    
-    getData();
 
-  }, [day, dates])
+    getData();
+  }, [day, dates]);
 
   async function checkOrCreateReports(thisClass: Class, date: string) {
     // CHECK IF REPORT EXISTS
-    const report = await reportAdapter.getReportByClassAndDate({class_id: thisClass.id, date: date}).then((res) => {
-      // IF EXISTS, GO THERE
-      if (res.id) {
-        console.log('report exists, going now')
-        console.log(res)
-        router.push(`report-mgmt/report-details/${res.id}/`)
-      } 
-      // IF NO REPORTS, CREATE AND GO
-      else {
-        console.log('report does not exist, creating now')
-        reportAdapter.createReportForClassAndDate({class_id: thisClass.id, date: date}).then((res) => {
-          console.log('report created, going now')
-          console.log(res)
-          router.push(`report-mgmt/report-details/${res.id}/`)
-        });
-      }
-    })
+    const report = await reportAdapter
+      .getReportByClassAndDate({ class_id: thisClass.id, date: date })
+      .then((res) => {
+        // IF EXISTS, GO THERE
+        if (res.id) {
+          console.log("report exists, going now");
+          console.log(res);
+          router.push(`report-mgmt/report-details/${res.id}/`);
+        }
+        // IF NO REPORTS, CREATE AND GO
+        else {
+          // return;
+          console.log("report does not exist, creating now");
+          reportAdapter
+            .createReportForClassAndDate({ class_id: thisClass.id, date: date })
+            .then((res) => {
+              console.log("report created, going now");
+              console.log(res);
+              thisClass.class_list.forEach((student, index) => {
+                reportDetailAdapter.createReportDetails({student_id: student.id, report_id: res.id})
+              })
+              router.push(`report-mgmt/report-details/${res.id}/`);
+            });
+        }
+      });
   }
 
   return (
@@ -71,37 +92,53 @@ export default function ReportsHome({
       <div>
         <h1 className="mb-4 p-4">Write daily reports here.</h1>
         <section className="bg-white p-4 rounded-lg">
-          <h2 className='flex justify-between gap-4 items-baseline text-3xl mb-4'>Reports for {day} <span><input 
-              type="date"
-              className='mb-4 text-xl text-right'
-              defaultValue={format(new Date, 'yyyy-MM-dd')}
-              onChange={(e) => {
-                const dateObject = new Date(e.target.value);
-                const newDay = dates[dateObject.getDay()]
-                setDay(newDay)
-                setDate(format(dateObject, 'yyyy-MM-dd'))
-              }}
-            /></span></h2>
+          <h2 className="flex justify-between gap-4 items-baseline text-3xl mb-4">
+            Reports for {day}{" "}
+            <span>
+              <input
+                type="date"
+                className="mb-4 text-xl text-right"
+                defaultValue={format(new Date(), "yyyy-MM-dd")}
+                onChange={(e) => {
+                  const dateObject = new Date(e.target.value);
+                  const newDay = dates[dateObject.getDay()];
+                  setDay(newDay);
+                  setDate(format(dateObject, "yyyy-MM-dd"));
+                }}
+              />
+            </span>
+          </h2>
           <ul className="flex flex-col gap-2">
             {classes?.map((thisClass: Class, index: number) => (
-              <li 
+              <li
                 key={index}
                 className="p-2 rounded-md hover:bg-blue-200 flex justify-between"
               >
                 <Link href={`/report-mgmt/${thisClass.id}/${date}`}>
-                  {thisClass.name}
+                  {thisClass.name} {thisClass.class_list.length}/8
                 </Link>
-                <button
-                  className='bg-blue-300 py-1 px-2 rounded'
-                  onClick={() => {
-                    checkOrCreateReports(thisClass, date);
-                  }}
-                >Write reports</button>
+                <div className="flex gap-4">
+                  {thisClass.class_list.length === 0 ? (
+                    <Link href={`/class-mgmt/${thisClass.id}`}
+                      className="bg-gray-300 py-1 px-2 rounded disabled:cursor-not-allowed"
+                    >Add Students</Link>
+                  ) : (
+                    <button
+                      disabled={thisClass.class_list.length === 0}
+                      className="bg-blue-300 py-1 px-2 rounded disabled:cursor-not-allowed"
+                      onClick={() => {
+                        checkOrCreateReports(thisClass, date);
+                      }}
+                    >
+                      Write reports
+                    </button>
+                  )}
+                </div>
               </li>
             ))}
           </ul>
         </section>
       </div>
     </Layout>
-  )
+  );
 }
