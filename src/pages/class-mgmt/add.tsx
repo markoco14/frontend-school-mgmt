8d5bf52d4f34d6e472a1f7aa81e465cb6ffa59d6
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-16 12:45:59 +0800

    chore: format create class page with prettier

--- src/pages/class-mgmt/add.tsx
+++ src/pages/class-mgmt/add.tsx
@@ -10,37 +10,42 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 
 type Inputs = {
-	className: string;
-	schoolId: number;
+  className: string;
+  schoolId: number;
   level: number;
   daysOfWeek: string[];
-}
+};
 
-export default function AddClass() {  
-	const { user } = useContext(AuthContext);
-	const [loading, setLoading] = useState<boolean>(false);
-	const [userSchools, setUserSchools] = useState<School[]>([]);
-	const { reset, register, handleSubmit, formState: { errors }} = useForm<Inputs>();
+export default function AddClass() {
+  const { user } = useContext(AuthContext);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [userSchools, setUserSchools] = useState<School[]>([]);
+  const {
+    reset,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
 
-	const onSubmit: SubmitHandler<Inputs> = async (data) => {
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
     const days: number[] = [];
-    data.daysOfWeek.forEach((day) => days.push(Number(day)))
+    data.daysOfWeek.forEach((day) => days.push(Number(day)));
 
     try {
       const newClass: Class = await classAdapter.addClass({
         name: data.className,
         school_id: Number(data.schoolId),
         level: Number(data.level),
-        day: days
+        day: days,
       });
-      toast.success('New class added.');
+      toast.success("New class added.");
       reset();
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   };
 
-	useEffect(() => {
+  useEffect(() => {
     async function getSchoolsByOwnerId(id: number) {
       setLoading(true);
       await schoolAdapter.getSchoolsByOwnerId({ id: id }).then((res) => {
@@ -57,19 +62,19 @@ export default function AddClass() {
       }
     }
   }, [user]);
-	
+
   return (
     <Layout>
       <div>
         <h1 className="mb-4 p-4">Create classes here</h1>
         <section className="bg-white p-4 rounded-lg">
           <div className="flex justify-between items-baseline mb-4">
-            <h2 className='text-3xl'>Add Class</h2>
+            <h2 className="text-3xl">Add Class</h2>
             <Link href="/class-mgmt">Back</Link>
           </div>
-					<form onSubmit={handleSubmit(onSubmit)}>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className="flex flex-col mb-4">
-              <label className='mb-2'>School</label>
+              <label className="mb-2">School</label>
               <select
                 className="py-2 rounded bg-white shadow"
                 {...register("schoolId", { required: true })}
@@ -82,86 +87,84 @@ export default function AddClass() {
                 ))}
               </select>
               {errors.schoolId?.type === "required" && (
-                <p 
-                  role="alert"
-                  className='text-red-500 mt-2'
-                >
+                <p role="alert" className="text-red-500 mt-2">
                   School is required
                 </p>
               )}
             </div>
             <div className="flex flex-col mb-4">
-              <label className='mb-2'>
-								Class Name
-							</label>
+              <label className="mb-2">Class Name</label>
               <input
                 className="px-1 py-2 rounded shadow"
                 type="text"
-                {...register("className", { required: true, minLength: 2, maxLength: 50 })}
+                {...register("className", {
+                  required: true,
+                  minLength: 2,
+                  maxLength: 50,
+                })}
               />
               {errors.className?.type === "required" && (
-                <p 
-                  role="alert"
-                  className='text-red-500 mt-2'
-                >
+                <p role="alert" className="text-red-500 mt-2">
                   Class name is required
                 </p>
               )}
             </div>
             <div className="flex flex-col mb-4">
-              <label className='mb-2'>
-								Level
-							</label>
+              <label className="mb-2">Level</label>
               <input
                 className="px-1 py-2 rounded shadow"
                 type="text"
                 {...register("level", { required: true })}
               />
               {errors.level?.type === "required" && (
-                <p 
-                  role="alert"
-                  className='text-red-500 mt-2'
-                >
+                <p role="alert" className="text-red-500 mt-2">
                   Class level is required
                 </p>
               )}
             </div>
             <div className="grid grid-cols-5 mb-4">
               <div className="flex flex-col">
-                <label className="text-center">
-                  Monday
-                </label>
-                <input type="checkbox" {...register("daysOfWeek", { required: true })} value={1} />
+                <label className="text-center">Monday</label>
+                <input
+                  type="checkbox"
+                  {...register("daysOfWeek", { required: true })}
+                  value={1}
+                />
               </div>
               <div className="flex flex-col">
-                <label className="text-center">
-                  Tuesday
-                </label>
-                <input type="checkbox" {...register("daysOfWeek", { required: true })} value={2} />
+                <label className="text-center">Tuesday</label>
+                <input
+                  type="checkbox"
+                  {...register("daysOfWeek", { required: true })}
+                  value={2}
+                />
               </div>
               <div className="flex flex-col">
-                <label className="text-center">
-                  Wednesday
-                </label>
-                <input type="checkbox" {...register("daysOfWeek", { required: true })} value={3} />
+                <label className="text-center">Wednesday</label>
+                <input
+                  type="checkbox"
+                  {...register("daysOfWeek", { required: true })}
+                  value={3}
+                />
               </div>
               <div className="flex flex-col">
-                <label className="text-center">
-                  Thursday
-                </label>
-                <input type="checkbox" {...register("daysOfWeek", { required: true })} value={4} />
+                <label className="text-center">Thursday</label>
+                <input
+                  type="checkbox"
+                  {...register("daysOfWeek", { required: true })}
+                  value={4}
+                />
               </div>
               <div className="flex flex-col">
-                <label className="text-center">
-                  Friday
-                </label>
-                <input type="checkbox" {...register("daysOfWeek", { required: true })} value={5} />
+                <label className="text-center">Friday</label>
+                <input
+                  type="checkbox"
+                  {...register("daysOfWeek", { required: true })}
+                  value={5}
+                />
               </div>
               {errors.daysOfWeek?.type === "required" && (
-                <p 
-                  role="alert"
-                  className='text-red-500 mt-2 col-span-5'
-                >
+                <p role="alert" className="text-red-500 mt-2 col-span-5">
                   Days are required
                 </p>
               )}
