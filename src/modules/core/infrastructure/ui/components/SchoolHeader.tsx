dc7f5ae6be2b4def7cccad36ea8d12b3c28ac08e
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-17 12:18:27 +0800

    feature: init SchoolHeader component to display selectedSchool name

---
+++ src/modules/core/infrastructure/ui/components/SchoolHeader.tsx
@@ -0,0 +1,9 @@
+import AuthContext from "@/src/AuthContext";
+import { useContext } from "react";
+
+export default function SchoolHeader() {
+  const { selectedSchool } = useContext(AuthContext);
+  return (
+    <p className="text-xl text-gray-500 mb-4">{selectedSchool.name}</p>
+  );
+}
