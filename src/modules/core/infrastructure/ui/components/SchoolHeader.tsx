4b723dee90758a92105a02561d0592a914195651
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-17 12:41:25 +0800

    feature: change to selectedSchool?.id in SchoolHeader component

--- src/modules/core/infrastructure/ui/components/SchoolHeader.tsx
+++ src/modules/core/infrastructure/ui/components/SchoolHeader.tsx
@@ -4,6 +4,6 @@ import { useContext } from "react";
 export default function SchoolHeader() {
   const { selectedSchool } = useContext(AuthContext);
   return (
-    <p className="text-xl text-gray-500 mb-4">{selectedSchool.name}</p>
+    <p className="text-xl text-gray-500 mb-4">{selectedSchool?.name}</p>
   );
 }
