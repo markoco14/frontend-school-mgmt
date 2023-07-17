d38b96591b3fd7d4879d6d7f3eb736294579ee4d
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-17 12:45:50 +0800

    feature: remove school selector from add class form

--- src/pages/class-mgmt/add.tsx
+++ src/pages/class-mgmt/add.tsx
@@ -3,8 +3,7 @@ import { Class } from "@/src/modules/class-mgmt/domain/entities/Class";
 import { Level } from "@/src/modules/class-mgmt/domain/entities/Level";
 import { classAdapter } from "@/src/modules/class-mgmt/infrastructure/adapters/classAdapter";
 import Layout from "@/src/modules/core/infrastructure/ui/components/Layout";
-import { School } from "@/src/modules/school-mgmt/domain/entities/School";
-import { schoolAdapter } from "@/src/modules/school-mgmt/infrastructure/adapters/schoolAdapter";
+import SchoolHeader from "@/src/modules/core/infrastructure/ui/components/SchoolHeader";
 import Link from "next/link";
 import { useContext, useEffect, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
@@ -12,15 +11,12 @@ import toast from "react-hot-toast";
 
 type Inputs = {
   className: string;
-  schoolId: number;
   level: number;
   daysOfWeek: string[];
 };
 
 export default function AddClass() {
-  const { user } = useContext(AuthContext);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [userSchools, setUserSchools] = useState<School[]>([]);
+  const { user, selectedSchool } = useContext(AuthContext);
   const [levels, setLevels] = useState<Level[]>([]);
   const {
     reset,
@@ -36,7 +32,7 @@ export default function AddClass() {
     try {
       const newClass: Class = await classAdapter.addClass({
         name: data.className,
-        school_id: Number(data.schoolId),
+        school_id: selectedSchool.id,
         level: Number(data.level),
         day: days,
       });
@@ -48,26 +44,15 @@ export default function AddClass() {
   };
 
   useEffect(() => {
-    async function getSchoolsByOwnerId(id: number) {
-      setLoading(true);
-      await schoolAdapter.getSchoolsByOwnerId({ id: id }).then((res) => {
-        setUserSchools(res);
-        setLoading(false);
-      });
-    }
-
     async function getLevels() {
-      setLoading(true);
       await classAdapter.getLevels().then((res) => {
         console.log(res)
         setLevels(res);
-        setLoading(false);
       });
     }
 
     if (user) {
       try {
-        getSchoolsByOwnerId(user.user_id);
         getLevels();
       } catch (error) {
         console.error(error);
@@ -80,30 +65,12 @@ export default function AddClass() {
       <div>
         <h1 className="mb-4 p-4">Create classes here</h1>
         <section className="bg-white p-4 rounded-lg">
+          <SchoolHeader />
           <div className="flex justify-between items-baseline mb-4">
             <h2 className="text-3xl">Add Class</h2>
             <Link href="/class-mgmt">Back</Link>
           </div>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="flex flex-col mb-4">
-              <label className="mb-2">School</label>
-              <select
-                className="py-2 rounded bg-white shadow"
-                {...register("schoolId", { required: true })}
-              >
-                <option value="">Choose a school</option>
-                {userSchools?.map((school: School, index: number) => (
-                  <option key={index} value={school.id} className="p-4">
-                    {school.name}
-                  </option>
-                ))}
-              </select>
-              {errors.schoolId?.type === "required" && (
-                <p role="alert" className="text-red-500 mt-2">
-                  School is required
-                </p>
-              )}
-            </div>
             <div className="flex flex-col mb-4">
               <label className="mb-2">Class Name</label>
               <input
@@ -121,19 +88,6 @@ export default function AddClass() {
                 </p>
               )}
             </div>
-            {/* <div className="flex flex-col mb-4">
-              <label className="mb-2">Level</label>
-              <input
-                className="px-1 py-2 rounded shadow"
-                type="text"
-                {...register("level", { required: true })}
-              />
-              {errors.level?.type === "required" && (
-                <p role="alert" className="text-red-500 mt-2">
-                  Class level is required
-                </p>
-              )}
-            </div> */}
             <div className="flex flex-col mb-4">
               <label className="mb-2">Level</label>
               <select
