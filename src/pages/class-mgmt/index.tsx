74b3b427a955ce96a06473cf65823b219143b1d0
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-17 12:42:10 +0800

    feature: change classes list to classesBySchoolId in class-mgmt index page

--- src/pages/class-mgmt/index.tsx
+++ src/pages/class-mgmt/index.tsx
@@ -1,27 +1,33 @@
+import AuthContext from "@/src/AuthContext";
 import { Class } from "@/src/modules/class-mgmt/domain/entities/Class";
 import { classAdapter } from "@/src/modules/class-mgmt/infrastructure/adapters/classAdapter";
 import Layout from "@/src/modules/core/infrastructure/ui/components/Layout";
+import SchoolHeader from "@/src/modules/core/infrastructure/ui/components/SchoolHeader";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 export default function ClassHome() {
+  const { selectedSchool } = useContext(AuthContext);
   const [classes, setClasses] = useState<Class[]>([]);
 
   useEffect(() => {
     async function getData() {
-      await classAdapter.getClasses().then((res) => {
-        setClasses(res);
-      });
+      if (selectedSchool) {
+        await classAdapter.getClassesBySchoolId({id: selectedSchool.id}).then((res) => {
+          setClasses(res);
+        });
+      }
     }
 
     getData();
-  }, [])
+  }, [selectedSchool])
 
   return (
     <Layout>
       <div>
         <h1 className="mb-4 p-4">Create classes here</h1>
         <section className="bg-white p-4 rounded-lg">
+          <SchoolHeader />
           <div className="flex justify-between items-baseline mb-4">
             <h2 className='text-3xl'>Class time!</h2>
             <Link href="class-mgmt/add">Add</Link>
