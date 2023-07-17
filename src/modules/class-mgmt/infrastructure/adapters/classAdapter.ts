486024843bac9dd87b33794753c09415304564fe
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-17 12:40:50 +0800

    feature: add getClassesBySchoolId method in classAdapter

--- src/modules/class-mgmt/infrastructure/adapters/classAdapter.ts
+++ src/modules/class-mgmt/infrastructure/adapters/classAdapter.ts
@@ -15,8 +15,16 @@ class ClassAdapter {
 		return classes
 	}
 
+	public async getClassesBySchoolId({id}: {id: number}): Promise<Class[]> {
+		console.log(id)
+		const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/get-classes-by-school-id/${id}/`);
+		const classes: Class[] = await res.json();
+
+		return classes
+	}
+
 	public async getClassById({id}: {id: number}): Promise<Class> {
-		const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/get-class-by-id/${id}`);
+		const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/get-class-by-id/${id}/`);
 		const thisClass: Class = await res.json();
 		
 		return thisClass
