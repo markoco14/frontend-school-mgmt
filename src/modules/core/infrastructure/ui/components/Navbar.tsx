9a08052ff03541aeab64ad36138f6c287a68f7b9
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-17 11:57:54 +0800

    feature: use school or no school as condition to render nav links

--- src/modules/core/infrastructure/ui/components/Navbar.tsx
+++ src/modules/core/infrastructure/ui/components/Navbar.tsx
@@ -6,16 +6,15 @@ import { useContext } from "react";
 
 export default function Navbar() {
   const context = useContext(UserContext);
-  const { user, logout } = useContext(AuthContext);
+  const { user, school, logout } = useContext(AuthContext);
   const router = useRouter();
   return (
     <nav className="h-[48px] flex justify-between items-center px-4">
       <div className="flex gap-2">
         <Link href="/">Home</Link>
-        {user ? <Link href="/school-mgmt/">Schools</Link> : null}
-        {user ? <Link href="/class-mgmt/">Classes</Link> : null}
-        {user ? <Link href="/student-mgmt/">Students</Link> : null}
-        {user ? <Link href="/report-mgmt/">Reports</Link> : null}
+        {user && school ? <Link href="/class-mgmt/">Classes</Link> : null}
+        {user && school ? <Link href="/student-mgmt/">Students</Link> : null}
+        {user && school ? <Link href="/report-mgmt/">Reports</Link> : null}
       </div>
       {user ? (
         <button
