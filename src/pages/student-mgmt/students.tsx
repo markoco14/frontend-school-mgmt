c078acf1e3f949e1162ff503c60115a3acd4d330
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-23 16:17:47 +0800

    feature: remove school selector from student list page and change to getStudentsBySchool request

--- src/pages/student-mgmt/students.tsx
+++ src/pages/student-mgmt/students.tsx
@@ -12,21 +12,18 @@ import { useContext, useEffect, useState } from "react";
 
 
 export default function ListStudents() {
-  const { user } = useContext(AuthContext);
+  const { user, selectedSchool } = useContext(AuthContext);
   const [loading, setLoading] = useState<boolean>(false);
   const [filteredStudents, setFilteredStudents] = useState<Student[]>();
-  const [userSchools, setUserSchools] = useState<School[]>();
 
   useEffect(() => {
     async function getData(id: number) {  
       setLoading(true);
-      await schoolAdapter.getSchoolsByOwnerId({ id: id }).then((res) => {
-        setUserSchools(res);
-      });
-
-      await studentAdapter.getStudentsByOwnerId({ id: id }).then((res) => {
-        setFilteredStudents(res);
-      });
+      if (selectedSchool) {
+        await studentAdapter.getStudentsBySchool({ id: selectedSchool.id }).then((res) => {
+          setFilteredStudents(res);
+        });
+      }
       setLoading(false);
     }
   
@@ -37,21 +34,7 @@ export default function ListStudents() {
         console.error(error);
       }
     }
-  }, [user]);
-
-  async function handleChangeSelectedSchool(id: number) {
-    if (id === 0 && user) {
-      await studentAdapter.getStudentsByOwnerId({ id: user.user_id }).then((res) => {
-        setFilteredStudents(res);
-      });
-      return;
-    }
-
-    await studentAdapter.getStudentsBySchoolId({id: id }).then((res) => {
-      setFilteredStudents(res);
-      return
-    });
-  }
+  }, [user, selectedSchool]);
 
   return (
     <Layout>
@@ -61,19 +44,6 @@ export default function ListStudents() {
           <h2 className="text-3xl">Click student to see profile</h2>
           <Link href="/student-mgmt/">Back</Link>
         </div>
-        <select
-          className="py-2 rounded bg-white shadow"
-          onChange={(e) => {
-            handleChangeSelectedSchool(Number(e.target.value));
-          }}
-        >
-          <option value="0">All</option>
-          {userSchools?.map((school: School, index: number) => (
-            <option key={index} value={school.id} className="p-4">
-              {school.name}
-            </option>
-          ))}
-        </select>
         <ul>
           {filteredStudents?.map((student: Student, index) => (
             <li key={index} className="flex justify-between gap-4">
