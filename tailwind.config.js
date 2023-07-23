c6f8b27e342a8230ec107688a20f1d4804797f1b
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-23 11:54:51 +0800

    feature: add xs breakpoint for mobile device styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      'xs': '500px',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
