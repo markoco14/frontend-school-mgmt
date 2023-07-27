220b7256a9b988b54fe3de3a1d004320abd50589
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-27 16:28:38 +0800

    feature: implement TeacherSignup form in Admin page

--- src/pages/school-mgmt/index.tsx
+++ src/pages/school-mgmt/index.tsx
@@ -3,6 +3,7 @@ import { Level } from "@/src/modules/class-mgmt/domain/entities/Level";
 import { classAdapter } from "@/src/modules/class-mgmt/infrastructure/adapters/classAdapter";
 import Layout from "@/src/modules/core/infrastructure/ui/components/Layout";
 import SchoolHeader from "@/src/modules/core/infrastructure/ui/components/SchoolHeader";
+import TeacherSignup from "@/src/modules/user-mgmt/infrastructure/ui/TeacherSignup";
 import { useContext, useEffect, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import toast from "react-hot-toast";
@@ -11,7 +12,7 @@ type Inputs = {
   levelName: string;
 }
 
-export default function Home() {
+export default function AdminHome() {
   const { selectedSchool } = useContext(AuthContext);
   const [loading, setLoading] = useState<boolean>(false);
   const [levels, setLevels] = useState<Level[]>([]);
@@ -56,63 +57,69 @@ export default function Home() {
 
   return (
     <Layout>
-      <div>
+      <div className="grid gap-4">
         {loading ? (
           <p className="flex justify-center bg-white p-4 rounded-lg">
             loading...
           </p>
         ) : (
-          <section>
-            <SchoolHeader />
-            <article className="relative xs:grid xs:grid-cols-3 xs:gap-6">
-              <div className="mb-4 xs:mb-0 xs:col-span-2">
-                <h2 className="text-2xl">Levels</h2>
-                <ul>
-                  {levels?.map((level, index) => (
-                    <li 
-                      key={index}
-                      className='flex justify-between'
-                    >
-                      <span>{level.name}</span>
-                      <button 
-                        onClick={async() => {
-                          handleDeleteLevel(level.id);
-                        }}
+          <>
+            <section>
+              <SchoolHeader />
+              <article className="relative xs:grid xs:grid-cols-3 xs:gap-6">
+                <div className="mb-4 xs:mb-0 xs:col-span-2">
+                  <h2 className="text-2xl">Levels</h2>
+                  <ul>
+                    {levels?.map((level, index) => (
+                      <li 
+                        key={index}
+                        className='flex justify-between'
                       >
-                        delete
-                      </button>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <div className="xs:col-span-1">
-                <h2 className="text-2xl">Add new level</h2>
-                <form 
-                  onSubmit={handleSubmit(onSubmit)}
-                >
-                  <div className="flex flex-col mb-4">
-                    <label>Name</label>
-                    <input 
-                      className="shadow p-2"
-                      type="text" 
-                      {...register("levelName", {required: true, minLength: 1, maxLength: 50})}
-                    />
-                    {errors.levelName?.type === "required" && (
-                      <p 
-                        role="alert"
-                        className='text-red-500 mt-2'
-                      >
-                        Level name is required
-                      </p>
-                    )}
-                  </div>
-                  <button className="bg-blue-300 px-2 py-1 rounded text-blue-900">
-                    Submit
-                  </button>
-                </form>
-              </div>
-            </article>
-          </section>
+                        <span>{level.name}</span>
+                        <button 
+                          onClick={async() => {
+                            handleDeleteLevel(level.id);
+                          }}
+                        >
+                          delete
+                        </button>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                <div className="xs:col-span-1">
+                  <h2 className="text-2xl">Add new level</h2>
+                  <form 
+                    onSubmit={handleSubmit(onSubmit)}
+                  >
+                    <div className="flex flex-col mb-4">
+                      <label>Name</label>
+                      <input 
+                        className="shadow p-2"
+                        type="text" 
+                        {...register("levelName", {required: true, minLength: 1, maxLength: 50})}
+                      />
+                      {errors.levelName?.type === "required" && (
+                        <p 
+                          role="alert"
+                          className='text-red-500 mt-2'
+                        >
+                          Level name is required
+                        </p>
+                      )}
+                    </div>
+                    <button className="bg-blue-300 px-2 py-1 rounded text-blue-900">
+                      Submit
+                    </button>
+                  </form>
+                </div>
+              </article>
+            </section>
+            <section>
+              <h2>Teachers</h2>
+              <TeacherSignup />
+            </section>
+          </>
         )}
       </div>
     </Layout>
