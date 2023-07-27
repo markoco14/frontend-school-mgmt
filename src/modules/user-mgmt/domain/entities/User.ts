5e67fe8661b7e62e2d6dbbcc1a970b5c5835605c
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-27 16:27:23 +0800

    chore: format User entity

--- src/modules/user-mgmt/domain/entities/User.ts
+++ src/modules/user-mgmt/domain/entities/User.ts
@@ -1,3 +1,8 @@
 export class User {
-	constructor(public first_name: string, public last_name: string, public id: number, public email: string) {}
+  constructor(
+    public first_name: string,
+    public last_name: string,
+    public id: number,
+    public email: string
+  ) {}
 }
