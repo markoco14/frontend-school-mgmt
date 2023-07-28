779044c80397ac61e793d032f5b5b7b3c79e9e39
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-28 22:38:23 +0800

    refactor: create LevelList component for admin Level sectin

---
+++ src/modules/core/infrastructure/ui/components/LevelList.tsx
@@ -0,0 +1,28 @@
+import { Level } from "@/src/modules/class-mgmt/domain/entities/Level";
+
+export default function LevelSection({levels, handleDeleteLevel}: {levels: Level[], handleDeleteLevel: Function}) {
+  
+  
+  return (
+		<div className="mb-4 xs:mb-0">
+			<h2 className="text-2xl">Levels</h2>
+			<ul className="bg-gray-100 rounded shadow-inner mb-4">
+				{levels?.map((level, index) => (
+					<li 
+						key={index}
+						className="p-2 hover:bg-gray-300 flex justify-between"
+					>
+						<span>{level.name}</span>
+						<button 
+							onClick={async() => {
+								handleDeleteLevel(level.id);
+							}}
+						>
+							delete
+						</button>
+					</li>
+				))}
+			</ul>
+		</div>
+  );
+}
