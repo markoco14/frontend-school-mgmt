62d58ff919c7f8b44a783785aae2c6106794e5f2
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-28 22:38:00 +0800

    refactor: move admin page level section to LevelSection component

--- src/modules/core/infrastructure/ui/components/LevelSection.tsx
+++ src/modules/core/infrastructure/ui/components/LevelSection.tsx
@@ -1,35 +1,18 @@
 import AuthContext from "@/src/AuthContext";
 import { Level } from "@/src/modules/class-mgmt/domain/entities/Level";
 import { classAdapter } from "@/src/modules/class-mgmt/infrastructure/adapters/classAdapter";
-import { useRouter } from "next/router";
 import { useContext, useEffect, useState } from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
 import toast from "react-hot-toast";
-
-type Inputs = {
-  levelName: string;
-}
+import LevelList from "./LevelList";
+import { Dialog, Transition } from "@headlessui/react";
+import AddLevel from "./AddLevel";
 
 export default function LevelSection() {
   const { selectedSchool } = useContext(AuthContext);
-  const router = useRouter();
 
   const [loading, setLoading] = useState<boolean>(false);
   const [levels, setLevels] = useState<Level[]>([]);
-  const { reset, register, handleSubmit, formState: { errors }} = useForm<Inputs>();
-  const { user } = useContext(AuthContext)
-
-  const onSubmit: SubmitHandler<Inputs> = async (data) => {
-    try {
-      await classAdapter.addLevel({name: data.levelName, school: selectedSchool.id}).then((res) => {
-        setLevels(prevLevels => [...prevLevels, res])
-        toast.success('Level added.');
-      });
-      reset();
-    } catch (error) {
-      console.error(error)
-    }
-  };
+  const [ isAddLevel, setIsAddLevel ] = useState<boolean>(false);
 
   useEffect(() => {
     async function getLevelsBySchoolId(id: number) {
@@ -58,53 +41,41 @@ export default function LevelSection() {
   
   return (
     <section>
-      <article className="relative xs:grid xs:grid-cols-3 xs:gap-6">
-        <div className="mb-4 xs:mb-0 xs:col-span-2">
-          <h2 className="text-2xl">Levels</h2>
-          <ul>
-            {levels?.map((level, index) => (
-              <li 
-                key={index}
-                className='flex justify-between'
-              >
-                <span>{level.name}</span>
-                <button 
-                  onClick={async() => {
-                    handleDeleteLevel(level.id);
-                  }}
-                >
-                  delete
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="xs:col-span-1">
-          <h2 className="text-2xl">Add new level</h2>
-          <form 
-            onSubmit={handleSubmit(onSubmit)}
+      <article>
+        <LevelList levels={levels} handleDeleteLevel={handleDeleteLevel}/>
+        <button
+          className="bg-blue-300 text-blue-900 hover:bg-blue-500 hover:text-white px-4 py-1 rounded"
+          onClick={() => setIsAddLevel(true)}
+        >Add Level</button>
+        <Transition
+          show={isAddLevel}
+          enter="transition ease-in duration-100"
+          enterFrom="transform opacity-0 scale-90"
+          enterTo="opacity-100 scale-100"
+          leave="transition ease-out duration-150"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-90"
+        >
+          <Dialog
+            onClose={() => setIsAddLevel(false)}
+            className="fixed inset-0 flex items-center justify-center"
           >
-            <div className="flex flex-col mb-4">
-              <label>Name</label>
-              <input 
-                className="shadow p-2"
-                type="text" 
-                {...register("levelName", {required: true, minLength: 1, maxLength: 50})}
-              />
-              {errors.levelName?.type === "required" && (
-                <p 
-                  role="alert"
-                  className='text-red-500 mt-2'
+            <div className="fixed inset-0 bg-blue-900/25" />
+            <Dialog.Panel className="bg-white rounded-2xl shadow-xl p-8 z-10">
+              <Dialog.Title>Add Level</Dialog.Title>
+              <AddLevel setLevels={setLevels}/>
+              <div className="flex justify-end">
+                <button
+                  type="button"
+                  onClick={() => setIsAddLevel(false)}
+                  className="bg-gray-300 text-gray-900 hover:bg-gray-500 hover:text-white px-4 py-1 rounded"
                 >
-                  Level name is required
-                </p>
-              )}
-            </div>
-            <button className="bg-blue-300 px-2 py-1 rounded text-blue-900">
-              Submit
-            </button>
-          </form>
-        </div>
+                  Cancel
+                </button>
+              </div>
+            </Dialog.Panel>
+          </Dialog>
+        </Transition>
       </article>
     </section>
   );
