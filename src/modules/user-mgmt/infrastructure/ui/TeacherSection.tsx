18b34cb1b985d9836422ba0449de4da5ed272520
Mark O'Connor <mark.oconnor14@gmail.com>
2023-07-28 16:24:52 +0800

    feature: extract admin page teacher section html to component

---
+++ src/modules/user-mgmt/infrastructure/ui/TeacherSection.tsx
@@ -0,0 +1,35 @@
+import { useContext, useEffect, useState } from "react";
+import { userAdapter } from "../adapters/userAdapter";
+import AuthContext from "@/src/AuthContext";
+import TeacherList from "./TeacherList";
+import TeacherSignup from "./TeacherSignup";
+import { Teacher } from "../../domain/entities/Teacher";
+
+
+export default function TeacherSection() {
+	const {user, selectedSchool} = useContext(AuthContext)
+	const [teachers, setTeachers] = useState<Teacher[]>([]);
+
+	useEffect(() => {
+		async function getData(school_id: number, user_id: number) {
+			await userAdapter.getTeachersBySchool({school: school_id, owner: user_id})
+			.then((res) => {
+				console.log(res)
+				setTeachers(res)
+			})
+		}
+		if (user && selectedSchool) {
+			getData(selectedSchool.id, user.user_id)
+		}
+	}, [selectedSchool, user])
+	
+
+  return (
+		
+		<section>
+			<h2>Teachers</h2>
+			<TeacherList teachers={teachers}/>
+			<TeacherSignup setTeachers={setTeachers}/>
+		</section>
+  );
+}
