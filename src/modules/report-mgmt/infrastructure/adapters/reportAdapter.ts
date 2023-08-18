0c735378eb74b3afcebdcac988b208a86e5100b3
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-18 17:57:41 +0800

    refactor: rename url endpoints and functions in report and reportDetails adapters

--- src/modules/report-mgmt/infrastructure/adapters/reportAdapter.ts
+++ src/modules/report-mgmt/infrastructure/adapters/reportAdapter.ts
@@ -9,14 +9,14 @@ class ReportAdapter {
 	}
 
 	public async getReportByClassAndDate({class_id, date}: {class_id: number, date: string}): Promise<Report> {
-		const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/get-report-by-date/${class_id}/${date}/`);
+		const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/classes/${class_id}/reports/date/${date}/`);
 		const report: Report = await res.json();
 		
 		return report
 	}
 
 	public async createReportForClassAndDate({class_id, date}: {class_id: number, date: string}): Promise<Report> {
-		const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/create-report/`, { 
+		const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/reports/create/`, { 
 			method: 'POST', 
 			headers: {
 				"Content-Type": "application/json",
@@ -31,25 +31,25 @@ class ReportAdapter {
 		return report;
 	}
 
-	public async getTodayReportByStudentId({student_id}: {student_id: number}): Promise<Report[]> {
-		const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/get-today-report-by-student-id/${student_id}`);
-		const report: Report[] = await res.json();
+	// public async getTodayReportByStudentId({student_id}: {student_id: number}): Promise<Report[]> {
+	// 	const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/get-today-report-by-student-id/${student_id}`);
+	// 	const report: Report[] = await res.json();
 
-		return report
-	}
+	// 	return report
+	// }
 
-	public async updateReportById({report}: {report: Report}): Promise<Report> {		
-		const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/update-report/${report.id}/`, { 
-			method: 'POST', 
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ id: report.id, is_complete: report.is_complete}) 
-		})
-		const updatedReport: Report = await response.json();
+	// public async updateReportById({report}: {report: Report}): Promise<Report> {		
+	// 	const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/update-report/${report.id}/`, { 
+	// 		method: 'POST', 
+	// 		headers: {
+	// 			"Content-Type": "application/json",
+	// 		},
+	// 		body: JSON.stringify({ id: report.id, is_complete: report.is_complete}) 
+	// 	})
+	// 	const updatedReport: Report = await response.json();
 
-		return updatedReport;
-	}
+	// 	return updatedReport;
+	// }
 }
 
 export const reportAdapter = new ReportAdapter();
--- src/modules/report-mgmt/infrastructure/adapters/reportDetailAdapter.ts
+++ src/modules/report-mgmt/infrastructure/adapters/reportDetailAdapter.ts
@@ -3,30 +3,30 @@ import { ReportDetail } from "../../domain/entities/ReportDetail";
 class ReportDetailAdapter {
 
 	public async getReportDetailsByReportId({id}: {id: number}): Promise<ReportDetail[]> {
-		const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/get-report-details-by-report-id/${id}/`);
+		const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/reports/${id}/details/`);
 		const reportDetails: ReportDetail[] = await res.json();
 		
 		return reportDetails
 	}
 
-	public async createReportDetails({student_id, report_id}: {student_id: number, report_id: number}): Promise<ReportDetail> {
-		const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/create-report-detail`, { 
-			method: 'POST', 
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ 
-				student_id: student_id,
-				report_id: report_id 
-			}) 
-		});
-		const reportDetail: ReportDetail = await res.json();
+	// public async createReportDetails({student_id, report_id}: {student_id: number, report_id: number}): Promise<ReportDetail> {
+	// 	const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/reports/details/create/`, { 
+	// 		method: 'POST', 
+	// 		headers: {
+	// 			"Content-Type": "application/json",
+	// 		},
+	// 		body: JSON.stringify({ 
+	// 			student_id: student_id,
+	// 			report_id: report_id 
+	// 		}) 
+	// 	});
+	// 	const reportDetail: ReportDetail = await res.json();
 		
-		return reportDetail
-	}
+	// 	return reportDetail
+	// }
 
 	public async updateReportDetailById({id, data}: {id: number, data: any}): Promise<ReportDetail> {
-		const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/update-report-details/${id}/`, { 
+		const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/reports/details/${id}/`, { 
 			method: 'PUT', 
 			headers: {
 				"Content-Type": "application/json",
