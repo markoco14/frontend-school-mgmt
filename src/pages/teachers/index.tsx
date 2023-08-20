cc8a04fe2ecbcb1485812f5c149b1952d86f9577
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-21 00:25:21 +0800

    chore: clean up teacher/index page

--- src/pages/teachers/index.tsx
+++ src/pages/teachers/index.tsx
@@ -1,29 +1,12 @@
 import AuthContext from "@/src/AuthContext";
 import Layout from "@/src/modules/core/infrastructure/ui/components/Layout";
-import LevelSection from "@/src/modules/curriculum/infrastructure/ui/components/LevelSection";
 import PermissionDenied from "@/src/modules/core/infrastructure/ui/components/PermissionDenied";
 import SchoolHeader from "@/src/modules/core/infrastructure/ui/components/SchoolHeader";
 import TeacherSection from "@/src/modules/user-mgmt/infrastructure/ui/TeacherSection";
-import { Dialog, Transition } from "@headlessui/react";
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 
-export default function AdminHome() {
-  const { user, selectedSchool } = useContext(AuthContext)
-  const [subjects, setSubjects] = useState<any[]>();
-  const [isAddSubject, setIsAddSubject] = useState<boolean>(false)
-
-  useEffect(() => {
-    async function getSubjects() {
-      if(selectedSchool) {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/subjects/?school=5`)
-        const subjects = await res.json()
-        console.log(subjects)
-        setSubjects(subjects.results)
-      }
-    }
-
-    getSubjects();
-  }, [selectedSchool])
+export default function Teachers() {
+  const { user } = useContext(AuthContext)
 
   if (user?.role !== "OWNER") {
     return (
@@ -36,8 +19,8 @@ export default function AdminHome() {
   return (
     <Layout>
       <div className="grid gap-4">
-          <SchoolHeader />
-          <TeacherSection />
+        <SchoolHeader />
+        <TeacherSection />
       </div>
     </Layout>
   );
