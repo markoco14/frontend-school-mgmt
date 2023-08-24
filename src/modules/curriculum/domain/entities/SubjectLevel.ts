a7ea1b866852fd5aca38207a562a51af0b931b09
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-24 18:38:18 +0800

    feature: add Subject and Level types to subject and level fields on SubjectLevel entity

--- src/modules/curriculum/domain/entities/SubjectLevel.ts
+++ src/modules/curriculum/domain/entities/SubjectLevel.ts
@@ -1,8 +1,11 @@
+import { Level } from "./Level";
+import { Subject } from "./Subject";
+
 export class SubjectLevel {
   constructor(
     public id: number,
-    public subject: number,
-    public level: number,
+    public subject: Subject,
+    public level: Level,
     public curriculum_description: string,
   ) {}
 }
