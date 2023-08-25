fdc74c89b35777157f66388a074db7d31f030603
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-25 12:17:30 +0800

    feature: move fetch schoolSubjectLevels up to CurriculumHome component and pass to SubjectLevelComponent as prop

--- src/modules/curriculum/infrastructure/ui/components/SubjectLevelSection.tsx
+++ src/modules/curriculum/infrastructure/ui/components/SubjectLevelSection.tsx
@@ -1,98 +1,90 @@
 import AuthContext from "@/src/AuthContext";
 import { useContext, useEffect, useState } from "react";
-import { Level } from "../../../domain/entities/Level";
 import { Subject } from "../../../domain/entities/Subject";
-import PaginationButtons from "@/src/modules/core/infrastructure/ui/components/PaginationButtons";
-import { subjectLevelAdapter } from "../../adapters/subjectLevelAdapter";
 import { SubjectLevel } from "../../../domain/entities/SubjectLevel";
-import { resolve } from "dns";
+import { Level } from "../../../domain/entities/Level";
 
-export default function SubjectLevelSection() {
+export default function SubjectLevelSection({subjectLevels}: {subjectLevels: SubjectLevel[]}) {
   const { selectedSchool } = useContext(AuthContext);
 
-  const [response, setResponse] = useState<any[]>([]);
-  const [subjectLevels, setSubjectLevels] = useState<any[]>([]);
+  const [currentSubjectLevels, setCurrentSubjectLevels] = useState<Level[]>([]);
   const [uniqueSubjects, setUniqueSubjects] = useState<Subject[]>([]);
   const [currentSubject, setCurrentSubject] = useState<string>("");
 
   useEffect(() => {
-    async function getSubjectLevels() {
-      await subjectLevelAdapter
-        .listSchoolSubjectLevels({ id: selectedSchool.id })
-        .then((res) => {
-          const uniqueSubjects = res.reduce(
-            (acc: Subject[], subjectLevel: SubjectLevel) => {
-              if (
-                !acc.find(
-                  (subject: Subject) =>
-                    subject.name === subjectLevel.subject.name
-                )
-              ) {
-                acc.push(subjectLevel.subject);
-              }
-              return acc;
-            },
-            []
-          );
-          const filteredLevels = res
-            .filter(
-              (subjectLevel) =>
-                subjectLevel.subject.name === uniqueSubjects[0].name
-            )
-            .map((subjectLevel) => subjectLevel.level)
-            .sort((a, b) => a.order - b.order);
+		const uniqueSubjects = subjectLevels.reduce(
+			(acc: Subject[], subjectLevel: SubjectLevel) => {
+				if (
+					!acc.find(
+						(subject: Subject) =>
+						subject.name === subjectLevel.subject.name
+						)
+						) {
+							acc.push(subjectLevel.subject);
+						}
+						return acc;
+					},
+		[]
+		);
+		setUniqueSubjects(uniqueSubjects);
+		setCurrentSubject(uniqueSubjects[0]?.name);
+		
+		const filteredLevels = subjectLevels
+		.filter(
+			(subjectLevel) =>
+			subjectLevel.subject.name === uniqueSubjects[0]?.name
+			)
+			.map((subjectLevel) => subjectLevel.level)
+			.sort((a, b) => a.order - b.order);
+		setCurrentSubjectLevels(filteredLevels);
+  }, [selectedSchool, subjectLevels]);
 
-          setResponse(res);
-          setUniqueSubjects(uniqueSubjects);
-          setCurrentSubject(uniqueSubjects[0].name);
-          setSubjectLevels(filteredLevels);
-        });
-    }
-    getSubjectLevels();
-  }, [selectedSchool]);
-
-  function handleChangeLevel(response: SubjectLevel[], subjectName: string) {
-    const filteredLevels = response
+  function handleChangeLevel(subjectLevels: SubjectLevel[], subjectName: string) {
+    const filteredLevels = subjectLevels
       .filter((subjectLevel) => subjectLevel.subject.name === subjectName)
       .map((subjectLevel) => subjectLevel.level)
       .sort((a, b) => a.order - b.order);
     setCurrentSubject(subjectName);
-    setSubjectLevels(filteredLevels);
+    setCurrentSubjectLevels(filteredLevels);
   }
 
   return (
     <section className="col-span-2">
       <h2 className="text-3xl mb-2">Subjects/Levels</h2>
-      <article className="grid grid-cols-2">
-        <div className="col-span-3 xs:col-span-1">
-          <h3 className="text-center">Subjects</h3>
-          <ul className="bg-gray-100 rounded shadow-inner mb-4">
-            {uniqueSubjects?.map((subject: Subject, index) => (
-              <li
-                key={index}
-                className={`${
-                  subject.name === currentSubject ? "bg-blue-300" : "bg-white"
-                } p-2 hover:bg-gray-300`}
-                onClick={() => {
-                  handleChangeLevel(response, subject.name);
-                }}
-              >
-                {subject.name}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="col-span-3 xs:col-span-1">
-          <h3 className="text-center">Levels</h3>
-          <ul className="bg-gray-100 rounded shadow-inner mb-4">
-            {subjectLevels?.map((level, index) => (
-              <li key={index} className="p-2 hover:bg-gray-300">
-                {level.name}
-              </li>
-            ))}
-          </ul>
-        </div>
-      </article>
+			{!subjectLevels.length ? (
+				<p>there are no levels connected to your subjects</p>
+			) : (
+				<article className="grid grid-cols-2">
+					<div className="col-span-3 xs:col-span-1">
+						<h3 className="text-xl">Subjects</h3>
+						<ul className="bg-gray-100 rounded shadow-inner mb-4">
+							{uniqueSubjects?.map((subject: Subject, index) => (
+								<li
+									key={index}
+									className={`${
+										subject.name === currentSubject ? "bg-blue-300" : "bg-white"
+									} p-2 hover:bg-gray-300`}
+									onClick={() => {
+										handleChangeLevel(subjectLevels, subject.name);
+									}}
+								>
+									{subject.name}
+								</li>
+							))}
+						</ul>
+					</div>
+					<div className="col-span-3 xs:col-span-1">
+						<h3 className="text-xl">Levels</h3>
+						<ul className="bg-gray-100 rounded shadow-inner mb-4">
+							{currentSubjectLevels?.map((level, index) => (
+								<li key={index} className="p-2 hover:bg-gray-300">
+									{level.name}
+								</li>
+							))}
+						</ul>
+					</div>
+				</article>
+			)}
     </section>
   );
 }
--- src/pages/curriculum/index.tsx
+++ src/pages/curriculum/index.tsx
@@ -3,12 +3,29 @@ import Layout from "@/src/modules/core/infrastructure/ui/components/Layout";
 import LevelSection from "@/src/modules/curriculum/infrastructure/ui/components/LevelSection";
 import PermissionDenied from "@/src/modules/core/infrastructure/ui/components/PermissionDenied";
 import SchoolHeader from "@/src/modules/core/infrastructure/ui/components/SchoolHeader";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import SubjectSection from "@/src/modules/curriculum/infrastructure/ui/components/SubjectSection";
 import SubjectLevelSection from "@/src/modules/curriculum/infrastructure/ui/components/SubjectLevelSection";
+import { subjectLevelAdapter } from "@/src/modules/curriculum/infrastructure/adapters/subjectLevelAdapter";
+import { Subject } from "@/src/modules/curriculum/domain/entities/Subject";
+import { SubjectLevel } from "@/src/modules/curriculum/domain/entities/SubjectLevel";
 
 export default function CurriculumHome() {
-  const { user } = useContext(AuthContext)
+  const { user, selectedSchool } = useContext(AuthContext)
+
+
+  const [subjectLevels, setSubjectLevels] = useState<any[]>([]);
+
+  useEffect(() => {
+    async function getSubjectLevels() {
+      await subjectLevelAdapter
+        .listSchoolSubjectLevels({ id: selectedSchool?.id })
+        .then((res) => {
+          setSubjectLevels(res);
+        });
+    }
+    getSubjectLevels();
+  }, [selectedSchool]);
 
   if (user?.role !== "OWNER") {
     return (
@@ -24,7 +41,7 @@ export default function CurriculumHome() {
       <div className="grid xs:grid-cols-2 gap-4">
         <LevelSection />
         <SubjectSection />
-        <SubjectLevelSection />
+        <SubjectLevelSection subjectLevels={subjectLevels}/>
       </div>
     </Layout>
   );
