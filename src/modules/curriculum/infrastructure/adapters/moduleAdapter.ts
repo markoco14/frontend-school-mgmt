0f35158508d9192e82bc2c9c95046f22dbfacb85
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-26 18:40:00 +0800

    feature: add optional levelOrder and subjectName params to listSchoolModules method and implement in SubjectLevelSection

--- src/modules/curriculum/infrastructure/adapters/moduleAdapter.ts
+++ src/modules/curriculum/infrastructure/adapters/moduleAdapter.ts
@@ -1,36 +1,69 @@
 import { Module } from "../../domain/entities/Module";
 
 class ModuleAdapter {
+  public async listSchoolModules({
+    schoolId,
+    levelOrder,
+    subjectName,
+  }: {
+    schoolId: number;
+    levelOrder?: number;
+    subjectName?: string;
+  }): Promise<Module[]> {
+    let url = `${process.env.NEXT_PUBLIC_API_URL}/schools/${schoolId}/modules/`;
 
-	public async listSchoolModules({schoolId}: {schoolId: number}): Promise<Module[]> {
-		const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/schools/${schoolId}/modules/`);
-		const moduleList: Module[] = await res.json();
+    const queryParams: string[] = [];
+    if (levelOrder) queryParams.push(`level=${levelOrder}`);
+    if (subjectName) queryParams.push(`subject=${encodeURIComponent(subjectName)}`);
 
-		return moduleList
-	}
+    if (queryParams.length) {
+      url += `?${queryParams.join("&")}`;
+    }
 
-	public async addModule({name, order, subject_level, type}: {name: string, order: number, subject_level: number, type: number}): Promise<Module> {
-		const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/modules/`, { 
-			method: 'POST', 
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ name: name, order: order, subject_level: subject_level, type: type, parent }) 
-		});
-		const new_module: Module = await res.json();
+    const res = await fetch(url);
+    const moduleList: Module[] = await res.json();
 
-		return new_module
+    return moduleList;
+  }
 
-	}
+  public async addModule({
+    name,
+    order,
+    subject_level,
+    type,
+  }: {
+    name: string;
+    order: number;
+    subject_level: number;
+    type: number;
+  }): Promise<Module> {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/modules/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        order: order,
+        subject_level: subject_level,
+        type: type,
+      }),
+    });
+    const new_module: Module = await res.json();
 
+    return new_module;
+  }
 
-	public async deleteModule({id}: {id:number}): Promise<any> {
-		const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/modules/${id}/`,{
-			method: 'DELETE'
-		});
+  public async deleteModule({ id }: { id: number }): Promise<any> {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/modules/${id}/`,
+      {
+        method: "DELETE",
+      }
+    );
 
-		return res
-	}
+    return res;
+  }
 }
 
 export const moduleAdapter = new ModuleAdapter();
--- src/modules/curriculum/infrastructure/ui/components/SubjectLevelSection.tsx
+++ src/modules/curriculum/infrastructure/ui/components/SubjectLevelSection.tsx
@@ -6,119 +6,176 @@ import { Level } from "../../../domain/entities/Level";
 import { Module } from "../../../domain/entities/Module";
 import { moduleAdapter } from "../../adapters/moduleAdapter";
 
-export default function SubjectLevelSection({subjectLevels}: {subjectLevels: SubjectLevel[]}) {
+export default function SubjectLevelSection({
+  subjectLevels,
+}: {
+  subjectLevels: SubjectLevel[];
+}) {
   const { selectedSchool } = useContext(AuthContext);
 
-  const [currentSubjectLevels, setCurrentSubjectLevels] = useState<Level[]>([]);
-  const [uniqueSubjects, setUniqueSubjects] = useState<Subject[]>([]);
-  const [currentSubject, setCurrentSubject] = useState<string>("");
+  const [uniqueSubjects, setUniqueSubjects] = useState<Subject[]>(
+    subjectLevels.reduce((acc: Subject[], subjectLevel: SubjectLevel) => {
+      if (
+        !acc.find(
+          (subject: Subject) => subject.name === subjectLevel.subject.name
+        )
+      ) {
+        acc.push(subjectLevel.subject);
+      }
+      return acc;
+    }, [])
+  );
 
-	const [currentLevel, setCurrentLevel] = useState<number>(0);
+  const [currentSubject, setCurrentSubject] = useState<string>(
+    subjectLevels.reduce((acc: Subject[], subjectLevel: SubjectLevel) => {
+      if (
+        !acc.find(
+          (subject: Subject) => subject.name === subjectLevel.subject.name
+        )
+      ) {
+        acc.push(subjectLevel.subject);
+      }
+      return acc;
+    }, [])[0].name
+  );
 
-	const [modules, setModules] = useState<Module[]>([]);
+  const [currentSubjectLevels, setCurrentSubjectLevels] = useState<Level[]>(
+    subjectLevels
+      .filter(
+        (subjectLevel) => subjectLevel.subject.name === uniqueSubjects[0]?.name
+      )
+      .map((subjectLevel) => subjectLevel.level)
+      .sort((a, b) => a.order - b.order)
+  );
+  const [currentLevel, setCurrentLevel] = useState<number>(
+    subjectLevels
+      .filter(
+        (subjectLevel) => subjectLevel.subject.name === uniqueSubjects[0]?.name
+      )
+      .map((subjectLevel) => subjectLevel.level)
+      .sort((a, b) => a.order - b.order)[0].order
+  );
+
+  const [modules, setModules] = useState<Module[]>([]);
 
   useEffect(() => {
+    async function getSchoolModules() {
+      if (selectedSchool) {
+        await moduleAdapter
+          .listSchoolModules({
+            schoolId: selectedSchool.id,
+            levelOrder: currentLevel,
+            subjectName: currentSubject,
+          })
+          .then((res) => setModules(res));
+      }
+    }
 
-		async function getSchoolModules() {
-			if (selectedSchool) {
-				await moduleAdapter.listSchoolModules({schoolId: selectedSchool.id})
-					.then((res) => setModules(res))
-			}
-		}
+    getSchoolModules();
+  }, [selectedSchool, subjectLevels, currentLevel, currentSubject]);
 
-		const uniqueSubjects = subjectLevels.reduce(
-			(acc: Subject[], subjectLevel: SubjectLevel) => {
-				if (
-					!acc.find(
-						(subject: Subject) =>
-						subject.name === subjectLevel.subject.name
-						)
-						) {
-							acc.push(subjectLevel.subject);
-						}
-						return acc;
-					},
-		[]
-		);
-		setUniqueSubjects(uniqueSubjects);
-		setCurrentSubject(uniqueSubjects[0]?.name);
-		
-		const filteredLevels = subjectLevels
-		.filter(
-			(subjectLevel) =>
-			subjectLevel.subject.name === uniqueSubjects[0]?.name
-			)
-			.map((subjectLevel) => subjectLevel.level)
-			.sort((a, b) => a.order - b.order);
-		setCurrentSubjectLevels(filteredLevels);
-		setCurrentLevel(filteredLevels[0]?.order)
+  async function handleFetchModules({
+    schoolId,
+    level,
+    subject,
+  }: {
+    schoolId: number;
+    level: number;
+    subject: string;
+  }) {
+    if (selectedSchool) {
+      await moduleAdapter
+        .listSchoolModules({
+          schoolId: schoolId,
+          levelOrder: level,
+          subjectName: subject,
+        })
+        .then((res) => setModules(res));
+    }
+  }
 
-		getSchoolModules();
-  }, [selectedSchool, subjectLevels]);
-
-  function handleChangeLevel(subjectLevels: SubjectLevel[], subjectName: string) {
+  function handleChangeLevel(
+    subjectLevels: SubjectLevel[],
+    subjectName: string
+  ) {
     const filteredLevels = subjectLevels
       .filter((subjectLevel) => subjectLevel.subject.name === subjectName)
       .map((subjectLevel) => subjectLevel.level)
       .sort((a, b) => a.order - b.order);
     setCurrentSubject(subjectName);
     setCurrentSubjectLevels(filteredLevels);
+    setCurrentLevel(filteredLevels[0].order);
   }
 
   return (
     <section className="col-span-2">
       <h2 className="text-3xl mb-2">Modules</h2>
-			{!subjectLevels.length ? (
-				<p>there are no levels connected to your subjects</p>
-			) : (
-				<article className="grid grid-cols-4">
-					<div className="col-span-4 xs:col-span-1">
-						<h3 className="text-xl">Subjects</h3>
-						<ul className="bg-gray-100 rounded shadow-inner mb-4">
-							{uniqueSubjects?.map((subject: Subject, index) => (
-								<li
-									key={index}
-									className={`${
-										subject.name === currentSubject ? "bg-blue-300 hover:bg-blue-500" : "bg-white hover:bg-gray-300"
-									} p-2 hover:cursor-pointer`}
-									onClick={() => {
-										handleChangeLevel(subjectLevels, subject.name);
-									}}
-								>
-									{subject.name}
-								</li>
-							))}
-						</ul>
-					</div>
-					<div className="col-span-4 xs:col-span-1">
-						<h3 className="text-xl">Levels</h3>
-						<ul className="bg-gray-100 rounded shadow-inner mb-4">
-							{currentSubjectLevels?.map((level, index) => (
-								<li 
-								key={index} 
-								className={`${
-									level.order === currentLevel ? "bg-blue-300 hover:bg-blue-500" : "bg-white hover:bg-gray-300"
-									} p-2 hover:cursor-pointer`
-								}
-								onClick={() => setCurrentLevel(level.order)}
-								>
-									{level.name}
-								</li>
-							))}
-						</ul>
-					</div>
-					<div className="col-span-4 xs:col-span-2">
-						<h3 className="text-xl">Modules</h3>
-						<ul className="bg-gray-100 rounded shadow-inner mb-4">
-							{modules?.map((module, index) => (
-								<li key={index} className="p-2 hover:bg-gray-300">
-									{module.name}
-								</li>
-							))}
-						</ul>
-					</div>
-				</article>
-			)}
+      {!subjectLevels.length ? (
+        <p>there are no levels connected to your subjects</p>
+      ) : (
+        <article className="grid grid-cols-4">
+          <div className="col-span-4 xs:col-span-1">
+            <h3 className="text-xl">Subjects</h3>
+            <ul className="bg-gray-100 rounded shadow-inner mb-4">
+              {uniqueSubjects?.map((subject: Subject, index) => (
+                <li
+                  key={index}
+                  className={`${
+                    subject.name === currentSubject
+                      ? "bg-blue-300 hover:bg-blue-500"
+                      : "bg-white hover:bg-gray-300"
+                  } p-2 hover:cursor-pointer`}
+                  onClick={() => {
+                    handleChangeLevel(subjectLevels, subject.name);
+                    handleFetchModules({
+                      schoolId: selectedSchool.id,
+                      subject: subject.name,
+                      level: currentLevel,
+                    });
+                  }}
+                >
+                  {subject.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="col-span-4 xs:col-span-1">
+            <h3 className="text-xl">Levels</h3>
+            <ul className="bg-gray-100 rounded shadow-inner mb-4">
+              {currentSubjectLevels?.map((level, index) => (
+                <li
+                  key={index}
+                  className={`${
+                    level.order === currentLevel
+                      ? "bg-blue-300 hover:bg-blue-500"
+                      : "bg-white hover:bg-gray-300"
+                  } p-2 hover:cursor-pointer`}
+                  onClick={() => {
+                    setCurrentLevel(level.order);
+                    handleFetchModules({
+                      schoolId: selectedSchool.id,
+                      subject: currentSubject,
+                      level: level.order,
+                    });
+                  }}
+                >
+                  {level.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="col-span-4 xs:col-span-2">
+            <h3 className="text-xl">Modules</h3>
+            <ul className="bg-gray-100 rounded shadow-inner mb-4">
+              {modules?.map((module, index) => (
+                <li key={index} className="p-2 hover:bg-gray-300">
+                  {module.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </article>
+      )}
     </section>
   );
 }
