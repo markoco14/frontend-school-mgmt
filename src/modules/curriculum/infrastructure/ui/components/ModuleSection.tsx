18edda00a4f51d5cb1653b78f1ed647bc06ae9e2
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-28 09:52:55 +0800

    feature: create basic AddModule modal in ModuleSection

--- src/modules/curriculum/infrastructure/ui/components/ModuleSection.tsx
+++ src/modules/curriculum/infrastructure/ui/components/ModuleSection.tsx
@@ -1,10 +1,67 @@
 import AuthContext from "@/src/AuthContext";
-import { useContext, useEffect, useState } from "react";
+import { Fragment, useContext, useEffect, useState } from "react";
 import { Subject } from "../../../domain/entities/Subject";
 import { SubjectLevel } from "../../../domain/entities/SubjectLevel";
 import { Level } from "../../../domain/entities/Level";
 import { Module } from "../../../domain/entities/Module";
 import { moduleAdapter } from "../../adapters/moduleAdapter";
+import { Dialog, Transition } from "@headlessui/react";
+
+const AddModule = ({
+  currentSubject,
+  currentLevel,
+  isAddModule,
+  setIsAddModule,
+}: {
+  currentSubject: string;
+  currentLevel: number;
+  isAddModule: boolean;
+  setIsAddModule: Function;
+}) => {
+  return (
+    <Transition appear={true} show={isAddModule}>
+      <Dialog
+        onClose={() => setIsAddModule(false)}
+        className="fixed inset-0 flex items-center justify-center"
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-blue-900 bg-opacity-50" />
+        </Transition.Child>
+        <Transition.Child
+          as={Fragment}
+          enter="transition-opacity transition-scale ease-in duration-500"
+          enterFrom="opacity-0 scale-90"
+          enterTo="opacity-100 scale-100"
+          leave="transition-opacity transition-scale ease-out duration-150"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0"
+        >
+          <Dialog.Panel className="bg-white rounded-2xl shadow-xl p-8 z-10">
+            <Dialog.Title>Create New Module</Dialog.Title>
+            <p>You are creating a new Module for {currentSubject} Level {currentLevel} </p>
+            <div className="flex justify-end">
+              <button
+                type="button"
+                onClick={() => setIsAddModule(false)}
+                className="bg-gray-300 text-gray-900 hover:bg-gray-500 hover:text-white px-4 py-1 rounded"
+              >
+                Cancel
+              </button>
+            </div>
+          </Dialog.Panel>
+        </Transition.Child>
+      </Dialog>
+    </Transition>
+  );
+};
 
 export default function ModuleSection({
   subjectLevels,
@@ -12,6 +69,7 @@ export default function ModuleSection({
   subjectLevels: SubjectLevel[];
 }) {
   const { selectedSchool } = useContext(AuthContext);
+  const [isAddModule, setIsAddModule] = useState<boolean>(false);
 
   const [uniqueSubjects, setUniqueSubjects] = useState<Subject[]>([]);
 
@@ -34,8 +92,16 @@ export default function ModuleSection({
   const [modules, setModules] = useState<Module[]>([]);
 
   useEffect(() => {
-    async function fetchModules({schoolId, currentSubject, currentLevel}: {schoolId: number, currentSubject: string, currentLevel: number}){
-       if (selectedSchool) {
+    async function fetchModules({
+      schoolId,
+      currentSubject,
+      currentLevel,
+    }: {
+      schoolId: number;
+      currentSubject: string;
+      currentLevel: number;
+    }) {
+      if (selectedSchool) {
         await moduleAdapter
           .listSchoolModules({
             schoolId: schoolId,
@@ -45,36 +111,42 @@ export default function ModuleSection({
           .then((res) => setModules(res));
       }
     }
-    
 
-    const uniqueSubjects = subjectLevels.reduce((acc: Subject[], subjectLevel: SubjectLevel) => {
-      if (
-        !acc.find(
-          (subject: Subject) => subject.name === subjectLevel.subject.name
-        )
-      ) {
-        acc.push(subjectLevel.subject);
-      }
-      return acc;
-    }, [])
+    const uniqueSubjects = subjectLevels.reduce(
+      (acc: Subject[], subjectLevel: SubjectLevel) => {
+        if (
+          !acc.find(
+            (subject: Subject) => subject.name === subjectLevel.subject.name
+          )
+        ) {
+          acc.push(subjectLevel.subject);
+        }
+        return acc;
+      },
+      []
+    );
 
-    const defaultSubject = uniqueSubjects[0]?.name
+    const defaultSubject = uniqueSubjects[0]?.name;
     setUniqueSubjects(uniqueSubjects);
-    setCurrentSubject(defaultSubject)
+    setCurrentSubject(defaultSubject);
 
-    const filteredLevels =  subjectLevels
+    const filteredLevels = subjectLevels
       .filter(
         (subjectLevel) => subjectLevel.subject.name === uniqueSubjects[0]?.name
       )
       .map((subjectLevel) => subjectLevel.level)
-      .sort((a, b) => a.order - b.order)
+      .sort((a, b) => a.order - b.order);
 
-    const defaultLevel = filteredLevels[0]?.order
-    setCurrentSubjectLevels(filteredLevels)
-    setCurrentLevel(defaultLevel)
+    const defaultLevel = filteredLevels[0]?.order;
+    setCurrentSubjectLevels(filteredLevels);
+    setCurrentLevel(defaultLevel);
 
-    fetchModules({schoolId: selectedSchool.id, currentSubject: defaultSubject, currentLevel: defaultLevel,})
-  }, [subjectLevels, selectedSchool])
+    fetchModules({
+      schoolId: selectedSchool.id,
+      currentSubject: defaultSubject,
+      currentLevel: defaultLevel,
+    });
+  }, [subjectLevels, selectedSchool]);
 
   async function handleFetchModules({
     schoolId,
@@ -167,7 +239,17 @@ export default function ModuleSection({
             </ul>
           </div>
           <div className="col-span-4 xs:col-span-2">
-            <h3 className="text-xl">Modules</h3>
+            <div className="flex justify-between">
+              <h3 className="text-xl">Modules</h3>
+              <button
+                className=""
+                onClick={() => {
+                  setIsAddModule(!isAddModule);
+                }}
+              >
+                <i className="fa-solid fa-plus"></i>
+              </button>
+            </div>
             <ul className="bg-gray-100 rounded shadow-inner mb-4">
               {modules?.map((module, index) => (
                 <li key={index} className="p-2 hover:bg-gray-300">
@@ -176,6 +258,14 @@ export default function ModuleSection({
               ))}
             </ul>
           </div>
+          {isAddModule && (
+            <AddModule
+              currentSubject={currentSubject}
+              currentLevel={currentLevel}
+              isAddModule={isAddModule}
+              setIsAddModule={setIsAddModule}
+            />
+          )}
         </article>
       )}
     </section>
