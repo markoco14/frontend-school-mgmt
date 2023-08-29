20f38e911735ee1903b39eaa4d9716770d739201
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-29 23:21:09 +0800

    feature: create assessmentTypeAdapter with list method

---
+++ src/modules/curriculum/infrastructure/adapters/assessmentTypeAdapter.ts
@@ -0,0 +1,61 @@
+import { AssessmentType } from './../../domain/entities/AssessmentType';
+
+
+class AssessmentTypeAdapter {
+  public async list({
+    schoolId,
+  }: {
+    schoolId: number;
+  }): Promise<AssessmentType[]> {
+    let url = `${process.env.NEXT_PUBLIC_API_URL}/schools/${schoolId}/assessment-types/`;
+
+    const res = await fetch(url);
+    const moduleTypeList: AssessmentType[] = await res.json();
+
+    return moduleTypeList;
+  }
+  
+  // public async add({schoolId, typeName}: {schoolId: number, typeName: string}): Promise<AssessmentType> {
+  //   let url = `${process.env.NEXT_PUBLIC_API_URL}/module-types/`;
+  
+  //   const res = await fetch(url, { 
+	// 		method: 'POST', 
+	// 		headers: {
+	// 			"Content-Type": "application/json",
+	// 		},
+	// 		body: JSON.stringify({ name: typeName, school: schoolId }) 
+	// 	});
+  //   const newModule: AssessmentType = await res.json();
+  
+  //   return newModule;
+    
+  // }
+
+  // public async patch({typeId, typeName}: {typeId: number, typeName: string}): Promise<AssessmentType> {
+  //   let url = `${process.env.NEXT_PUBLIC_API_URL}/module-types/${typeId}/`;
+  
+  //   const res = await fetch(url, { 
+	// 		method: 'PATCH', 
+	// 		headers: {
+	// 			"Content-Type": "application/json",
+	// 		},
+	// 		body: JSON.stringify({ name: typeName}) 
+	// 	});
+  //   const newModule: AssessmentType = await res.json();
+  
+  //   return newModule;
+  // }
+
+  // public async delete({typeId}: {typeId: number}): Promise<any> {
+  //   let url = `${process.env.NEXT_PUBLIC_API_URL}/module-types/${typeId}/`;
+  
+  //   const res = await fetch(url, {
+  //       method: "DELETE",
+  //     });
+  
+  //   return res;
+  // }
+
+}
+
+export const assessmentTypeAdapter = new AssessmentTypeAdapter();
