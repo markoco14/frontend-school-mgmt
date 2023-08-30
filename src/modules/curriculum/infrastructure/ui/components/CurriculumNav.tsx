36ceee17ed9823f5f1550f0d74fbf45ba644595b
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-30 16:48:48 +0800

    feature: apply horizontal scroll to curriculum nav (temp fix)

--- src/modules/curriculum/infrastructure/ui/components/CurriculumNav.tsx
+++ src/modules/curriculum/infrastructure/ui/components/CurriculumNav.tsx
@@ -1,6 +1,6 @@
 export default function CurriculumNav({tab, setTab}: {tab: number, setTab: Function}) {
 	return (
-		<nav className="flex gap-4 mb-6">
+		<nav className="flex gap-4 mb-6 overflow-x-auto">
 			<button
 				className={`${tab === 1 ? "text-blue-500 underline underline-offset-2" : "text-gray-600"} text-lg hover:text-blue-700 ease-in-out duration-200`} 
 				onClick={() => setTab(1)}
