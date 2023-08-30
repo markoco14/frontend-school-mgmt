c027b0645a0fa9ac429502e4dbc9c7c1849aca9b
Mark O'Connor <mark.oconnor14@gmail.com>
2023-08-30 16:33:52 +0800

    feature: implement NewAssessmentForm and setSelectedModule state

--- src/modules/curriculum/infrastructure/ui/components/assessments/AssessmentSection.tsx
+++ src/modules/curriculum/infrastructure/ui/components/assessments/AssessmentSection.tsx
@@ -2,13 +2,8 @@ import AuthContext from "@/src/AuthContext";
 import { useContext, useEffect, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { assessmentAdapter } from "../../../adapters/assessmentAdapter";
-import { Assessment } from "@/src/modules/curriculum/domain/entities/Assessment";
 import { Module } from "@/src/modules/curriculum/domain/entities/Module";
-import { Subject } from "@/src/modules/curriculum/domain/entities/Subject";
-import { SubjectLevel } from "@/src/modules/curriculum/domain/entities/SubjectLevel";
-import toast from "react-hot-toast";
-import Modal from "@/src/modules/core/infrastructure/ui/components/Modal";
-import ManageAssessmentType from "@/src/modules/curriculum/infrastructure/ui/components/assessment-type/ManageAssessmentType";
+import NewAssessmentForm from "./NewAssessmentForm";
 
 type Inputs = {
   name: string;
@@ -32,8 +27,7 @@ export default function AssessmentSection() {
   const [modules, setModules] = useState<Module[]>([]);
   const subjectCategories = ["Grammar", "Phonics", "Reading"];
 
-  const [isManageType, setIsManageType] = useState<boolean>(false);
-  // const [selectedType, setSelectedType] = useState<AssessmentType | null>(null);
+  const [selectedModule, setSelectedModule] = useState<Module | null>(null);
 
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
     console.log(data);
@@ -87,7 +81,7 @@ export default function AssessmentSection() {
             subjectCategories?.map((category, categoryIndex) => (
               <div key={categoryIndex} className="grid gap-2 divide-y">
                 <h3 className="text-lg">{category}</h3>
-                <ul className="grid gap-2 indent-4 pt-2">
+                <ul className="grid gap-2 pt-2">
                   {modules
                     ?.filter(
                       (module) => module.subject_level.subject.name === category
@@ -95,22 +89,25 @@ export default function AssessmentSection() {
                     .map((filteredModule, index) => (
                       <li
                         key={index}
-                        // onClick={() => {
-                        // 	setSelectedType(filteredModule);
-                        // 	setIsManageType(true);
-                        // }}
+												className="grid"
+                        onClick={() => {
+													console.log(filteredModule)
+                        	setSelectedModule(filteredModule);
+                        }}
                       >
-                        <span>
+                        <span
+													className="hover:cursor-pointer hover:underline hover:underline-offset-4"
+												>
                           Level {filteredModule.subject_level.level.order} Unit{" "}
                           {filteredModule.order}: {filteredModule.name}{" "}
                         </span>
-                        <ul className="grid gap-2 indent-8 pt-2">
-                          {filteredModule.assessments?.map(
-                            (assessment, assessmentIndex) => (
-                              <li key={assessmentIndex}>{assessment.name}</li>
-                            )
-                          )}
-                        </ul>
+                          {filteredModule.assessments && filteredModule.assessments.length > 0 && (
+														<ul className="grid gap-2 indent-4 pt-2">
+															{filteredModule.assessments.map((assessment, assessmentIndex) => (
+																<li key={assessmentIndex}>{assessment.name}</li>
+															))}
+														</ul>
+													)}
                       </li>
                     ))}
                 </ul>
@@ -119,128 +116,20 @@ export default function AssessmentSection() {
           )}
         </article>
         <article className="border rounded shadow mb-4 p-4 flex flex-col gap-6 text-gray-700">
-          <h3 className="text-xl">New Assessment Type</h3>
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="flex flex-col gap-4"
-          >
-            <div className="flex flex-col gap-2">
-              <label className="flex justify-between">
-                <span>Name</span>{" "}
-                {errors.name && <span className="text-red-500">required</span>}
-              </label>
-              <input
-                type="text"
-                className="border shadow-inner rounded p-2"
-                {...register("name", {
-                  required: true,
-                  minLength: 2,
-                  maxLength: 50,
-                })}
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              <label className="flex justify-between">
-                <span>Description</span>{" "}
-                {errors.description && (
-                  <span className="text-red-500">required</span>
-                )}
-              </label>
-              <textarea
-                className="border shadow-inner rounded p-2"
-                {...register("description", {
-                  required: true,
-                  minLength: 2,
-                  maxLength: 255,
-                })}
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              <label className="flex justify-between">
-                <span>Module</span>{" "}
-                {errors.module?.type && (
-                  <span className="text-red-500">1 or higher</span>
-                )}
-              </label>
-              <input
-                type="number"
-                className="border shadow-inner rounded p-2"
-                {...register("module", {
-                  required: true,
-                  min: 1,
-                  valueAsNumber: true,
-                })}
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              <label className="flex justify-between">
-                <span>Type</span>{" "}
-                {errors.type && <span className="text-red-500">required</span>}
-              </label>
-              <input
-                type="number"
-                className="border shadow-inner rounded p-2"
-                {...register("type", {
-                  required: true,
-                  min: 1,
-                  valueAsNumber: true,
-                })}
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              <label className="flex justify-between">
-                <span>Order</span>{" "}
-                {errors.order && <span className="text-red-500">required</span>}
-              </label>
-              <input
-                type="number"
-                className="border shadow-inner rounded p-2"
-                {...register("order", {
-                  required: true,
-                  min: 1,
-                  valueAsNumber: true,
-                })}
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              <label className="flex justify-between">
-                <span>Max Score</span>{" "}
-                {errors.max_score && (
-                  <span className="text-red-500">required</span>
-                )}
-              </label>
-              <input
-                type="number"
-                className="border shadow-inner rounded p-2"
-                {...register("max_score", {
-                  required: true,
-                  min: 1,
-                  valueAsNumber: true,
-                })}
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              <label className="flex justify-between">
-                <span>Status</span>{" "}
-                {errors.status && (
-                  <span className="text-red-500">required</span>
-                )}
-              </label>
-              <input
-                type="number"
-                className="border shadow-inner rounded p-2"
-                {...register("status", {
-                  required: true,
-                  min: 0,
-                  max: 4,
-                  valueAsNumber: true,
-                })}
-              />
-            </div>
-            <button className="w-full ease-in-out duration-200 bg-blue-600  hover:bg-blue-900 p-2 rounded shadow text-white">
-              Submit
-            </button>
-          </form>
+					{!selectedModule && (
+						<>
+							<p>Click a module to add assessments.</p>
+						</>
+					)}
+					{selectedModule && (
+						<>
+							<div className="flex justify-between items-baseline">
+								<h3 className="text-xl text-gray-500">New {selectedModule.name} Assessment</h3>
+								<button onClick={() => setSelectedModule(null)}><i className="fa-solid fa-xmark" /></button>
+							</div>
+							<NewAssessmentForm selectedModule={selectedModule}/>
+						</>
+					)}
         </article>
       </div>
       {/* <Modal show={isManageType} close={handleClose} title={"Manage Assessment Type"}>
