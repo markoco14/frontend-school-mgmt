a9288cc9f08e73917c91e083c24ce5b2530c53f0
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-03 14:09:25 +0800

    feature: add student image and student names to ClassStudentList

--- src/modules/classes/infrastructure/ui/components/class-students/ClassStudentList.tsx
+++ src/modules/classes/infrastructure/ui/components/class-students/ClassStudentList.tsx
@@ -1,4 +1,5 @@
 
+import Image from "next/image";
 import { ClassStudent } from "../../../../domain/entities/ClassStudent";
 
 export default function ClassStudentList({classStudentList}: {classStudentList: ClassStudent[]}) {
@@ -17,21 +18,33 @@ export default function ClassStudentList({classStudentList}: {classStudentList: 
   // }
   
   return (
-    <>
+    <section className="grid md:grid-cols-2">
       {classStudentList?.length === 0 && (
         <article className="bg-gray-100 shadow-inner p-2 rounded">
           <p>There are no students in this class. Click here to add some.</p>
         </article>
       )}
       {classStudentList?.length >= 1 && (
-        <article className="bg-gray-100 shadow-inner p-2 rounded">
+        <article className="border-2 p-4 rounded">
           <ul className="flex flex-col gap-2 divide-y">
             {classStudentList?.map((student: ClassStudent, index: number) => (
               <li
                 key={index}
-                className="p-2 rounded hover:bg-blue-200 flex justify-between "
-              >
-                {student.student_id}
+                className="p-2 rounded hover:bg-blue-200 flex items-center gap-4"
+              > 
+                <div className="relative col-span-1 flex justify-center">
+                  <Image 
+                    src={student.student ? student.student?.photo_url :  ''}
+                    alt={`An image of ${student.student?.first_name}`}
+                    width={50}
+                    height={50}
+                    style={{ objectFit: 'cover' }}
+                    className="rounded-full"
+                  />
+                </div>
+                <div className="text-lg">
+                  {student.student?.first_name} {student.student?.last_name}
+                </div>
                 {/* <button
                   onClick={() => {
                     removeStudentFromClassList(selectedClass?.id, student.id);
@@ -44,6 +57,6 @@ export default function ClassStudentList({classStudentList}: {classStudentList: 
           </ul>
         </article>
       )}
-    </>
+    </section>
   );
 };
