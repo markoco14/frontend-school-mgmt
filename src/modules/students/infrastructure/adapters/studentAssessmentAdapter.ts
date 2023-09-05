fa9875ee78a9886291b570e86ce602f9939ad27c
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-05 12:48:16 +0800

    feature: set up student assessment list on student_id (profile) page

---
+++ src/modules/students/infrastructure/adapters/studentAssessmentAdapter.ts
@@ -0,0 +1,43 @@
+import { StudentAssessment } from "../../domain/entities/StudentAssessment";
+
+class StudentAssessmentAdapter {
+  public async list({
+    student_id,
+    details,
+  }: {
+    student_id?: number;
+    details?: boolean;
+  }): Promise<StudentAssessment[]> {
+    let url;
+
+    if (student_id) {
+      url = `${process.env.NEXT_PUBLIC_API_URL}/students/${student_id}/student-assessments/`;
+    } else {
+      url = `${process.env.NEXT_PUBLIC_API_URL}/student-assessments/`;
+    }
+
+    const queryParams: string[] = [];
+    if (details) queryParams.push(`details=${encodeURIComponent(details)}`);
+
+    if (queryParams.length) {
+      url += `?${queryParams.join("&")}`;
+    }
+
+    const res = await fetch(url);
+    const studentAttendance: StudentAssessment[] = await res.json();
+
+    return studentAttendance;
+  }
+
+  public async get({ id }: { id: number }): Promise<StudentAssessment> {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/student-attendances/${id}/`,
+    );
+    const student: StudentAssessment = await res.json();
+
+    return student;
+  }
+
+}
+
+export const studentAssessmentAdapter = new StudentAssessmentAdapter();
