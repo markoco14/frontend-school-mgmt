6bf166c70b56edeff8daf710856b59ae14f82070
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-05 11:24:19 +0800

    feature: add studentQuantity prop to StudentListSkeletonProp component

--- src/components/ui/skeleton/StudentListSkeletonProps.tsx
+++ src/components/ui/skeleton/StudentListSkeletonProps.tsx
@@ -1,26 +1,15 @@
-export default function StudentListSkeletonProps() {
+export default function StudentListSkeletonProps({studentQuantity}: {studentQuantity: number}) {
+
+  const students = Array.from({ length: studentQuantity }, (_, i) => i);
   return (
     <div className="grid gap-2">
-      <div className="flex h-[48px] items-center gap-2 rounded bg-gray-200 px-2 py-1">
-        <div className="aspect-square h-[40px] rounded-full bg-gray-300"></div>
-        <div className="h-[40px] w-full rounded bg-gray-300"></div>
-      </div>
-      <div className="flex h-[48px] items-center gap-2 rounded bg-gray-200 px-2 py-1">
-        <div className="aspect-square h-[40px] rounded-full bg-gray-300"></div>
-        <div className="h-[40px] w-full rounded bg-gray-300"></div>
-      </div>
-      <div className="flex h-[48px] items-center gap-2 rounded bg-gray-200 px-2 py-1">
-        <div className="aspect-square h-[40px] rounded-full bg-gray-300"></div>
-        <div className="h-[40px] w-full rounded bg-gray-300"></div>
-      </div>
-      <div className="flex h-[48px] items-center gap-2 rounded bg-gray-200 px-2 py-1">
-        <div className="aspect-square h-[40px] rounded-full bg-gray-300"></div>
-        <div className="h-[40px] w-full rounded bg-gray-300"></div>
-      </div>
-      <div className="flex h-[48px] items-center gap-2 rounded bg-gray-200 px-2 py-1">
-        <div className="aspect-square h-[40px] rounded-full bg-gray-300"></div>
-        <div className="h-[40px] w-full rounded bg-gray-300"></div>
-      </div>
+      {students.map((_, index: number) => (
+        <div key={index} className="flex h-[48px] items-center gap-2 rounded bg-gray-200 px-2 py-1">
+          <div className="aspect-square h-[40px] rounded-full bg-gray-300"></div>
+          <div className="h-[40px] w-full rounded bg-gray-300"></div>
+        </div>
+      ))}
+     
     </div>
   );
 }
