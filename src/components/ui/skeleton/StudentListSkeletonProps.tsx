ee6a9a556e27e6fa62e612a08e785b9fb47273b9
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-05 11:09:50 +0800

    refactor: organize Skeleton and SkeletonProps components in 'components/ui/skeleton'

---
+++ src/components/ui/skeleton/StudentListSkeletonProps.tsx
@@ -0,0 +1,26 @@
+export default function StudentListSkeletonProps() {
+  return (
+    <div className="grid gap-2">
+      <div className="flex h-[48px] items-center gap-2 rounded bg-gray-200 px-2 py-1">
+        <div className="aspect-square h-[40px] rounded-full bg-gray-300"></div>
+        <div className="h-[40px] w-full rounded bg-gray-300"></div>
+      </div>
+      <div className="flex h-[48px] items-center gap-2 rounded bg-gray-200 px-2 py-1">
+        <div className="aspect-square h-[40px] rounded-full bg-gray-300"></div>
+        <div className="h-[40px] w-full rounded bg-gray-300"></div>
+      </div>
+      <div className="flex h-[48px] items-center gap-2 rounded bg-gray-200 px-2 py-1">
+        <div className="aspect-square h-[40px] rounded-full bg-gray-300"></div>
+        <div className="h-[40px] w-full rounded bg-gray-300"></div>
+      </div>
+      <div className="flex h-[48px] items-center gap-2 rounded bg-gray-200 px-2 py-1">
+        <div className="aspect-square h-[40px] rounded-full bg-gray-300"></div>
+        <div className="h-[40px] w-full rounded bg-gray-300"></div>
+      </div>
+      <div className="flex h-[48px] items-center gap-2 rounded bg-gray-200 px-2 py-1">
+        <div className="aspect-square h-[40px] rounded-full bg-gray-300"></div>
+        <div className="h-[40px] w-full rounded bg-gray-300"></div>
+      </div>
+    </div>
+  );
+}
