85a43c513b684a79c043aedf814a1ca88e9bfbe3
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-05 11:15:40 +0800

    feature: add Skeleton to class_id page student list

--- src/modules/classes/infrastructure/ui/components/class-students/ManageClassStudents.tsx
+++ src/modules/classes/infrastructure/ui/components/class-students/ManageClassStudents.tsx
@@ -1,3 +1,4 @@
+import StudentListSkeletonProps from "@/src/components/ui/skeleton/StudentListSkeletonProps";
 import { Class } from "@/src/modules/classes/domain/entities/Class";
 import { ClassStudent } from "@/src/modules/classes/domain/entities/ClassStudent";
 import { useRouter } from "next/router";
@@ -6,47 +7,66 @@ import { classStudentAdapter } from "../../../adapters/classStudentAdapter";
 import AddClassStudent from "./AddClassStudent";
 import ClassStudentList from "./ClassStudentList";
 
-export default function ManageClassStudents({selectedClass}: {selectedClass: Class;}) {
-	const router = useRouter();
+export default function ManageClassStudents({
+  selectedClass,
+}: {
+  selectedClass: Class;
+}) {
+  const router = useRouter();
   const [classStudentList, setClassStudentList] = useState<ClassStudent[]>([]);
-	const [isAddingStudent, setIsAddingStudent] = useState<boolean>(false);
+  const [isAddingStudent, setIsAddingStudent] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
   useEffect(() => {
     async function getClassList() {
-      await classStudentAdapter.list({
-        class_id: Number(router?.query.class_id),
-        details: true,
-        order: 'last_name'
-      })
-      .then((res) => {
-        setClassStudentList(res);
-      })
+      setLoading(true);
+      await classStudentAdapter
+        .list({
+          class_id: Number(router?.query.class_id),
+          details: true,
+          order: "last_name",
+        })
+        .then((res) => {
+          setLoading(false);
+          setClassStudentList(res);
+        });
     }
     if (router) {
       getClassList();
     }
-  }, [router])
-	
-	return (
-		<div className="border-2 p-4 rounded">
-      <div className="flex justify-between items-baseline gap-4 mb-4">
+  }, [router]);
+
+  return (
+    <div className="rounded border-2 p-4">
+      <div className="mb-4 flex items-baseline justify-between gap-4">
         <h3 className="text-xl">Student List</h3>
         <button
           onClick={() => {
             setIsAddingStudent(!isAddingStudent);
           }}
         >
-          {isAddingStudent ? <span><i className="fa-solid fa-check"></i></span> : <span><i className="fa-solid fa-plus"></i> <i className="fa-solid fa-user"></i></span>}
+          {isAddingStudent ? (
+            <span>
+              <i className="fa-solid fa-check"></i>
+            </span>
+          ) : (
+            <span>
+              <i className="fa-solid fa-plus"></i>{" "}
+              <i className="fa-solid fa-user"></i>
+            </span>
+          )}
         </button>
       </div>
       {isAddingStudent ? (
         <AddClassStudent
-          selectedClass={selectedClass} 
+          selectedClass={selectedClass}
           classStudentList={classStudentList}
           setClassStudentList={setClassStudentList}
         />
+      ) : !loading ? (
+        <ClassStudentList classStudentList={classStudentList} />
       ) : (
-        <ClassStudentList classStudentList={classStudentList}/>
+        <StudentListSkeletonProps />
       )}
     </div>
-	);
+  );
 }
