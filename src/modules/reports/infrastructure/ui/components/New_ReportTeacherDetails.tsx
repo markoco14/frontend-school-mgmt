5783d68e7b509b858d1d7da421a4366ebc6e1828
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-07 15:07:47 +0800

    refactor: move SetStudentButtons component to separate file

--- src/modules/reports/infrastructure/ui/components/New_ReportTeacherDetails.tsx
+++ src/modules/reports/infrastructure/ui/components/New_ReportTeacherDetails.tsx
@@ -1,6 +1,7 @@
 import AuthContext from "@/src/AuthContext";
 import { classStudentAdapter } from "@/src/modules/classes/infrastructure/adapters/classStudentAdapter";
 import { evaluationAttributeAdapter } from "@/src/modules/evaluation/infrastructure/adapters/evaluationAdapter";
+import SetStudentButtons from "@/src/modules/evaluation/infrastructure/ui/components/SetStudentButtons";
 import { Student } from "@/src/modules/students/domain/entities/Student";
 import { Switch } from "@headlessui/react";
 import Image from "next/image";
@@ -285,68 +286,6 @@ const EvaluationAttribute = ({ attribute }: { attribute: any }) => {
       {attribute?.data_type_id === 8 && (
         <EvaluationTextAttribute attribute={attribute} />
       )}
-    </div>
-  );
-};
-
-const SetStudentButtons = ({
-  students,
-  selectedStudent,
-  setSelectedStudent,
-}: {
-  students: any[];
-  selectedStudent: any;
-  setSelectedStudent: Function;
-}) => {
-  const currentIndex = students.findIndex((s) => s === selectedStudent);
-
-  const selectNext = () => {
-    if (currentIndex < students.length - 1) {
-      setSelectedStudent(students[currentIndex + 1]);
-    }
-  };
-
-  const selectPrevious = () => {
-    if (currentIndex > 0) {
-      setSelectedStudent(students[currentIndex - 1]);
-    }
-  };
-
-  return (
-    <div className="flex gap-2">
-      <button
-        disabled={currentIndex === 0}
-        onClick={selectPrevious}
-        className="grid w-24 place-items-center rounded border border-blue-500 hover:bg-blue-300 disabled:cursor-not-allowed disabled:border-gray-500 disabled:bg-gray-300 disabled:hover:bg-gray-400 sm:w-32"
-      >
-        {students[currentIndex - 1]?.first_name ? (
-          students[currentIndex - 1]?.first_name
-        ) : (
-          <i className="fa-solid fa-xmark"></i>
-        )}
-      </button>
-      <button
-        onClick={() => {
-          toast.success(`${selectedStudent?.first_name}'s report saved!`);
-          if (currentIndex + 1 !== students.length) {
-            setSelectedStudent(students[currentIndex + 1]);
-          }
-        }}
-        className="w-24 rounded bg-blue-500 px-2 py-1 text-white shadow hover:bg-blue-600 disabled:bg-gray-300 sm:w-32 "
-      >
-        Save
-      </button>
-      <button
-        disabled={currentIndex + 1 === students.length}
-        onClick={selectNext}
-        className="grid w-24 place-items-center rounded border border-blue-500 hover:bg-blue-300 disabled:cursor-not-allowed disabled:border-gray-500 disabled:bg-gray-300 disabled:hover:bg-gray-400 sm:w-32"
-      >
-        {students[currentIndex + 1]?.first_name ? (
-          students[currentIndex + 1]?.first_name
-        ) : (
-          <i className="fa-solid fa-xmark items-center"></i>
-        )}
-      </button>
     </div>
   );
 };
