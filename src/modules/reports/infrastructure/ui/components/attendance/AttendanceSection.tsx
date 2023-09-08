9d57ff06c4a4ed1ebcbfb46b053f774478417074
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-08 19:48:30 +0800

    refactor: move Attendance section logic to component and clean reports index file

--- src/modules/reports/infrastructure/ui/components/attendance/AttendanceSection.tsx
+++ src/modules/reports/infrastructure/ui/components/attendance/AttendanceSection.tsx
@@ -1,67 +1,108 @@
-import ClassListSkeletonProps from "@/src/components/ui/skeleton/ClassListSkeletonProps";
 import { Skeleton } from "@/src/components/ui/skeleton/Skeleton";
 import StudentListSkeletonProps from "@/src/components/ui/skeleton/StudentListSkeletonProps";
 import { StudentAttendance } from "@/src/modules/attendance/domain/entities/StudentAttendance";
+import { studentAttendanceAdapter } from "@/src/modules/attendance/infrastructure/adapters/studentAttendanceAdapter";
 import AttendanceReasonForm from "@/src/modules/attendance/infrastructure/ui/components/AttendanceReasonForm";
-import ClassList from "@/src/modules/attendance/infrastructure/ui/components/ClassList";
 import StudentAttendanceList from "@/src/modules/attendance/infrastructure/ui/components/StudentAttendanceList";
-import { ClassEntity } from "@/src/modules/classes/domain/entities/ClassEntity";
 import Modal from "@/src/modules/core/infrastructure/ui/components/Modal";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const AttendanceSection = ({
-	selectedClass,
-	loading,
-	loadingAttendance,
-  classAttendance,
-	setIsWriteNote,
-	setSelectedAttendance,
-	isWriteNote,
-	handleClose,
-	selectedAttendance,
-	setClassAttendance,
-	handleUpdateAttendance,
+  selectedClass,
+  date,
 }: {
-	selectedClass: any;
-	loading: any;
-	loadingAttendance: any;
-  classAttendance: any;
-	setIsWriteNote: any;
-	setSelectedAttendance: any;
-	isWriteNote: any;
-	handleClose: any;
-	selectedAttendance: any;
-	setClassAttendance: any;
-	handleUpdateAttendance: any;
+  selectedClass: any;
+  date: Date;
 }) => {
-	
+  const [isWriteNote, setIsWriteNote] = useState<boolean>(false);
+  const [selectedAttendance, setSelectedAttendance] =
+    useState<StudentAttendance>();
+  const [loadingAttendance, setLoadingAttendance] = useState<boolean>(false);
+  const [classAttendance, setClassAttendance] = useState<StudentAttendance[]>(
+    [],
+  );
 
-	useEffect(() => {
+  function handleClose() {
+    setIsWriteNote(false);
+    setSelectedAttendance(undefined);
+  }
 
-	}, [])
+  const handleUpdateAttendance = ({
+    newAttendance,
+  }: {
+    newAttendance: StudentAttendance;
+  }) => {
+    // Use map to create a new array
+    const updatedAttendance = classAttendance.map(
+      (attendance: StudentAttendance) => {
+        // Find the attendance object that matches the ID of the newAttendance
+        if (attendance.id === newAttendance.id) {
+          // Replace it with newAttendance
+          return newAttendance;
+        }
+        // Leave all other objects unchanged
+        return attendance;
+      },
+    );
+
+    // Update the state
+    setClassAttendance(updatedAttendance);
+  };
+
+  async function getAttendanceList({
+    school_class,
+    date,
+  }: {
+    school_class: number | undefined;
+    date: string | undefined;
+  }) {
+    setLoadingAttendance(true);
+    await studentAttendanceAdapter
+      .list({ school_class: school_class, date: date, details: true })
+      .then((res) => {
+        setClassAttendance(res);
+        setLoadingAttendance(false);
+      });
+  }
+
+  useEffect(() => {
+    async function getAttendance() {
+      setLoadingAttendance(true);
+      await studentAttendanceAdapter
+        .list({
+          school_class: selectedClass?.id,
+          date: date.toISOString().split("T")[0],
+          details: true,
+        })
+        .then((res) => {
+          setClassAttendance(res);
+          setLoadingAttendance(false);
+        });
+    }
+
+    getAttendance();
+  }, [setClassAttendance, date, selectedClass?.id]);
+
   return (
     <>
-     
-      <article className="col-span-4 grid gap-4 rounded border bg-white p-4 shadow">
-        <div>
-          <h2 className="mb-2 text-2xl">
-            Class: {selectedClass?.name} Teacher {selectedClass?.teacher}
-          </h2>
-          <p>Track student attendance below.</p>
-        </div>
-        {loading || loadingAttendance ? (
-          <Skeleton>
-            <StudentListSkeletonProps studentQuantity={4} />
-          </Skeleton>
-        ) : (
-          <StudentAttendanceList
-            classAttendance={classAttendance}
-            setIsWriteNote={setIsWriteNote}
-            setSelectedAttendance={setSelectedAttendance}
-            handleUpdateAttendance={handleUpdateAttendance}
-          />
-        )}
-      </article>
+      <div>
+        <h2 className="mb-2 text-2xl">
+          Class: {selectedClass?.name} Teacher {selectedClass?.teacher}
+        </h2>
+        <p>Track student attendance below.</p>
+      </div>
+      {loadingAttendance ? (
+        <Skeleton>
+          <StudentListSkeletonProps studentQuantity={8} />
+        </Skeleton>
+      ) : (
+        <StudentAttendanceList
+          classAttendance={classAttendance}
+          setIsWriteNote={setIsWriteNote}
+          setSelectedAttendance={setSelectedAttendance}
+          handleUpdateAttendance={handleUpdateAttendance}
+        />
+      )}
       <Modal
         show={isWriteNote}
         close={handleClose}
--- src/pages/reports/index.tsx
+++ src/pages/reports/index.tsx
@@ -1,17 +1,11 @@
 import AuthContext from "@/src/AuthContext";
 import ClassListSkeletonProps from "@/src/components/ui/skeleton/ClassListSkeletonProps";
 import { Skeleton } from "@/src/components/ui/skeleton/Skeleton";
-import StudentListSkeletonProps from "@/src/components/ui/skeleton/StudentListSkeletonProps";
-import { StudentAttendance } from "@/src/modules/attendance/domain/entities/StudentAttendance";
-import { studentAttendanceAdapter } from "@/src/modules/attendance/infrastructure/adapters/studentAttendanceAdapter";
-import AttendanceReasonForm from "@/src/modules/attendance/infrastructure/ui/components/AttendanceReasonForm";
 import ClassList from "@/src/modules/attendance/infrastructure/ui/components/ClassList";
-import StudentAttendanceList from "@/src/modules/attendance/infrastructure/ui/components/StudentAttendanceList";
 import { ClassEntity } from "@/src/modules/classes/domain/entities/ClassEntity";
 import { classAdapter } from "@/src/modules/classes/infrastructure/adapters/classAdapter";
 import DateChangeButtons from "@/src/modules/core/infrastructure/ui/components/DateChangeButtons";
 import Layout from "@/src/modules/core/infrastructure/ui/components/Layout";
-import Modal from "@/src/modules/core/infrastructure/ui/components/Modal";
 import PageTabNavigation from "@/src/modules/core/infrastructure/ui/components/PageTabNavigation";
 import PermissionDenied from "@/src/modules/core/infrastructure/ui/components/PermissionDenied";
 import DailyReportOverview from "@/src/modules/reports/infrastructure/ui/components/DailyReportOverview";
@@ -23,7 +17,6 @@ export default function ReportsHome() {
   const { selectedSchool } = useContext(AuthContext);
   const [date, setDate] = useState<Date>(new Date());
   const [loading, setLoading] = useState<boolean>(false);
-  const [loadingAttendance, setLoadingAttendance] = useState<boolean>(false);
 
   const [tab, setTab] = useState<number>(1);
 
@@ -43,14 +36,8 @@ export default function ReportsHome() {
   ];
 
   const { user } = useContext(AuthContext);
-  // const date = new Date();
-  // date.setDate(date.getDate() + 1);
   const dayNumber = date.getDay();
-  const year = date.getFullYear(); // Get the year
-  const month = String(date.getMonth() + 1).padStart(2, "0"); // Get the month and pad with 0 if needed
-  const day = String(date.getDate()).padStart(2, "0"); // Get the day and pad with 0 if needed
-
-  const formattedDate = `${year}-${month}-${day}`; // Format the date
+  const formattedDate = date.toISOString().split("T")[0]; // Format the date
   const days = [
     "Sunday",
     "Monday",
@@ -63,13 +50,7 @@ export default function ReportsHome() {
   const dayName = days[dayNumber];
 
   const [todayClasses, setTodayClasses] = useState<ClassEntity[]>([]);
-  const [classAttendance, setClassAttendance] = useState<StudentAttendance[]>(
-    [],
-  );
   const [selectedClass, setSelectedClass] = useState<ClassEntity>();
-  const [isWriteNote, setIsWriteNote] = useState<boolean>(false);
-  const [selectedAttendance, setSelectedAttendance] =
-    useState<StudentAttendance>();
 
   useEffect(() => {
     async function getClasses() {
@@ -78,7 +59,7 @@ export default function ReportsHome() {
         .list({ school_id: selectedSchool.id, day: dayName })
         .then((res) => {
           setSelectedClass(res[0]);
-          getAttendanceList({ school_class: res[0]?.id, date: formattedDate });
+          // getAttendanceList({ school_class: res[0]?.id, date: formattedDate });
           setTodayClasses(res);
           setLoading(false);
         });
@@ -95,54 +76,10 @@ export default function ReportsHome() {
     );
   }
 
-  async function getAttendanceList({
-    school_class,
-    date,
-  }: {
-    school_class: number | undefined;
-    date: string | undefined;
-  }) {
-    setLoadingAttendance(true);
-    await studentAttendanceAdapter
-      .list({ school_class: school_class, date: date, details: true })
-      .then((res) => {
-        setClassAttendance(res);
-        setLoadingAttendance(false);
-      });
-  }
-
-  const handleUpdateAttendance = ({
-    newAttendance,
-  }: {
-    newAttendance: StudentAttendance;
-  }) => {
-    // Use map to create a new array
-    const updatedAttendance = classAttendance.map((attendance) => {
-      // Find the attendance object that matches the ID of the newAttendance
-      if (attendance.id === newAttendance.id) {
-        // Replace it with newAttendance
-        return newAttendance;
-      }
-      // Leave all other objects unchanged
-      return attendance;
-    });
-
-    // Update the state
-    setClassAttendance(updatedAttendance);
-  };
-
-  function handleClose() {
-    setIsWriteNote(false);
-    setSelectedAttendance(undefined);
-  }
-
   function handleChangeClass({ classEntity }: { classEntity: ClassEntity }) {
     setSelectedClass(classEntity);
-    getAttendanceList({
-      school_class: classEntity.id,
-      date: formattedDate,
-    });
   }
+
   return (
     <Layout>
       <div className="grid gap-4 divide-y">
@@ -156,7 +93,6 @@ export default function ReportsHome() {
           <div className="bg-white sm:col-span-8">
             <PageTabNavigation links={links} tab={tab} setTab={setTab} />
           </div>
-          {/* Class Attendance Logic */}
           {tab !== 3 && (
             <>
               <article className="col-span-4 flex flex-col gap-4 rounded border bg-white p-4 shadow">
@@ -179,19 +115,9 @@ export default function ReportsHome() {
                 )}
               </article>
               {tab === 1 && (
-                <AttendanceSection 
-                  selectedClass={selectedClass}
-                  loading={loading}
-                  loadingAttendance={loadingAttendance}
-                  classAttendance={classAttendance}
-                  setIsWriteNote={setIsWriteNote}
-                  setSelectedAttendance={setSelectedAttendance}
-                  isWriteNote={isWriteNote}
-                  handleClose={handleClose}
-                  selectedAttendance={selectedAttendance}
-                  setClassAttendance={setClassAttendance}
-                  handleUpdateAttendance={handleUpdateAttendance}
-                />
+                <article className="col-span-4 grid gap-4 rounded border bg-white p-4 shadow">
+                  <AttendanceSection date={date} selectedClass={selectedClass} />
+                </article>
               )}
               {tab === 2 && (
                 <article className="col-span-4 grid gap-4 rounded border bg-white p-4 shadow">
@@ -204,7 +130,6 @@ export default function ReportsHome() {
         {tab === 3 && (
           <section className="rounded border p-2 shadow">
             <p>daily reports</p>
-            {/* FAKE TEACHER DATA */}
             <DailyReportOverview date={date} />
           </section>
         )}
