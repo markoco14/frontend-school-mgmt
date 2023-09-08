b9c3909be4df661b80c42ab5c6eb8f923f37f257
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-08 10:18:40 +0800

    feature: create StudentEvaluation entity in 'evaluation' module

---
+++ src/modules/evaluation/domain/entities/StudentEvaluation.ts
@@ -0,0 +1,14 @@
+export class StudentEvaluation {
+  constructor(
+    public id: number,
+		public evaluation_type: number,
+		public student_id: number,
+		public author_id: number,
+		public date: string,
+		public evaluation_attribute_id: number,
+		public evaluation_value: string | number,
+		public class_id: number,
+		public subject_id: number,
+		public level_id: number,
+  ) {}
+}
