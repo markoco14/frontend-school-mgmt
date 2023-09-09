68146371401304fc2ef922c03a7fb3c264e5557d
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-09 19:04:38 +0800

    feature: set up loading state and links to report/evaluations page

--- src/modules/reports/infrastructure/ui/components/TodayClassList.tsx
+++ src/modules/reports/infrastructure/ui/components/TodayClassList.tsx
@@ -1,4 +1,6 @@
 import AuthContext from "@/src/AuthContext";
+import ClassListSkeletonProps from "@/src/components/ui/skeleton/ClassListSkeletonProps";
+import { Skeleton } from "@/src/components/ui/skeleton/Skeleton";
 import { ClassEntity } from "@/src/modules/classes/domain/entities/ClassEntity";
 import { classAdapter } from "@/src/modules/classes/infrastructure/adapters/classAdapter";
 import { format } from "date-fns";
@@ -6,13 +8,14 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { useContext, useEffect, useState } from "react";
 
+
 export const TodayClassList = () => {
   const router = useRouter();
   const { selectedSchool } = useContext(AuthContext);
   const [classEntities, setClasses] = useState<ClassEntity[]>();
   const [date, setDate] = useState<Date>(new Date());
   const dayNumber = date.getDay();
-
+  const [loading, setLoading] = useState<boolean>(false);
 
   const formattedDateString = date.toISOString().split("T")[0];
 
@@ -30,12 +33,14 @@ export const TodayClassList = () => {
 
   useEffect(() => {
     async function getData() {
+      setLoading(true);
       await classAdapter
         .list({
           school_id: selectedSchool?.id,
           day: dayName,
         })
         .then((res) => {
+          setLoading(false);
           setClasses(() => res);
         });
     }
@@ -100,52 +105,30 @@ export const TodayClassList = () => {
       </h2>
       <div className="mb-2 flex gap-4"></div>
       <hr className="mb-2"></hr>
-      <ul className="flex flex-col gap-2 divide-y">
-        {!classEntities?.length ? (
-          <p>There are no classes today</p>
-        ) : (
-          classEntities?.map((classEntity: ClassEntity, index: number) => (
+      {loading && (
+        <Skeleton>
+          <ClassListSkeletonProps />
+        </Skeleton>
+      )}
+      {!loading && classEntities?.length === 0 ? (
+        <p className="p-2">There are no classes today</p>
+      ) : (
+        <ul>
+          {classEntities?.map((classEntity) => (
             <li
-              key={index}
-              className="flex items-baseline justify-between rounded-md p-2 hover:bg-blue-200"
+              key={`class-${classEntity.id}`}
+              className="flex justify-between p-2"
             >
-              <span>
-                {classEntity.name} {classEntity.teacher}
-              </span>
-              <Link href={`reports/evaluations/${classEntity.id}/${formattedDateString}`}
-                onClick={() => {
-                  console.log(`writing evaluations for ${classEntity.name}`);
-                }}
+              <span>{classEntity.name}</span>
+              <Link
+                href={`reports/evaluations/${classEntity.id}/${formattedDateString}/`}
               >
                 Evaluations
               </Link>
-              {/* <div className="flex gap-4">
-                {classEntity.class_list?.length === 0 ? (
-                  <Link
-                    href={`/classes/${classEntity.id}`}
-                    className="rounded bg-gray-300 px-2 py-1 disabled:cursor-not-allowed"
-                  >
-                    Add Students
-                  </Link>
-                ) : (
-                  <button
-                    disabled={classEntity.class_list?.length === 0}
-                    className="rounded bg-blue-300 px-2 py-1 disabled:cursor-not-allowed"
-                    onClick={() => {
-                      checkOrCreateReports(
-                        classEntity,
-                        format(date, "yyyy-MM-dd"),
-                      );
-                    }}
-                  >
-                    Write reports
-                  </button>
-                )}
-              </div> */}
             </li>
-          ))
-        )}
-      </ul>
+          ))}
+        </ul>
+      )}
     </>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,13 @@ import { useContext, useEffect, useState } from "react";
 import AuthContext from "../AuthContext";
 import Layout from "../modules/core/infrastructure/ui/components/Layout";
 import SchoolHeader from "../modules/core/infrastructure/ui/components/SchoolHeader";
-import { ReportList, TodayClassList } from "../modules/reports/infrastructure/ui/components/TodayClassList";
+import { TodayClassList } from "../modules/reports/infrastructure/ui/components/TodayClassList";
 import { School } from "../modules/school-mgmt/domain/entities/School";
 import { schoolAdapter } from "../modules/school-mgmt/infrastructure/adapters/schoolAdapter";
 import Login from "../modules/user-mgmt/infrastructure/ui/Login";
 import Signup from "../modules/user-mgmt/infrastructure/ui/Signup";
 
 export default function Home() {
-
   const { user, selectedSchool, handleSelectSchool } = useContext(AuthContext);
   const [isSignUp, setIsSignUp] = useState<boolean>(true);
   const [schools, setSchools] = useState<School[]>([]);
@@ -19,88 +18,92 @@ export default function Home() {
   useEffect(() => {
     async function getData() {
       if (user) {
-        await schoolAdapter.listUserSchools({id: user?.user_id})
+        await schoolAdapter
+          .listUserSchools({ id: user?.user_id })
           .then((res) => {
             setSchools(res);
-          })
+          });
       }
     }
     getData();
-  }, [user])
+  }, [user]);
 
   return (
     <Layout>
-      
-        {!user && (
-          <section>
-            <div className="flex gap-4 mb-8">
-              <button
-                onClick={() => setIsSignUp(true)}
-                className={`${
-                  isSignUp
-                    ? "underline decoration-4 decoration-blue-500 underline-offset-[6px]"
-                    : null
-                }`}
-              >
-                Sign Up
-              </button>
-              <button
-                onClick={() => setIsSignUp(false)}
-                className={`${
-                  !isSignUp
-                    ? "underline decoration-4 decoration-blue-500 underline-offset-[6px]"
-                    : null
-                }`}
-              >
-                Log In
-              </button>
-            </div>
+      {!user && (
+        <section>
+          <div className="mb-8 flex gap-4">
+            <button
+              onClick={() => setIsSignUp(true)}
+              className={`${
+                isSignUp
+                  ? "underline decoration-blue-500 decoration-4 underline-offset-[6px]"
+                  : null
+              }`}
+            >
+              Sign Up
+            </button>
+            <button
+              onClick={() => setIsSignUp(false)}
+              className={`${
+                !isSignUp
+                  ? "underline decoration-blue-500 decoration-4 underline-offset-[6px]"
+                  : null
+              }`}
+            >
+              Log In
+            </button>
+          </div>
 
-            {isSignUp ? (
-              <Signup />
-            ) : (
-              <Login />
-            )}
-          </section>
-        )}
+          {isSignUp ? <Signup /> : <Login />}
+        </section>
+      )}
 
-        {user && !selectedSchool && (
-          <section>
-            <h2 className="text-3xl mb-4">Welcome back!</h2>
-            <p className='mb-4'><strong>Managing</strong> your school and student <strong>data</strong> has never been <strong>easier</strong>.</p>
-            <ul className="flex flex-col gap-2 mb-4">
-              {schools?.map((school: School, index: number) => (
-                <li 
-                  key={index}
-                  className="p-2 rounded hover:bg-blue-300"
-                >
-                  <button 
+      {user && !selectedSchool && (
+        <section>
+          <h2 className="mb-4 text-3xl">Welcome back!</h2>
+          <p className="mb-4">
+            <strong>Managing</strong> your school and student{" "}
+            <strong>data</strong> has never been <strong>easier</strong>.
+          </p>
+          <ul className="mb-4 flex flex-col gap-2">
+            {schools?.map((school: School, index: number) => (
+              <li key={index} className="rounded p-2 hover:bg-blue-300">
+                <button
                   onClick={() => {
-                  if (!selectedSchool) {
-                    handleSelectSchool(school);
-                  }
-                }}>{school.name}</button></li>
-              ))}
-            </ul>
-            {user.role === 'OWNER' && (
-              <Link href="/school-mgmt/add" className="bg-blue-300 rounded-lg p-2 hover:bg-blue-500">Add School</Link>
-            )}
-          </section>
-        )}
+                    if (!selectedSchool) {
+                      handleSelectSchool(school);
+                    }
+                  }}
+                >
+                  {school.name}
+                </button>
+              </li>
+            ))}
+          </ul>
+          {user.role === "OWNER" && (
+            <Link
+              href="/school-mgmt/add"
+              className="rounded-lg bg-blue-300 p-2 hover:bg-blue-500"
+            >
+              Add School
+            </Link>
+          )}
+        </section>
+      )}
 
-        {user && selectedSchool && (
-          <section>
-            <SchoolHeader />
-            <div className="text-3xl mb-4 grid sm:grid-cols-4">
-              <Link href='reports/sample1/'>Sample 1</Link>
-              <Link href='reports/sample2/'>Sample 2</Link>
-              <Link href='reports/sample3/'>Sample 3</Link>
-              <Link href='reports/sample4/'>Sample 4</Link>
-            </div>
-            <TodayClassList />
-          </section>
-        )}
-      
+      {user && selectedSchool && (
+        <section>
+          <SchoolHeader />
+          <div className="mb-4 grid text-3xl sm:grid-cols-4">
+            <Link href="reports/sample1/">Sample 1</Link>
+            <Link href="reports/sample2/">Sample 2</Link>
+            <Link href="reports/sample3/">Sample 3</Link>
+            <Link href="reports/sample4/">Sample 4</Link>
+          </div>
+          <TodayClassList />
+        </section>
+      )}
     </Layout>
   );
 }
