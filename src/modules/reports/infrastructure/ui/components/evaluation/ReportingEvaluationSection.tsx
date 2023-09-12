5d651b9731d39e26e3df35c8ceae1b87926ddc26
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-12 22:34:32 +0800

    feature: change reports evaluations sectin to presentStudentsWithEvaluations

--- src/modules/reports/infrastructure/ui/components/evaluation/ReportingEvaluationSection.tsx
+++ src/modules/reports/infrastructure/ui/components/evaluation/ReportingEvaluationSection.tsx
@@ -1,12 +1,12 @@
-import ClassListSkeletonProps from "@/src/components/ui/skeleton/ClassListSkeletonProps";
 import EvaluationListSkeletonProps from "@/src/components/ui/skeleton/EvaluationListSkeletonProps";
 import { Skeleton } from "@/src/components/ui/skeleton/Skeleton";
-import StudentListSkeletonProps from "@/src/components/ui/skeleton/StudentListSkeletonProps";
 import { ClassEntity } from "@/src/modules/classes/domain/entities/ClassEntity";
 import { StudentEvaluation } from "@/src/modules/evaluation/domain/entities/StudentEvaluation";
-import { studentEvaluationAdapter } from "@/src/modules/evaluation/infrastructure/adapters/studentEvaluationAdapter";
+import { Student } from "@/src/modules/students/domain/entities/Student";
 import { StudentEvaluationFilters } from "@/src/modules/students/domain/entities/StudentEvaluationFilters";
-import { Dictionary, groupBy } from "lodash";
+import { studentAdapter } from "@/src/modules/students/infrastructure/adapters/studentAdapter";
+import { Dictionary } from "lodash";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 const ReportingEvaluationSection = ({
@@ -17,8 +17,8 @@ const ReportingEvaluationSection = ({
   selectedClass: ClassEntity | undefined;
 }) => {
   const [loading, setLoading] = useState<boolean>(false);
-  const [evaluations, setEvaluations] =
-    useState<Dictionary<StudentEvaluation[]>>();
+  const [studentsWithEvaluations, setStudentsWithEvaluations] =
+    useState<Student[]>();
   const [filters, setFilters] = useState<StudentEvaluationFilters>({});
   // const [date, setDate] = useState<Date>(new Date());
 
@@ -26,18 +26,19 @@ const ReportingEvaluationSection = ({
     async function getEvaluations() {
       const preparedDate = date?.toISOString().split("T")[0];
       setLoading(true);
-      await studentEvaluationAdapter
-        .list({
+      await studentAdapter
+        .listPresentStudentsWithEvaluations({
           date: preparedDate,
-          class_id: selectedClass?.id,
-          filters: filters,
+          classId: Number(selectedClass?.id),
+          // filters: filters,
         })
         .then((res) => {
-          const groupedData = groupBy(
-            res,
-            (evaluation) => evaluation.student?.last_name,
-          );
-          setEvaluations(groupedData);
+          // const groupedData = groupBy(
+          //   res,
+          //   (evaluation) => evaluation.student?.last_name,
+          // );
+          console.log(res);
+          setStudentsWithEvaluations(res);
           setLoading(false);
         });
     }
@@ -58,72 +59,49 @@ const ReportingEvaluationSection = ({
     return Object.keys(obj).length === 0 && obj.constructor === Object;
   }
   return (
-    <ul className="grid gap-4">
-      {loading && (
+    <>
+      {loading ? (
         <Skeleton>
           <EvaluationListSkeletonProps />
         </Skeleton>
-      )}
-      {!loading &&
-        evaluations &&
-        isEmpty(evaluations) &&
-        isSatOrSun({ date: date }) && (
-          <p className="rounded-lg border bg-white p-4 text-2xl">
-            No school today
-          </p>
-        )}
-      {!loading &&
-        evaluations &&
-        isEmpty(evaluations) &&
-        !isSatOrSun({ date: date }) && (
-          <p className="rounded-lg border bg-white p-4 text-2xl">
-            No evaluations yet
-          </p>
-        )}
-      {!loading &&
-        evaluations &&
-        Object.entries(evaluations).map(([studentId, studentData]) => (
-          <li
-            key={studentId}
-            className="grid gap-2 rounded-lg border bg-white p-4"
-          >
-            <p className="text-xl">
-              {studentData[0].student?.first_name}{" "}
-              {studentData[0].student?.last_name}{" "}
-            </p>
-            <div className="grid grid-cols-2 gap-2">
-              {studentData
-                .sort(
-                  (a: StudentEvaluation, b: StudentEvaluation) =>
-                    a.evaluation_attribute_id - b.evaluation_attribute_id,
-                )
-                .map((evaluation: any) => (
-                  <div
+      ) : (
+        <ul className="grid gap-4">
+          {studentsWithEvaluations?.map((student) => (
+            <li key={student.id} className="grid gap-4 rounded-lg bg-white p-4 shadow">
+              <div className="flex gap-4 items-center">
+                <div className="relative col-span-1 flex justify-center">
+                  <Image
+                    src={student?.photo_url}
+                    alt={`An image of ${student.first_name}`}
+                    width={50}
+                    height={50}
+                    style={{ objectFit: "cover" }}
+                    className="rounded-full"
+                  />
+                </div>
+                <p>
+                  {student.first_name} {student.last_name}
+                </p>
+              </div>
+              <div className="grid grid-cols-2">
+                {student.evaluations_for_day?.map((evaluation) => (
+                  <p
                     key={evaluation.id}
                     className={`${
-                      evaluation.evaluation_attribute_id !== 3
-                        ? "col-span-1"
-                        : "col-span-2"
+                      evaluation.evaluation_attribute_id === 3
+                        ? "col-span-2"
+                        : "col-span-1"
                     }`}
                   >
-                    {evaluation.evaluation_attribute_id !== 3 ? (
-                      <div className="rounded border p-2 shadow">
-                        <p>
-                          {evaluation.evaluation_attribute.name}:{" "}
-                          {evaluation.evaluation_value}
-                        </p>
-                      </div>
-                    ) : (
-                      <div className="rounded border p-2 shadow">
-                        <p>{evaluation.evaluation_value}</p>
-                      </div>
-                    )}
-                  </div>
+                    {evaluation.evaluation_value}
+                  </p>
                 ))}
-            </div>
-          </li>
-        ))}
-    </ul>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 };
 
