048ddc054b5c9e2b2f68ba270d9507b56143a1e6
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-13 10:40:40 +0800

    feature: create bathDelete student evaluation method with button on frontend

--- src/modules/reports/infrastructure/ui/components/evaluation/ReportingEvaluationSection.tsx
+++ src/modules/reports/infrastructure/ui/components/evaluation/ReportingEvaluationSection.tsx
@@ -2,12 +2,14 @@ import EvaluationListSkeletonProps from "@/src/components/ui/skeleton/Evaluation
 import { Skeleton } from "@/src/components/ui/skeleton/Skeleton";
 import { ClassEntity } from "@/src/modules/classes/domain/entities/ClassEntity";
 import { StudentEvaluation } from "@/src/modules/evaluation/domain/entities/StudentEvaluation";
+import { studentEvaluationAdapter } from "@/src/modules/evaluation/infrastructure/adapters/studentEvaluationAdapter";
 import { Student } from "@/src/modules/students/domain/entities/Student";
 import { StudentEvaluationFilters } from "@/src/modules/students/domain/entities/StudentEvaluationFilters";
 import { studentAdapter } from "@/src/modules/students/infrastructure/adapters/studentAdapter";
 import { Dictionary } from "lodash";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 const ReportingEvaluationSection = ({
   date,
@@ -58,6 +60,31 @@ const ReportingEvaluationSection = ({
   function isEmpty(obj: Dictionary<StudentEvaluation[]>) {
     return Object.keys(obj).length === 0 && obj.constructor === Object;
   }
+
+  
+
+  async function handleDeleteEvaluations({student_id}: {student_id: number}) {
+    await studentEvaluationAdapter
+      .batchDelete({
+        student_id: student_id,
+        date: date.toISOString().split("T")[0], // gets "2023-09-07 YYYY-mm-dd format"
+      })
+      .then((res) => {
+        const updatedEvaluations = studentsWithEvaluations?.map(
+          (student: Student) => {
+            // Find the attendance object that matches the ID of the newAttendance
+            if (student.id === student_id) {
+              // Replace it with newAttendance
+              student.evaluations_for_day = null;
+            }
+            // Leave all other objects unchanged
+            return student;
+          },
+        );
+        setStudentsWithEvaluations(updatedEvaluations)
+        toast.success("deleted");
+      });
+  }
   return (
     <>
       {loading ? (
@@ -66,39 +93,63 @@ const ReportingEvaluationSection = ({
         </Skeleton>
       ) : (
         <ul className="grid gap-4">
-          {studentsWithEvaluations?.map((student) => (
-            <li key={student.id} className="grid gap-4 rounded-lg bg-white p-4 shadow">
-              <div className="flex gap-4 items-center">
-                <div className="relative col-span-1 flex justify-center">
-                  <Image
-                    src={student?.photo_url}
-                    alt={`An image of ${student.first_name}`}
-                    width={50}
-                    height={50}
-                    style={{ objectFit: "cover" }}
-                    className="rounded-full"
-                  />
-                </div>
-                <p>
-                  {student.first_name} {student.last_name}
-                </p>
-              </div>
-              <div className="grid grid-cols-2">
-                {student.evaluations_for_day?.map((evaluation) => (
-                  <p
-                    key={evaluation.id}
-                    className={`${
-                      evaluation.evaluation_attribute_id === 3
-                        ? "col-span-2"
-                        : "col-span-1"
-                    }`}
+          {!studentsWithEvaluations?.length ? (
+            <p className="grid gap-4 rounded-lg bg-white p-4 shadow">
+              Evaluations not created
+            </p>
+          ) : (
+            studentsWithEvaluations?.map((student) => (
+              <li
+                key={student.id}
+                className="grid gap-4 rounded-lg bg-white p-4 shadow"
+              >
+                <div className="flex justify-between">
+                  <div className="flex items-center gap-4">
+                    <div className="relative col-span-1 flex justify-center">
+                      <Image
+                        src={student?.photo_url}
+                        alt={`An image of ${student.first_name}`}
+                        width={50}
+                        height={50}
+                        style={{ objectFit: "cover" }}
+                        className="rounded-full"
+                      />
+                    </div>
+                    <p>
+                      {student.first_name} {student.last_name}
+                    </p>
+                  </div>
+                  <button
+                    onClick={() =>
+                      handleDeleteEvaluations({
+                        student_id: student.id,
+                      })
+                    }
                   >
-                    {evaluation.evaluation_value}
-                  </p>
-                ))}
-              </div>
-            </li>
-          ))}
+                    delete
+                  </button>
+                </div>
+                <div className="grid grid-cols-2">
+                  {!student.evaluations_for_day ? (
+                    <p>No student evaluations today.</p>
+                  ) : (
+                    student.evaluations_for_day.map((evaluation) => (
+                      <p
+                        key={evaluation.id}
+                        className={`${
+                          evaluation.evaluation_attribute_id === 3
+                            ? "col-span-2"
+                            : "col-span-1"
+                        }`}
+                      >
+                        {evaluation.evaluation_value}
+                      </p>
+                    ))
+                  )}
+                </div>
+              </li>
+            ))
+          )}
         </ul>
       )}
     </>
