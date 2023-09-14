c5bb8b207326ef2633af3423093740fdf0f13a15
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-14 10:55:28 +0800

    feature: implement ParamsPageTabNav on reports page

--- src/pages/reports/index.tsx
+++ src/pages/reports/index.tsx
@@ -6,32 +6,36 @@ import { ClassEntity } from "@/src/modules/classes/domain/entities/ClassEntity";
 import { classAdapter } from "@/src/modules/classes/infrastructure/adapters/classAdapter";
 import DateChangeButtons from "@/src/modules/core/infrastructure/ui/components/DateChangeButtons";
 import Layout from "@/src/modules/core/infrastructure/ui/components/Layout";
-import PageTabNavigation from "@/src/modules/core/infrastructure/ui/components/PageTabNavigation";
+import ParamsPageTabNav from "@/src/modules/core/infrastructure/ui/components/ParamsPageTabNav";
 import PermissionDenied from "@/src/modules/core/infrastructure/ui/components/PermissionDenied";
 import DailyReportOverview from "@/src/modules/reports/infrastructure/ui/components/DailyReportOverview";
 import AttendanceSection from "@/src/modules/reports/infrastructure/ui/components/attendance/AttendanceSection";
 import ReportingEvaluationSection from "@/src/modules/reports/infrastructure/ui/components/evaluation/ReportingEvaluationSection";
+import { useSearchParams } from "next/navigation";
 import { useContext, useEffect, useState } from "react";
 
 export default function ReportsHome() {
   const { selectedSchool } = useContext(AuthContext);
   const [date, setDate] = useState<Date>(new Date());
   const [loading, setLoading] = useState<boolean>(false);
-
-  const [tab, setTab] = useState<number>(1);
+  const searchParams = useSearchParams();
+  const tab = searchParams.get("tab") || "attendance";
 
   const links = [
     {
       value: 1,
       name: "Attendance",
+      urlString: "attendance",
     },
     {
       value: 2,
       name: "Evaluations",
+      urlString: "evaluations",
     },
     {
       value: 3,
       name: "Daily Reports",
+      urlString: "daily reports",
     },
   ];
 
@@ -91,9 +95,9 @@ export default function ReportsHome() {
 
         <section className="grid gap-4 rounded border bg-gray-100 p-2 shadow-inner sm:grid-cols-8 sm:gap-2">
           <div className="bg-white sm:col-span-8">
-            <PageTabNavigation links={links} tab={tab} setTab={setTab} />
+            <ParamsPageTabNav links={links} tab={tab} />
           </div>
-          {tab !== 3 && (
+          {tab !== "daily reports" && (
             <div className="col-span-8 grid grid-cols-8 gap-2">
               <article className="relative col-span-4 flex flex-col gap-4">
                 <div className="sticky top-4 rounded-lg border bg-white p-4 shadow">
@@ -113,7 +117,7 @@ export default function ReportsHome() {
                     <>
                       <p className="mb-2">
                         Click a class to see the student{" "}
-                        {tab === 1 ? "Attendance" : "Evaluations"} on the right.
+                        {tab === "attendance" ? "Attendance" : "Evaluations"} on the right.
                       </p>
                       <ClassList
                         todayClasses={todayClasses}
@@ -124,7 +128,7 @@ export default function ReportsHome() {
                   )}
                 </div>
               </article>
-              {tab === 1 && selectedClass && (
+              {tab === "attendance" && selectedClass && (
                 <article className="col-span-4 rounded-lg border bg-white p-4">
                   <AttendanceSection
                     date={date}
@@ -132,7 +136,7 @@ export default function ReportsHome() {
                   />
                 </article>
               )}
-              {tab === 2 && selectedClass && (
+              {tab === "evaluations" && selectedClass && (
                 <article className="col-span-4">
                   <ReportingEvaluationSection
                     date={date}
@@ -143,9 +147,8 @@ export default function ReportsHome() {
             </div>
           )}
         </section>
-        {tab === 3 && (
+        {tab === "daily reports" && (
           <section className="rounded border p-2 shadow">
-            <p>daily reports</p>
             <DailyReportOverview date={date} />
           </section>
         )}
