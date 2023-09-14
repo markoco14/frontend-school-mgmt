b1866d38fe6e36abdbe13ef8e7b31713ed392c64
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-14 09:24:17 +0800

    refactor: rearrange class_id page into tabs and remove Assessments

--- src/pages/classes/[class_id]/index.tsx
+++ src/pages/classes/[class_id]/index.tsx
@@ -1,10 +1,6 @@
 import AuthContext from "@/src/AuthContext";
-import ClassListSkeletonProps from "@/src/components/ui/skeleton/ClassListSkeletonProps";
-import { Skeleton } from "@/src/components/ui/skeleton/Skeleton";
-import { ClassAssessment } from "@/src/modules/classes/domain/entities/ClassAssessment";
 import { ClassEntity } from "@/src/modules/classes/domain/entities/ClassEntity";
 import { classAdapter } from "@/src/modules/classes/infrastructure/adapters/classAdapter";
-import { classAssessmentAdapter } from "@/src/modules/classes/infrastructure/adapters/classAssessmentAdapter";
 import ManageClassTeacher from "@/src/modules/classes/infrastructure/ui/components/ManageClassTeacher";
 import ManageClassStudents from "@/src/modules/classes/infrastructure/ui/components/class-students/ManageClassStudents";
 import Layout from "@/src/modules/core/infrastructure/ui/components/Layout";
@@ -12,7 +8,7 @@ import PermissionDenied from "@/src/modules/core/infrastructure/ui/components/Pe
 import { GetServerSideProps } from "next";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 
 export const getServerSideProps: GetServerSideProps<{
   classEntity: ClassEntity;
@@ -29,38 +25,25 @@ export default function ManageClassDetails({
   classEntity: ClassEntity;
 }) {
   const [selectedClass, setSelectedClass] = useState<ClassEntity>(classEntity);
-  const [assessments, setAssessments] = useState<ClassAssessment[]>([]);
   const { user } = useContext(AuthContext);
-  const [loading, setLoading] = useState<boolean>(false);
+  const [tab, setTab] = useState<number>(1);
 
-  const router = useRouter();
+  const links = [
+    {
+      value: 1,
+      name: "Class Info",
+    },
+    {
+      value: 2,
+      name: "Students",
+    },
+    {
+      value: 3,
+      name: "Teachers",
+    },
+  ];
 
-  useEffect(() => {
-    async function getAssessments() {
-      setLoading(true);
-      await classAssessmentAdapter
-        .list({ class_id: classEntity.id, details: true })
-        .then((res) => {
-          setAssessments(res);
-          setLoading(false);
-        });
-    }
-
-    getAssessments();
-    // async function getClassData() {
-    //   await classAdapter
-    //     .getClassById({ class_id: Number(router.query.class_id) })
-    //     .then((res) => {
-    //       setSelectedClass(res);
-    //     });
-    // }
-
-    // if (router.query.class_id) {
-    //   getClassData();
-    // }
-  }, [classEntity]);
-
-  if (user?.role !== "OWNER") {
+  if (!user?.permissions.includes(1)) {
     return (
       <Layout>
         <PermissionDenied />
@@ -70,61 +53,48 @@ export default function ManageClassDetails({
 
   return (
     <Layout>
-      <>
-        {selectedClass && (
-          <>
-            <section className="mb-4">
-              <div className="mb-2 flex items-baseline justify-between">
-                <h2 className="text-3xl">{selectedClass?.name}</h2>
-                <Link href="/classes">Back</Link>
-              </div>
-              {selectedClass.day && (
-                <p className="text-xl">
-                  {selectedClass.day[0] === 1 ? "Monday" : "Wednesday"} &{" "}
-                  {selectedClass.day[1] === 4 ? "Thursday" : "Friday"}
-                </p>
-              )}
-            </section>
-            <section className="grid gap-4 md:grid-cols-2 md:grid-rows-2">
-              <div className="col-span-1 md:row-span-2">
-                <ManageClassStudents selectedClass={selectedClass} />
-              </div>
-              <article className="col-span-1 row-span-1 rounded border-2 p-4">
-                <h2 className="text-xl">Class Assessments</h2>
-                {loading ? (
-                  <Skeleton>
-                    <ClassListSkeletonProps />
-                  </Skeleton>
-                ) : (
-                  <ul>
-                    {assessments?.map((assessment, index) => (
-                      <li key={`assessment-${assessment.id}`}>
-                        {assessment.assessment.name}
-                      </li>
-                    ))}
-                  </ul>
-                )}
-              </article>
-              <article className="col-span-1 row-span-1">
-                <ManageClassTeacher
-                  selectedClass={selectedClass}
-                  setSelectedClass={setSelectedClass}
-                />
-              </article>
-            </section>
-            {/* <DeleteClass
-              selectedClass={selectedClass}
-              setSelectedClass={setSelectedClass}
-            /> */}
-          </>
+      <section className="grid gap-4">
+        <div className="mb-2 flex items-baseline justify-between">
+          <h2 className="text-3xl">{selectedClass?.name}</h2>
+          <Link href="/classes">Back</Link>
+        </div>
+        <nav className="flex gap-4 overflow-x-auto rounded border p-2 shadow">
+          {links.map((button) => (
+            <button
+              className={`${
+                button.value === tab &&
+                "duration underline decoration-blue-500 decoration-2 underline-offset-2 ease-in-out"
+              }`}
+              key={button.value}
+              onClick={() => {
+                setTab(button.value);
+                // router.push(`/?tab=${button.value}`)
+              }}
+            >
+              {button.name}
+            </button>
+          ))}
+        </nav>
+        {/* <PageTabNavigation links={links} tab={tab} setTab={setTab} /> */}
+        {tab === 1 ? (
+          <article className="col-span-1 rounded border p-2 shadow md:row-span-2">
+            <p>Class info goes here.</p>
+          </article>
+        ) : tab === 2 ? (
+          <article className="col-span-1 md:row-span-2">
+            <ManageClassStudents selectedClass={selectedClass} />
+          </article>
+        ) : (
+          tab === 3 && (
+            <article className="col-span-1 row-span-1">
+              <ManageClassTeacher
+                selectedClass={selectedClass}
+                setSelectedClass={setSelectedClass}
+              />
+            </article>
+          )
         )}
-        {/* {!selectedClass && (
-          <div className="flex flex-col xs:flex-row xs:justify-between xs:gap-2 items-baseline mb-4 bg-gray-100 shadow-inner p-2 rounded">
-            <h2 className="text-3xl">This class was deleted.</h2>
-            <Link href="/classes">Back</Link>
-          </div>
-        )} */}
-      </>
+      </section>
     </Layout>
   );
 }
