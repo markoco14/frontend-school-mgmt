625c2ea868ad1cb5ec894a7a058cb64942f1a366
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-15 16:19:55 +0800

    refactor: change AddSubject buttom to AddSubjectForm, move to main component in Modal

--- src/modules/curriculum/infrastructure/ui/components/SubjectSection.tsx
+++ src/modules/curriculum/infrastructure/ui/components/SubjectSection.tsx
@@ -1,25 +1,26 @@
 import AuthContext from "@/src/AuthContext";
+import Modal from "@/src/modules/core/infrastructure/ui/components/Modal";
+import PaginationButtons from "@/src/modules/core/infrastructure/ui/components/PaginationButtons";
+import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, useContext, useEffect, useState } from "react";
 import toast from "react-hot-toast";
-import { subjectAdapter } from "../../adapters/subjectAdapter";
-import { Subject } from "../../../domain/entities/Subject";
-import AddSubject from "./AddSubject";
-import { levelAdapter } from "../../adapters/levelAdapter";
 import { Level } from "../../../domain/entities/Level";
-import PaginationButtons from "@/src/modules/core/infrastructure/ui/components/PaginationButtons";
-import { subjectLevelAdapter } from "../../adapters/subjectLevelAdapter";
+import { Subject } from "../../../domain/entities/Subject";
 import { SubjectLevel } from "../../../domain/entities/SubjectLevel";
-import { Dialog, Transition } from "@headlessui/react";
+import { levelAdapter } from "../../adapters/levelAdapter";
+import { subjectAdapter } from "../../adapters/subjectAdapter";
+import { subjectLevelAdapter } from "../../adapters/subjectLevelAdapter";
+import AddSubjectForm from "./AddSubjectForm";
 
 const SubjectLevelModal = ({
   subject,
   setCurrentSubject,
-	subjectLevels,
+  subjectLevels,
   setSubjectLevels,
 }: {
   subject: Subject;
   setCurrentSubject: Function;
-	subjectLevels: SubjectLevel[];
+  subjectLevels: SubjectLevel[];
   setSubjectLevels: Function;
 }) => {
   const { selectedSchool } = useContext(AuthContext);
@@ -29,7 +30,9 @@ const SubjectLevelModal = ({
   const [page, setPage] = useState<number>(1);
   const [next, setNext] = useState<boolean>(false);
   const [count, setCount] = useState<number>(0);
-	const [show, setShow] = useState<boolean>(() => subject.name === "" ? false : true);
+  const [show, setShow] = useState<boolean>(() =>
+    subject.name === "" ? false : true,
+  );
 
   useEffect(() => {
     async function listSchoolLevels() {
@@ -75,93 +78,115 @@ const SubjectLevelModal = ({
     }
   }
 
-	function checkLevelAssigned({level, subject, subjectLevels}: {level: Level, subject: Subject; subjectLevels: SubjectLevel[]}) {
-		return subjectLevels?.some((subjectLevel: SubjectLevel) => {
-			return subjectLevel.level.id === level.id && subjectLevel.subject.id === subject.id
-		});
-	}
+  function checkLevelAssigned({
+    level,
+    subject,
+    subjectLevels,
+  }: {
+    level: Level;
+    subject: Subject;
+    subjectLevels: SubjectLevel[];
+  }) {
+    return subjectLevels?.some((subjectLevel: SubjectLevel) => {
+      return (
+        subjectLevel.level.id === level.id &&
+        subjectLevel.subject.id === subject.id
+      );
+    });
+  }
 
   return (
     <>
-		<Transition
-				appear={true}
-				show={show}
-			>
-				<Dialog
-					onClose={() => setCurrentSubject(false)}
-					className="fixed inset-0 grid place-items-center "
-				>	
-					<Transition.Child
-						as={Fragment}
-						enter="ease-out duration-300"
-						enterFrom="opacity-0"
-						enterTo="opacity-100"
-						leave="ease-in duration-200"
-						leaveFrom="opacity-100"
-						leaveTo="opacity-0"
-					>
-						<div className="fixed inset-0 bg-blue-900 bg-opacity-50" />
-					</Transition.Child>
-					<Transition.Child
-						as={Fragment}
-						enter="transition-opacity transition-scale ease-in duration-500"
-						enterFrom="opacity-0 scale-90"
-						enterTo="opacity-100 scale-100"
-						leave="transition-opacity transition-scale ease-out duration-150"
-						leaveFrom="opacity-100 scale-100"
-						leaveTo="opacity-0"
-					>
-						<Dialog.Panel className="bg-white rounded-2xl shadow-xl p-8 z-10">
-							<div className="flex justify-between">
-								<h1 className="text-lg text-gray-500">Manage Subject Levels</h1>
-								<button onClick={() => setCurrentSubject("")}>
-									<i className="fa-solid fa-xmark" />
-								</button>
-							</div>
-							<p className="text-2xl">Add levels to {subject.name}</p>
-							<ul className="bg-gray-100 rounded shadow-inner mb-4">
-								{loading ? (
-									<p>loading...</p>
-								) : levels?.length >= 1 ? (
-									levels?.map((level, index) => (
-										<li
-											key={index}
-										>
-											<button
-												disabled={checkLevelAssigned({level: level, subject: subject, subjectLevels: subjectLevels})}
-												onClick={() => {
-													handleAddSubjectLevel({ subject: subject, level: level });
-												}}
-												className={`${checkLevelAssigned({level: level, subject: subject, subjectLevels: subjectLevels}) ? 'bg-blue-300 hover:bg-blue-500' : 'hover:bg-gray-300 '} w-full p-2 flex justify-between disabled:cursor-not-allowed`}
-											>{level.name}</button>
-										</li>
-									))
-								) : (
-									<article className="bg-gray-100 rounded shadow-inner mb-4">
-										<p>This page is empty.</p>
-									</article>
-								)}
-							</ul>
-							<PaginationButtons
-								count={count}
-								page={page}
-								setPage={setPage}
-								next={next}
-							/>
-							<div className="flex justify-end">
-								<button
-									type="button"
-									onClick={() => setCurrentSubject(false)}
-									className="bg-gray-300 text-gray-900 hover:bg-gray-500 hover:text-white px-4 py-1 rounded"
-								>
-									Cancel
-								</button>
-							</div>
-						</Dialog.Panel>
-					</Transition.Child>
-				</Dialog>
-			</Transition>
-      
+      <Transition appear={true} show={show}>
+        <Dialog
+          onClose={() => setCurrentSubject(false)}
+          className="fixed inset-0 grid place-items-center "
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-blue-900 bg-opacity-50" />
+          </Transition.Child>
+          <Transition.Child
+            as={Fragment}
+            enter="transition-opacity transition-scale ease-in duration-500"
+            enterFrom="opacity-0 scale-90"
+            enterTo="opacity-100 scale-100"
+            leave="transition-opacity transition-scale ease-out duration-150"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Panel className="z-10 rounded-2xl bg-white p-8 shadow-xl">
+              <div className="flex justify-between">
+                <h1 className="text-lg text-gray-500">Manage Subject Levels</h1>
+                <button onClick={() => setCurrentSubject("")}>
+                  <i className="fa-solid fa-xmark" />
+                </button>
+              </div>
+              <p className="text-2xl">Add levels to {subject.name}</p>
+              <ul className="mb-4 rounded bg-gray-100 shadow-inner">
+                {loading ? (
+                  <p>loading...</p>
+                ) : levels?.length >= 1 ? (
+                  levels?.map((level, index) => (
+                    <li key={index}>
+                      <button
+                        disabled={checkLevelAssigned({
+                          level: level,
+                          subject: subject,
+                          subjectLevels: subjectLevels,
+                        })}
+                        onClick={() => {
+                          handleAddSubjectLevel({
+                            subject: subject,
+                            level: level,
+                          });
+                        }}
+                        className={`${
+                          checkLevelAssigned({
+                            level: level,
+                            subject: subject,
+                            subjectLevels: subjectLevels,
+                          })
+                            ? "bg-blue-300 hover:bg-blue-500"
+                            : "hover:bg-gray-300 "
+                        } flex w-full justify-between p-2 disabled:cursor-not-allowed`}
+                      >
+                        {level.name}
+                      </button>
+                    </li>
+                  ))
+                ) : (
+                  <article className="mb-4 rounded bg-gray-100 shadow-inner">
+                    <p>This page is empty.</p>
+                  </article>
+                )}
+              </ul>
+              <PaginationButtons
+                count={count}
+                page={page}
+                setPage={setPage}
+                next={next}
+              />
+              <div className="flex justify-end">
+                <button
+                  type="button"
+                  onClick={() => setCurrentSubject(false)}
+                  className="rounded bg-gray-300 px-4 py-1 text-gray-900 hover:bg-gray-500 hover:text-white"
+                >
+                  Cancel
+                </button>
+              </div>
+            </Dialog.Panel>
+          </Transition.Child>
+        </Dialog>
+      </Transition>
     </>
   );
 };
@@ -169,26 +194,27 @@ const SubjectLevelModal = ({
 const SubjectList = ({
   subjects,
   setSubjects,
-	subjectLevels,
+  subjectLevels,
   setSubjectLevels,
 }: {
   subjects: Subject[];
   setSubjects: Function;
-	subjectLevels: SubjectLevel[]
+  subjectLevels: SubjectLevel[];
   setSubjectLevels: Function;
 }) => {
   const [currentSubject, setCurrentSubject] = useState<Subject>();
 
   async function handleDeleteSubject({ subjectId }: { subjectId: number }) {
     await subjectAdapter.deleteSubject({ id: subjectId }).then((res) => {
-      setSubjects((prevSubjects: Subject[]) =>
-        prevSubjects?.filter((subject) => subject.id !== subjectId)
+      setSubjects(
+        (prevSubjects: Subject[]) =>
+          prevSubjects?.filter((subject) => subject.id !== subjectId),
       );
-      setSubjectLevels((prevSubjectLevels: SubjectLevel[]) =>{ 
+      setSubjectLevels((prevSubjectLevels: SubjectLevel[]) => {
         prevSubjectLevels.filter(
-          (subjectLevel) => subjectLevel.subject.id !== subjectId
-        )}
-      );
+          (subjectLevel) => subjectLevel.subject.id !== subjectId,
+        );
+      });
       toast.success("Subject deleted.");
     });
   }
@@ -227,14 +253,15 @@ const SubjectList = ({
 };
 
 export default function SubjectSection({
-	subjectLevels,
+  subjectLevels,
   setSubjectLevels,
 }: {
-	subjectLevels: SubjectLevel[]
+  subjectLevels: SubjectLevel[];
   setSubjectLevels: Function;
 }) {
   const { selectedSchool } = useContext(AuthContext);
   const [subjects, setSubjects] = useState<Subject[]>([]);
+  const [isAddSubject, setIsAddSubject] = useState<boolean>(false);
 
   useEffect(() => {
     async function getSubjects() {
@@ -250,12 +277,22 @@ export default function SubjectSection({
     getSubjects();
   }, [selectedSchool]);
 
+  function handleClose() {
+    setIsAddSubject(false);
+  }
+
   return (
     <section className="col-span-2 rounded border p-4 shadow xs:col-span-1">
-      <article className="w-1/2 grid gap-4">
+      <article className="grid w-1/2 gap-4">
         <div className="flex justify-between">
           <h2 className="mb-2 text-3xl">Subjects</h2>
-          <AddSubject setSubjects={setSubjects} />
+          <button
+            className="rounded  underline underline-offset-2 hover:text-blue-700"
+            onClick={() => setIsAddSubject(true)}
+          >
+            Add subject
+          </button>
+          {/* <AddSubject setSubjects={setSubjects} /> */}
         </div>
         <p>Click a subject to assign Levels.</p>
         <SubjectList
@@ -265,6 +302,9 @@ export default function SubjectSection({
           setSubjectLevels={setSubjectLevels}
         />
       </article>
+      <Modal show={isAddSubject} close={handleClose} title="Add New Subject">
+        <AddSubjectForm setSubjects={setSubjects} />
+      </Modal>
     </section>
   );
 }
