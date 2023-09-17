cbbfb65a51b055cb15e7a079b5c09d725b99e736
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-17 13:39:14 +0800

    feat: implement textAttributeAdapter.add method with success/error messages

--- src/modules/evaluation/infrastructure/ui/components/eval-editor/newTextMetricForm.tsx
+++ src/modules/evaluation/infrastructure/ui/components/eval-editor/newTextMetricForm.tsx
@@ -1,6 +1,9 @@
 import AuthContext from "@/src/AuthContext";
-import { useContext, useState } from "react";
+import { TextAttributePayload } from "@/src/modules/evaluation/domain/entities/payloads/TextAttributePayload";
+import { useContext } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { textAttributeAdapter } from "../../../adapters/textAttributeAdapter";
 
 type Inputs = {
   name: string;
@@ -10,33 +13,27 @@ const NewTextMetricForm = () => {
   const { selectedSchool } = useContext(AuthContext);
   const {
     register,
-    setValue,
-    watch,
     reset,
     handleSubmit,
     formState: { errors },
   } = useForm<Inputs>();
-  const name = watch("name");
-  const [descriptions, setDescriptions] = useState<{ [key: string]: string }[]>(
-    [],
-  );
-  const [isWriteDescriptions, setIsWriteDescriptions] =
-    useState<boolean>(false);
 
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
-    console.log(data);
-    const preparedData = {
+    const payload: TextAttributePayload = {
       name: data.name,
       school_id: selectedSchool?.id,
       data_type_id: 8,
     };
-    console.log("prepared data", preparedData);
-    return;
+    try {
+      await textAttributeAdapter.add({ payload: payload }).then((res) => {
+        toast.success("Saved!");
+        reset();
+      });
+    } catch (error) {
+      // @ts-ignore
+      toast.error(error?.message);
+    }
   };
-
-  function handleClose() {
-    setIsWriteDescriptions(false);
-  }
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="grid gap-4 p-8">
@@ -57,8 +54,6 @@ const NewTextMetricForm = () => {
         )}
       </div>
       <hr></hr>
-      {/* <p>You need to choose descriptions for {name} before you can save.</p> */}
-
       <div className="grid grid-cols-2 gap-4">
         <button className="col-start-2 rounded bg-blue-700 px-2 py-1 text-white disabled:bg-gray-700 ">
           Save
