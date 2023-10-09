45de0d1e070024a9e20c866d59b36db46eccc3a2
Mark O'Connor <mark.oconnor14@gmail.com>
2023-10-09 17:35:32 +0800

    fix: build errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,6 +25,7 @@ export default function Home() {
             setSchools(res);
           });
         } catch (error) {
+          // @ts-ignore
           toast.error(error.message);
         }
       }
