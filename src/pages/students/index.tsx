bc5b10057df9d4fb7dda4dc8c111aefdc439f7ac
Mark O'Connor <mark.oconnor14@gmail.com>
2023-11-19 22:54:16 +0800

    feat: wrap student list page in AdminLayout

--- src/pages/students/index.tsx
+++ src/pages/students/index.tsx
@@ -1,4 +1,5 @@
 import { useUserContext } from "@/src/UserContext";
+import AdminLayout from "@/src/modules/core/infrastructure/ui/components/AdminLayout";
 import Layout from "@/src/modules/core/infrastructure/ui/components/Layout";
 import Modal from "@/src/modules/core/infrastructure/ui/components/Modal";
 import PermissionDenied from "@/src/modules/core/infrastructure/ui/components/PermissionDenied";
@@ -59,7 +60,9 @@ export default function StudentsHome() {
   if (user?.role !== "OWNER") {
     return (
       <Layout>
-        <PermissionDenied />
+        <AdminLayout>
+          <PermissionDenied />
+        </AdminLayout>
       </Layout>
     );
   }
@@ -69,71 +72,77 @@ export default function StudentsHome() {
   }
   return (
     <Layout>
-      <div>
-        {/* <SchoolHeader /> */}
-        <section>
-          <div className="mb-4 flex items-baseline justify-between">
-            <h2 className="text-3xl">Students</h2>
-            <button
-              onClick={() => {
-                setIsAddStudent(true);
-              }}
-              className="rounded bg-blue-300 p-2 hover:bg-blue-500"
-            >
-              <i className="fa-solid fa-plus"></i>{" "}
-              <i className="fa-solid fa-user"></i>
-            </button>
-          </div>
-          {loading ? (
-            <article className="min-h-[200px] rounded bg-gray-100 p-2 shadow-inner">
-              <p>loading...</p>
-            </article>
-          ) : (
-            <article className="mb-4 grid gap-2 rounded border p-4 shadow">
-              {!students && <NoData text={"No students found"} />}
-              {students && (
-                <>
-                  <ul className="min-h-[420px] divide-y divide-gray-300 sm:h-full">
-                    {students?.map((student: Student, index) => (
-                      <li key={index} className="flex justify-between gap-4">
-                        <Link
-                          href={`/students/${student.id}`}
-                          className="w-full rounded p-2 hover:bg-blue-300"
-                        >
-                          {student.first_name} {student.last_name}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                  <div className="flex justify-evenly gap-2">
-                    <button
-                      className="w-full rounded bg-blue-300 px-2 py-1 disabled:cursor-not-allowed disabled:bg-gray-300"
-                      disabled={page === 1}
-                      onClick={() => {
-                        setPage((prevPage) => prevPage - 1);
-                      }}
-                    >
-                      <i className="fa-solid fa-arrow-left"></i>
-                    </button>
-                    <button
-                      className="w-full rounded bg-blue-300 px-2 py-1 disabled:cursor-not-allowed disabled:bg-gray-300"
-                      disabled={!isNext}
-                      onClick={() => {
-                        setPage((prevPage) => prevPage + 1);
-                      }}
-                    >
-                      <i className="fa-solid fa-arrow-right"></i>
-                    </button>
-                  </div>
-                </>
-              )}
-            </article>
-          )}
-        </section>
-        <Modal show={isAddStudent} close={handleClose} title="Add New Student">
-          <RegisterNewStudentModal setStudents={setStudents} />
-        </Modal>
-      </div>
+      <AdminLayout>
+        <div className="bg-white w-full h-full">
+          {/* <SchoolHeader /> */}
+          <section className="max-w-[1000px]">
+            <div className="mb-4 flex items-baseline justify-between">
+              <h2 className="text-3xl">Students</h2>
+              <button
+                onClick={() => {
+                  setIsAddStudent(true);
+                }}
+                className="rounded bg-blue-300 p-2 hover:bg-blue-500"
+              >
+                <i className="fa-solid fa-plus"></i>{" "}
+                <i className="fa-solid fa-user"></i>
+              </button>
+            </div>
+            {loading ? (
+              <article className="min-h-[200px] rounded bg-gray-100 p-2 shadow-inner">
+                <p>loading...</p>
+              </article>
+            ) : (
+              <article className="mb-4 grid gap-2 rounded border p-4 shadow">
+                {!students && <NoData text={"No students found"} />}
+                {students && (
+                  <>
+                    <ul className="min-h-[420px] divide-y divide-gray-300 sm:h-full">
+                      {students?.map((student: Student, index) => (
+                        <li key={index} className="flex justify-between gap-4">
+                          <Link
+                            href={`/students/${student.id}`}
+                            className="w-full rounded p-2 hover:bg-blue-300"
+                          >
+                            {student.first_name} {student.last_name}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                    <div className="flex justify-evenly gap-2">
+                      <button
+                        className="w-full rounded bg-blue-300 px-2 py-1 disabled:cursor-not-allowed disabled:bg-gray-300"
+                        disabled={page === 1}
+                        onClick={() => {
+                          setPage((prevPage) => prevPage - 1);
+                        }}
+                      >
+                        <i className="fa-solid fa-arrow-left"></i>
+                      </button>
+                      <button
+                        className="w-full rounded bg-blue-300 px-2 py-1 disabled:cursor-not-allowed disabled:bg-gray-300"
+                        disabled={!isNext}
+                        onClick={() => {
+                          setPage((prevPage) => prevPage + 1);
+                        }}
+                      >
+                        <i className="fa-solid fa-arrow-right"></i>
+                      </button>
+                    </div>
+                  </>
+                )}
+              </article>
+            )}
+          </section>
+          <Modal
+            show={isAddStudent}
+            close={handleClose}
+            title="Add New Student"
+          >
+            <RegisterNewStudentModal setStudents={setStudents} />
+          </Modal>
+        </div>
+      </AdminLayout>
     </Layout>
   );
 }
