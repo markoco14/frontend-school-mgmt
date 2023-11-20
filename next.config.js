a34625f0bebb010ad7eb270ce6673d91fcdb589d
Mark O'Connor <mark.oconnor14@gmail.com>
2023-11-20 23:01:00 +0800

    feat: set up next.config and _document for PWA

--- next.config.js
+++ next.config.js
@@ -1,14 +1,31 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'storage.googleapis.com',        
+//       }
+//     ]
+//   },
+// }
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
+
+module.exports = withPWA({
   reactStrictMode: true,
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',        
-      }
-    ]
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+      },
+    ],
   },
-}
-
-module.exports = nextConfig
+});
