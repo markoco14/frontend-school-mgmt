3ce47db7e6424ac964a871f6e6760b2856e5d46c
Mark O'Connor <mark.oconnor14@gmail.com>
2023-11-22 12:25:09 +0800

    fix: remove Assessment enitity imports

--- src/modules/students/entities/StudentAssessment.ts
+++ src/modules/students/entities/StudentAssessment.ts
@@ -8,6 +8,5 @@ export class StudentAssessment {
     public score: number,
     public date_completed: string,
     public student?: Student,
-    public assessment?: any, // TODO: change to Assessment when model created
   ) {}
 }
