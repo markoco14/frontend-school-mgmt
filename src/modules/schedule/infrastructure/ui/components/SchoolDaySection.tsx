030c151e55275749c7d4e80f5971d6439f695551
Mark O'Connor <mark.oconnor14@gmail.com>
2023-11-22 21:06:13 +0800

    refactor: move SchoolDayList logic from SchoolDaySection to component file

--- src/modules/schedule/infrastructure/ui/components/SchoolDaySection.tsx
+++ src/modules/schedule/infrastructure/ui/components/SchoolDaySection.tsx
@@ -1,72 +1,11 @@
 import { useUserContext } from "@/src/UserContext";
 import { SchoolDay } from "@/src/modules/school-mgmt/domain/entities/SchoolDay";
 import AddSchoolDay from "@/src/modules/school-mgmt/infrastructure/ui/components/AddSchoolDay";
-import { removeListItem } from "@/src/utils/removeListItem";
 import { useEffect, useState } from "react";
-import toast from "react-hot-toast";
 import { schoolDayAdapter } from "../adapters/schoolDayAdapter";
+import SchoolDayList from "@/src/components/schedule/SchoolDayList";
 
-const SchoolDayList = ({
-  schoolDays,
-  setSchoolDays,
-}: {
-  schoolDays: SchoolDay[];
-  setSchoolDays: Function;
-}) => {
-  const { selectedSchool } = useUserContext();
 
-  useEffect(() => {
-    async function getSubjects() {
-      if (selectedSchool) {
-        await schoolDayAdapter
-          .listSchoolSchoolDays({ schoolId: selectedSchool.id })
-          .then((res) => {
-            setSchoolDays(res);
-          });
-      }
-    }
-
-    getSubjects();
-  }, [selectedSchool, setSchoolDays]);
-
-  async function handleDeleteSchoolDay({
-    schoolDayId,
-  }: {
-    schoolDayId: number;
-  }) {
-    await schoolDayAdapter
-      .deleteSchoolDay({ schoolDayId: schoolDayId })
-      .then(() => {
-        removeListItem(setSchoolDays, schoolDayId);
-        toast.success("School Day deleted.");
-      });
-  }
-
-  return (
-    <ul className="mb-4 rounded bg-gray-100 shadow-inner">
-      {schoolDays.length > 0 ? (
-        schoolDays.map((schoolDay, index) => (
-          <li
-            key={index}
-            className="flex justify-between p-2 hover:bg-gray-300"
-          >
-            <span>{schoolDay?.day}</span>
-            <button
-              onClick={() =>
-                handleDeleteSchoolDay({ schoolDayId: schoolDay.id })
-              }
-              className="text-red-500 hover:text-red-600"
-            >
-              delete
-            </button>
-          </li>
-        ))
-      ) : (
-        <p className="p-2">No school days chosen.</p>
-      )}
-    </ul>
-  );
-};
 
 export default function SchoolDaySection() {
   const { selectedSchool } = useUserContext();
