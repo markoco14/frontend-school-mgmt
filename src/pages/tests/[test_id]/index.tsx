a4e20df1c2289f47754b026649b7f2718f892ce8
Mark O'Connor <mark.oconnor14@gmail.com>
2023-11-23 17:15:00 +0800

    feat: set up do test page layout and set back button, header, question, and font sizes

--- src/pages/tests/[test_id]/index.tsx
+++ src/pages/tests/[test_id]/index.tsx
@@ -17,9 +17,18 @@ const DoTestPage: NextPageWithLayout = () => {
 
   return (
     <main>
-      <button onClick={() => router.push('/tests')}className="absolute left-0 top-0">Back</button>
-      <div>
-        <h1>{selectedTest?.name}</h1>
+      <div className="bg-gray-200 absolute left-0 top-0 grid py-2 w-full grid-cols-10 opacity-0 duration-200 ease-in-out hover:opacity-100">
+        <button onClick={() => router.push("/tests")} className="col-span-1">
+          Back
+        </button>
+        <h1 className="col-span-8 text-center text-5xl">
+          {selectedTest?.name}
+        </h1>
+      </div>
+      <div className="h-screen">
+        <div className="grid h-full place-items-center">
+          <p className="text-center text-[180px]">Did her give the it to he?</p>
+        </div>
       </div>
     </main>
   );
