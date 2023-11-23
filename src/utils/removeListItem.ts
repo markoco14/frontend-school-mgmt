653929bea1ab041cf4df49166413a959863c39e3
Mark O'Connor <mark.oconnor14@gmail.com>
2023-11-23 11:34:36 +0800

    refactor: change removeListItem to removeListItemById and update logic

--- src/utils/removeListItem.ts
+++ src/utils/removeListItem.ts
@@ -1,3 +1,9 @@
+export function removeListItemById(list: any[], id: number) {
+  const filteredList = list.filter((item: any) => item.id !== id);
+  return filteredList
+}
+
+// removed because hard to test with set state function
 export function removeListItem(setState: Function, id: number) {
   setState((prevList: any[]) => prevList.filter((item) => item.id !== id));
 }
