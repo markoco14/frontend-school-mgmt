93b32a8103975f1c86031e374bbaca4bb4d79ba3
Mark O'Connor <mark.oconnor14@gmail.com>
2023-11-23 14:34:55 +0800

    feat: install storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,30 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+
+const path = require("path");
+
+const config: StorybookConfig = {
+  stories: [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions"
+  ],
+  framework: {
+    "name": "@storybook/nextjs",
+    "options": {}
+  },
+  docs: {
+    "autodocs": "tag"
+  },
+  webpackFinal: async (baseConfig, options) => {
+    if (baseConfig) {
+      baseConfig.resolve.alias['@/src'] = path.resolve(__dirname, '../src')
+      return baseConfig
+    }
+  }
+};
+export default config;
