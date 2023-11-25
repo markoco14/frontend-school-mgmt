96b631d989f2dbb1a3e2e6f1c463d6068a06eeec
Mark O'Connor <mark.oconnor14@gmail.com>
2023-11-25 16:35:29 +0800

    feat: remove mock UserContext from SchoolDayList tests

--- __tests__/components/schedule/SchoolDayList.test.tsx
+++ __tests__/components/schedule/SchoolDayList.test.tsx
@@ -1,52 +1,39 @@
 /**
  * @jest-environment jsdom
-*/
-import { render, screen } from '@testing-library/react';
-import "@testing-library/jest-dom";
+ */
 import SchoolDayList from "@/src/modules/schedule/components/SchoolDayList";
-import UserContextProvider from '@/src/contexts/UserContext';
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
 
-// Mock the useRouter hook
-jest.mock('next/router', () => ({
-  useRouter() {
-    return {
-      push: jest.fn(), // Mock any function you use from useRouter
-      // ... other properties and functions as needed
-    };
-  },
-}));
 
-describe('School Day List', () => {
-	it('renders no school days chosen', () => {
-		const mockHandleDeleteSchoolDay = jest.fn(); // Mock function for setSchoolDays
+describe("School Day List", () => {
+  it("renders no school days chosen", () => {
+    const mockHandleDeleteSchoolDay = jest.fn(); // Mock function for setSchoolDays
     render(
-			<UserContextProvider>
-				<SchoolDayList schoolDays={[]} handleDeleteSchoolDay={mockHandleDeleteSchoolDay} />,
-			</UserContextProvider>
+      <SchoolDayList
+        schoolDays={[]}
+        handleDeleteSchoolDay={mockHandleDeleteSchoolDay}
+      />,
     );
     const paragraph = screen.getByText("No school days chosen.");
-		
+
     expect(paragraph).toBeInTheDocument();
   });
-	
-	it('renders a list of days the school is open', async () => {
-		const mockHandleDeleteSchoolDay = jest.fn(); // Mock function for setSchoolDays
-		const schoolDays = [
+
+  it("renders a list of days the school is open", async () => {
+    const mockHandleDeleteSchoolDay = jest.fn(); // Mock function for setSchoolDays
+    const schoolDays = [
       { day: "Monday", school: 1, id: 1 },
       { day: "Thursday", school: 1, id: 4 },
     ];
-		render(
-      <UserContextProvider>
-        <SchoolDayList
-          schoolDays={schoolDays}
-          handleDeleteSchoolDay={mockHandleDeleteSchoolDay}
-        />
-        ,
-      </UserContextProvider>,
+    render(
+      <SchoolDayList
+        schoolDays={schoolDays}
+        handleDeleteSchoolDay={mockHandleDeleteSchoolDay}
+      />,
     );
 
-		const days = await screen.findAllByRole('listitem');
-		expect(days).toHaveLength(2)
-	});
+    const days = await screen.findAllByRole("listitem");
+    expect(days).toHaveLength(2);
+  });
 });
-
