05823cd580671e710db6382d77f1c7809e4de9f4
Mark O'Connor <mark.oconnor14@gmail.com>
2023-11-25 16:04:49 +0800

    feat: add access token type notes

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -27,6 +27,21 @@ type UserContextEntity = {
   handleDeselectSchool: Function;
 };
 
+// {
+  // "token_type": "access", // access | refresh
+//   "exp": 1701158298,
+//   "iat": 1700899098,
+//   "jti": "9a0d2ef3ce704edf8261d2850716eef3",
+//   "user_id": 1,
+//   "name": "First name",
+//   "role": "A ROLE",
+//   "permissions": [
+//     1,
+//     3
+//   ],
+//   "email": "some-email@gmail.com"
+// }
+
 type Token = {
   id: number;
   exp: number;
