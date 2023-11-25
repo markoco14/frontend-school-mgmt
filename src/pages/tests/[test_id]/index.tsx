65ff6dc57715b08f8e2ef8c23b768322f5206afd
Mark O'Connor <mark.oconnor14@gmail.com>
2023-11-25 18:08:44 +0800

    feat: add questions and question navigation buttons to tests

--- src/pages/tests/[test_id]/index.tsx
+++ src/pages/tests/[test_id]/index.tsx
@@ -1,12 +1,29 @@
 import { useRouter } from "next/router";
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import { NextPageWithLayout } from "../../_app";
 
-const tests = [
+type Question = {
+  id: number;
+  question: string;
+};
+
+type Test = {
+  id: number;
+  name: string;
+};
+
+const tests: Test[] = [
   { id: 1, name: "Test 1" },
   { id: 2, name: "Test 2" },
   { id: 3, name: "Test 3" },
   { id: 4, name: "Test 4" },
+];
+
+const questions: Question[] = [
+  { id: 1, question: "Do her give the it to he?" },
+  { id: 2, question: "Does I look like he?" },
+  { id: 3, question: "Do we want to take her from it?" },
+  { id: 4, question: "Do I want to talk to they?" },
 ];
 
 const DoTestPage: NextPageWithLayout = () => {
@@ -14,10 +31,28 @@ const DoTestPage: NextPageWithLayout = () => {
   const selectedTest = tests.find((test) => {
     return test.id === Number(router.query.test_id);
   });
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
+  const currentQuestion: Question = questions[currentQuestionIndex];
 
+  function previousQuestion() {
+    if (currentQuestionIndex - 1 < 0) {
+      // check if first index
+      return;
+    }
+    setCurrentQuestionIndex((prevIndex) => prevIndex - 1);
+  }
+
+  function nextQuestion() {
+    if (currentQuestionIndex + 1 >= questions.length) {
+      // check if last index
+      return;
+    }
+    setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+  }
   return (
     <main>
-      <div className="bg-gray-200 absolute left-0 top-0 grid py-2 w-full grid-cols-10 opacity-0 duration-200 ease-in-out hover:opacity-100">
+      {/* Header and In-game Navigation */}
+      <div className="absolute left-0 top-0 grid w-full grid-cols-10 bg-gray-200 py-2 opacity-0 duration-200 ease-in-out hover:opacity-100">
         <button onClick={() => router.push("/tests")} className="col-span-1">
           Back
         </button>
@@ -25,10 +60,28 @@ const DoTestPage: NextPageWithLayout = () => {
           {selectedTest?.name}
         </h1>
       </div>
+      {/* Current Question */}
       <div className="h-screen">
         <div className="grid h-full place-items-center">
-          <p className="text-center text-[180px]">Did her give the it to he?</p>
+          <p className="text-center text-[180px]">{currentQuestion.question}</p>
         </div>
+      </div>
+      {/* Question button bar */}
+      <div className="flex justify-center gap-8 absolute bottom-0 left-0 w-full bg-gray-200 py-2 text-5xl opacity-0 duration-200 ease-in-out hover:opacity-100">
+        <button
+          onClick={() => {
+            previousQuestion();
+          }}
+        >
+          Back
+        </button>
+        <button
+          onClick={() => {
+            nextQuestion();
+          }}
+        >
+          Next
+        </button>
       </div>
     </main>
   );
