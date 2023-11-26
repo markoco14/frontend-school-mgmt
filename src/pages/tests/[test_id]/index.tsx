630779b9875a6eea58759770350d4a908b5f4c91
Mark O'Connor <mark.oconnor14@gmail.com>
2023-11-26 10:39:16 +0800

    feat: add isStarted state to top bar and bottom bar, add Cancel button to starting page

--- src/pages/tests/[test_id]/index.tsx
+++ src/pages/tests/[test_id]/index.tsx
@@ -82,14 +82,16 @@ const DoTestPage: NextPageWithLayout = () => {
     <>
       <main>
         {/* Header and in-game Navigation */}
-        <div className="absolute left-0 top-0 grid w-full grid-cols-10 bg-gray-200 py-2 opacity-0 duration-200 ease-in-out hover:opacity-100">
-          <button onClick={() => router.push("/tests")} className="col-span-1">
-            Quit
-          </button>
-          <h1 className="col-span-8 text-center text-5xl">
-            {selectedTest?.name}
-          </h1>
-        </div>
+        {isStarted && (
+          <div className="absolute left-0 top-0 grid w-full grid-cols-10 bg-gray-200 py-2 opacity-0 duration-200 ease-in-out hover:opacity-100">
+            <button onClick={() => router.push("/tests")} className="col-span-1">
+              Quit
+            </button>
+            <h1 className="col-span-8 text-center text-5xl">
+              {selectedTest?.name}
+            </h1>
+          </div>
+        )}
 
         {/* Test sections */}
         <div className="h-screen">
@@ -97,12 +99,20 @@ const DoTestPage: NextPageWithLayout = () => {
             {!isStarted ? (
               <>
                 <TestStartDetails test={selectedTest} />
-                <button
-                  className="rounded p-4 text-5xl underline underline-offset-4 duration-100 ease-in-out hover:-translate-y-2 hover:bg-green-600 hover:text-white"
-                  onClick={() => setIsStarted(true)}
-                >
-                  Start Test
-                </button>
+                <div className="flex gap-4">
+                  <button
+                    className="rounded p-4 text-5xl duration-100 ease-in-out hover:-translate-y-2 hover:bg-gray-600 hover:text-white"
+                    onClick={() => router.push("/tests")}
+                  >
+                    Quit
+                  </button>
+                  <button
+                    className="rounded p-4 text-5xl underline underline-offset-4 duration-100 ease-in-out hover:-translate-y-2 hover:bg-green-600 hover:text-white"
+                    onClick={() => setIsStarted(true)}
+                  >
+                    Start Test
+                  </button>
+                </div>
               </>
             ) : (
               <TestQuestionRenderer question={currentQuestion} />
@@ -111,31 +121,33 @@ const DoTestPage: NextPageWithLayout = () => {
         </div>
 
         {/* TestQuestion button bar */}
-        <div className="absolute bottom-0 left-0 flex w-full justify-center gap-8 bg-gray-200 py-2 text-5xl opacity-0 duration-200 ease-in-out hover:opacity-100">
-          {isStarted && (
+        {isStarted && (
+          <div className="absolute bottom-0 left-0 flex w-full justify-center gap-8 bg-gray-200 py-2 text-5xl opacity-0 duration-200 ease-in-out hover:opacity-100">
+            {isStarted && (
+              <button
+                onClick={() => {
+                  setIsStarted(false);
+                }}
+              >
+                Start
+              </button>
+            )}
             <button
               onClick={() => {
-                setIsStarted(false);
+                previousQuestion();
               }}
             >
-              Start
+              Prev
             </button>
-          )}
-          <button
-            onClick={() => {
-              previousQuestion();
-            }}
-          >
-            Prev
-          </button>
-          <button
-            onClick={() => {
-              nextQuestion();
-            }}
-          >
-            Next
-          </button>
-        </div>
+            <button
+              onClick={() => {
+                nextQuestion();
+              }}
+            >
+              Next
+            </button>
+          </div>
+        )}
       </main>
     </>
   );
