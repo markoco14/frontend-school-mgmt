eb5837cf9db830594b916a35d2d5b30ef576667d
Mark O'Connor <mark.oconnor14@gmail.com>
2023-11-26 15:25:59 +0800

    fix: variable may not exist ESLint error

--- src/modules/tests/components/TestStartDetails.tsx
+++ src/modules/tests/components/TestStartDetails.tsx
@@ -1,24 +1,27 @@
 import { Test } from "../entities/Test";
 
-const TestStartDetails = ({test}: {test: Test}) => {
-	let mistakeString = ''
+const TestStartDetails = ({ test }: { test: Test }) => {
+  let mistakeString = "";
 
-	for (let i = 0; i <= test.maxCorrections; i++) {
-		// Skip 0 mistakes if 0 mistakes not allowed
-		if (!test.allowNoCorrections && i === 0) {
-			continue
-		}
-		mistakeString += `${i}-`
-	}
+  if (test.maxCorrections) {
+    for (let i = 0; i <= test.maxCorrections; i++) {
+      // Skip 0 mistakes if 0 mistakes not allowed
+      if (!test.allowNoCorrections && i === 0) {
+        continue;
+      }
+      mistakeString += `${i}-`;
+    }
+  }
 
-
-	return(
-		<div className="text-center grid gap-8">
-			<p className="text-3xl sm:text-8xl mb-16">{test.name}</p>
-			<p className="text-xl sm:text-5xl">Correct any mistakes in the following sentences.</p>
-			<p className="text-3xl sm:text-8xl text-red-700">{mistakeString}X</p>
-		</div>
-	)
+  return (
+    <div className="grid gap-8 text-center">
+      <p className="mb-16 text-3xl sm:text-8xl">{test.name}</p>
+      <p className="text-xl sm:text-5xl">
+        Correct any mistakes in the following sentences.
+      </p>
+      <p className="text-3xl text-red-700 sm:text-8xl">{mistakeString}X</p>
+    </div>
+  );
 };
 
 export default TestStartDetails;
