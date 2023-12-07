57acca3224078445bd35dbb31a32ff8c1b76a878
Mark O'Connor <mark.oconnor14@gmail.com>
2023-12-07 14:59:01 +0800

    fix: token error handling that broke browser if token key exists but value not valid

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -4,7 +4,13 @@ import React, { createContext, useContext, useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { jwtAdapter } from "../modules/auth/adapters/jwtAdapter";
 import { School } from "../modules/school-mgmt/entities/School";
-import { getToken, removeToken, setToken } from "../utils/tokenUtils";
+import {
+  getToken,
+  isValidToken,
+  isTokenExpiredUtil,
+  removeToken,
+  setToken,
+} from "../utils/tokenUtils";
 
 type UserContextProviderProps = {
   children: React.ReactNode; // not allowed: React.ReactNode
@@ -27,25 +33,7 @@ type UserContextEntity = {
   handleDeselectSchool: Function;
 };
 
-// {
-// "token_type": "access", // access | refresh
-//   "exp": 1701158298,
-//   "iat": 1700899098,
-//   "jti": "9a0d2ef3ce704edf8261d2850716eef3",
-//   "user_id": 1,
-//   "name": "First name",
-//   "role": "A ROLE",
-//   "permissions": [
-//     1,
-//     3
-//   ],
-//   "email": "some-email@gmail.com"
-// }
 
-type Token = {
-  id: number;
-  exp: number;
-};
 
 const UserContext = createContext<UserContextEntity | null>(null);
 
@@ -88,32 +76,7 @@ export default function UserContextProvider({
     router.push("/");
   };
 
-  
   useEffect(() => {
-    function getTokenExpiry({ token }: { token: Token }) {
-      return token.exp;
-    }
-
-    function getExpiryDate({ tokenExpiry }: { tokenExpiry: number }) {
-      return new Date(tokenExpiry * 1000);
-    }
-
-    function isTokenExpired({ currentToken }: { currentToken: string }) {
-      const token: Token = jwt_decode(currentToken);
-
-      const tokenExpiry: number = getTokenExpiry({ token: token });
-
-      const expiryTime = getExpiryDate({ tokenExpiry: tokenExpiry });
-
-      const now = new Date();
-
-      if (expiryTime > now) {
-        return false;
-      }
-
-      return true;
-    }
-
     let logoutOnFailedUpdate = () => {
       setUser(null);
       setSelectedSchool(undefined);
@@ -122,45 +85,59 @@ export default function UserContextProvider({
       router.push("/");
     };
 
-    let updateToken = async () => {
-      const refreshToken = getToken("refreshToken");
-
+    let refreshAccessToken = async () => {
       try {
-        await jwtAdapter.refresh({ refresh: refreshToken }).then((res) => {
-          setUser(jwt_decode(res.access));
-          setToken({ accessToken: res.access, refreshToken: res.refresh });
-        });
+        const refreshToken = getToken("refreshToken");
+        const res = await jwtAdapter.refresh({ refresh: refreshToken });
+        const decodedUser = jwt_decode<AuthUser>(res.access);
+        setUser(decodedUser);
+        setToken({ accessToken: res.access, refreshToken: res.refresh });
       } catch (error) {
         toast.error("Your session expired. Please log in again.");
         logoutOnFailedUpdate();
       }
     };
 
-    // const tenSeconds = 1000 * 10;
-    const tenMinutes = 1000 * 60 * 10;
+    const tenSeconds = 1000 * 10;
+    // const tenMinutes = 1000 * 60 * 10;
     if (!user) {
       const accessToken = getToken("accessToken");
 
-      if (accessToken && !isTokenExpired({ currentToken: accessToken })) {
-        setUser(() => jwt_decode(accessToken));
+      if (accessToken && !isTokenExpiredUtil(accessToken)) {
+        const decodedUser = jwt_decode<AuthUser>(accessToken);
+        setUser(decodedUser);
       }
 
-      if (accessToken && isTokenExpired({ currentToken: accessToken })) {
-        const refreshToken = getToken("refreshToken");
+      // logout if access token key exists, but value empty string
+      if (accessToken === "") {
+        logoutOnFailedUpdate();
+      }
 
-        if (refreshToken && isTokenExpired({ currentToken: refreshToken })) {
+      // will not refresh if access token invalid
+      // but we need this for when we open the browser
+      // and did not log out last time
+      // so have access token and refresh token
+      // but access token is expired, and refresh is not
+      if (accessToken && isTokenExpiredUtil(accessToken)) {
+        if (isValidToken(accessToken)) {
+          refreshAccessToken();
+        } else {
           logoutOnFailedUpdate();
-          return;
         }
-
-        updateToken();
       }
     } else {
       const interval = setInterval(() => {
-        if (getToken("accessToken")) {
-          updateToken();
+        const accessToken = getToken("accessToken");
+        if (accessToken) {
+          if (isValidToken(accessToken)) {
+            refreshAccessToken();
+          } else {
+            logoutOnFailedUpdate();
+          }
+        } else {
+          logoutOnFailedUpdate();
         }
-      }, tenMinutes);
+      }, tenSeconds);
       return () => clearInterval(interval);
     }
   }, [user, router]);
