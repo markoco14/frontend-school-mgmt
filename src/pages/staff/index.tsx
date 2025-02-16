6c517ebc958ca069298a3f1e82dc8bd9cb52afeb
Mark <mark.oconnor14@gmail.com>
2025-02-16 12:00:09 +0800

    remove teachers and admin list from staff page

--- src/pages/staff/index.tsx
+++ src/pages/staff/index.tsx
@@ -1,30 +1,11 @@
 import { useUserContext } from "@/src/contexts/UserContext";
 import AdminLayout from "@/src/modules/core/components/AdminLayout";
 import Layout from "@/src/modules/core/components/Layout";
-import ParamsPageTabNav from "@/src/modules/core/components/ParamsPageTabNav";
 import PermissionDenied from "@/src/modules/core/components/PermissionDenied";
-import AdminSection from "@/src/modules/user-mgmt/components/AdminSection";
-import TeacherSection from "@/src/modules/user-mgmt/components/TeacherSection";
-import { useSearchParams } from "next/navigation";
 
 export default function Staff() {
   const { user } = useUserContext();
-  const searchParams = useSearchParams();
-  const tab = searchParams.get("tab") || "teachers";
-
-  const links = [
-    {
-      value: 1,
-      name: "Teachers",
-      urlString: "teachers",
-    },
-    {
-      value: 2,
-      name: "Admins",
-      urlString: "admins",
-    },
-  ];
-
+  
   if (user?.role !== "OWNER") {
     return (
       <Layout>
@@ -38,9 +19,7 @@ export default function Staff() {
       <AdminLayout>
         <div className="h-full w-full bg-white">
           <section className="grid max-w-[1000px] gap-4">
-            <ParamsPageTabNav links={links} tab={tab} />
-            {tab === "teachers" && <TeacherSection />}
-            {tab === "admins" && <AdminSection />}
+            <p>Staff section coming soon.</p>
           </section>
         </div>
       </AdminLayout>
