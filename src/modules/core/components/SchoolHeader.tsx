72cc6b4ab79e6d1336df85c697f5fadb186dee8e
Mark <mark.oconnor14@gmail.com>
2025-02-21 17:53:52 +0800

    update selectedSchool and isInSchoolWorkspace in header

--- src/modules/core/components/SchoolHeader.tsx
+++ src/modules/core/components/SchoolHeader.tsx
@@ -1,16 +1,15 @@
 import { checkIsSchools } from "@/src/utils/checkIsSchools";
-import { getSelectedSchool } from "@/src/utils/getSelectedSchool";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
 export default function SchoolHeader() {
   const router = useRouter();
-  const isSchools = checkIsSchools(router.asPath)
-  const selectedSchool = getSelectedSchool(router.asPath)
+  const isInSchoolWorkspace = checkIsSchools(router.asPath)
+  const selectedSchool = router.query.school
   return (
     <section>
       <div className="border-b h-[48px] px-2 sm:px-16 md:h-[72px] flex w-full items-center justify-between text-gray-500 ">
-        {isSchools && selectedSchool && selectedSchool !== "new" && (
+        {isInSchoolWorkspace && selectedSchool && selectedSchool !== "new" && (
           <>
             <span className="text-2xl">{selectedSchool ? selectedSchool : "No school"}</span>
             <Link href="/schools">Change school</Link>
